a41461b9ccbfb561efce03af944c9ef153cdf4a3
Aaron Xiao <xiaoxiangquan@gmail.com>
2019-01-17 11:27:53 -0800

    Move charts and docker code to another repo.

--- docker/apollo-worker/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-IMAGE=apolloauto/internal:apollo-cluster_$(date +%Y%m%d-%H%M)
-docker build --network host -t ${IMAGE} .
--- docker/spark-worker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-IMAGE=apolloauto/apollo:k8s-spark
-
-echo "Building ${IMAGE}..."
-docker build --network=host -t ${IMAGE} .
