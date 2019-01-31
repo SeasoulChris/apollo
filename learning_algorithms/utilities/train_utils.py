0f6652772551681b737f62282cd12c93380a0d54
panjiacheng <panjiacheng@yahoo.com>
2019-01-30 18:02:52 -0800

    Added social-lstm and semantic-map models.

--- learning_algorithms/utilities/train_utils.py
+++ learning_algorithms/utilities/train_utils.py
@@ -30,6 +30,12 @@ from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader, sampler
 
 
+def cuda(x):
+    #return x
+    if isinstance(x, (list, tuple)):
+        return [cuda(y) for y in x]
+    return x.cuda() if torch.cuda.is_available() else x
+
 def train_vanilla(train_X, train_y, model, loss, optimizer, epoch,
                   batch_preprocess, batch_size=1024, print_period=100):
     model.train()
@@ -100,6 +106,67 @@ def train_valid_vanilla(train_X, train_y, valid_X, valid_y, model, loss,
                       batch_preprocess, train_batch, print_period)
         valid_loss = valid_vanilla(valid_X, valid_y, model, loss,
                                    batch_preprocess, valid_batch)
+
+        scheduler.step(valid_loss)
+
+        # TODO(jiacheng): add early stopping mechanism
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            # save_checkpoint()
+
+    return model
+
+def train_dataloader(train_loader, model, loss, optimizer, epoch, print_period=100):
+    with torch.autograd.set_detect_anomaly(True):
+        model.train()
+
+        loss_history = []
+        logging.info('Epoch: {}:'.format(epoch))
+        print('Epoch: {}:'.format(epoch))
+        for i, (X, y) in enumerate(train_loader):
+            optimizer.zero_grad()
+            X, y = cuda(X), cuda(y)
+            pred = model(X)
+            train_loss = loss.loss_fn(pred, y)
+            loss_history.append(train_loss.item())
+            train_loss.backward()
+            optimizer.step()
+
+            if (i > 0) and (i % print_period == 0):
+                logging.info('   Step: {}, training loss: {}'.format(
+                    i, np.mean(loss_history[-print_period:])))
+                print ('   Step: {}, training loss: {}'.format(
+                    i, np.mean(loss_history[-print_period:])))
+
+        train_loss = np.mean(loss_history)
+        logging.info('Training loss: {}'.format(train_loss))
+        print('Training Loss: {}'.format(train_loss))
+
+def valid_dataloader(valid_loader, model, loss):
+    model.eval()
+
+    loss_history = []
+    for i, (X, y) in enumerate(valid_loader):
+        X, y = cuda(X), cuda(y)
+        pred = model(X)
+        valid_loss = loss.loss_fn(pred, y)
+        loss_history.append(valid_loss.item())
+        valid_loss_info = loss.loss_info(pred, y).item()
+        print (valid_loss_info)
+        logging.info('Validation avg displacement = {}'.format(valid_loss_info))
+
+    valid_loss = np.mean(loss_history)
+    logging.info('Validation loss: {}.'.format(valid_loss))
+    print ('Validation loss: {}.'.format(valid_loss))
+
+    return valid_loss
+
+def train_valid_dataloader(train_loader, valid_loader, model, loss, optimizer,
+                           scheduler, epochs, save_name, print_period=100):
+    best_valid_loss = float('+inf')
+    for epoch in range(1, epochs+1):
+        train_dataloader(train_loader, model, loss, optimizer, epoch, print_period)
+        valid_loss = valid_dataloader(valid_loader, model, loss)
 
         scheduler.step(valid_loss)
 
