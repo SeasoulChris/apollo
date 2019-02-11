57f36258eab42e9851390e7fca3d65ea1054ed3b
panjiacheng <panjiacheng@yahoo.com>
2019-02-11 11:06:08 -0800

    Tools: updated lane_scanning model and training pipeline.

--- learning_algorithms/models/lane_scanning_model/lane_scanning_model.py
+++ learning_algorithms/models/lane_scanning_model/lane_scanning_model.py
@@ -23,15 +23,125 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+from torch.utils.data import Dataset
+
+from utilities.helper_utils import *
+from utilities.IO_utils import *
 
 
-def world_coord_to_relative_coord(input_world_coord, ref_world_coord):
-    x_diff = input_world_coord[0] - ref_world_coord[0]
-    y_diff = input_world_coord[1] - ref_world_coord[1]
-    rho = math.sqrt(x_diff ** 2 + y_diff ** 2)
-    theta = math.atan2(y_diff, x_diff) - ref_world_coord[2]
+'''
+========================================================================
+Dataset set-up
+========================================================================
+'''
+class LaneScanningDataset(Dataset):
+    def __init__(self, dir, verbose=False):
+        self.all_files = GetListOfFiles(dir)
+        self.obs_features = []
+        self.lane_features = []
+        #self.lane_start_end = []
+        self.traj_labels = []
+        # Each file contains an array of lists with each list having different sizes.
+        # Go through each file:
+        for i, file in enumerate(self.all_files):
+            # Convert file content to list of lists
+            file_content = np.load(file)
+            file_content = file_content.tolist()
+            # Go through each entry and, if data point is valid,
+            # append to the lists above.
+            for data_point in file_content:
+                feature_len = data_point[0]
+                if feature_len == 45:
+                    if verbose:
+                        print ('Skipped this one because it has no lane.')
+                    continue
+                if feature_len <= 0:
+                    if verbose:
+                        print ('Skipped this one because it has no feature.')
+                    continue
+                if (feature_len-45) % 400 != 0:
+                    if verbose:
+                        print ('Skipped this one because dimension isn\'t correct.')
+                    continue
 
-    return math.cos(theta)*rho, math.sin(theta)*rho
+                curr_num_lanes = int((feature_len-45)/400)
+                self.obs_features.append(
+                    np.asarray(data_point[1:46]).reshape((1, 45)))
+                self.lane_features.append(
+                    np.asarray(data_point[46:-30]).reshape((curr_num_lanes, 400)))
+                #self.lane_start_end.append(
+                #    (lane_start_idx, lane_start_idx+curr_num_lanes))
+                traj_label = np.zeros((1, 30))
+                for j, point in enumerate(data_point[-30:-15]):
+                    traj_label[0, j], traj_label[0, j+15] = \
+                        world_coord_to_relative_coord(point, data_point[-30])
+                self.traj_labels.append(traj_label)
+            print ('Loaded {} out of {} files'.format(i+1, len(self.all_files)))
+
+        self.length = len(self.obs_features)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        return (self.obs_features[idx], \
+                self.lane_features[idx], \
+                self.lane_features[idx].shape[0], \
+                self.traj_labels[idx])
+
+
+def collate_fn(batch):
+    # batch is a list of tuples, so unzip to form lists of np-arrays.
+    obs_features, lane_features, num_lanes, traj_labels = zip(*batch)
+
+    # Stack into numpy arrays.
+    N = len(obs_features)
+    # N x 45
+    obs_features = np.concatenate(obs_features, axis=0)
+    # M x 400 (M >= N, M = sum(num_lanes))
+    lane_features = np.concatenate(lane_features, axis=0)
+    # list of N numbers
+    num_lanes = np.asarray(num_lanes)
+    # N x 30
+    traj_labels = np.concatenate(traj_labels, axis=0)
+
+    # Convert to torch tensors.
+    return (torch.from_numpy(obs_features), \
+            torch.from_numpy(lane_features), \
+            torch.from_numpy(num_lanes)) ,\
+           torch.from_numpy(traj_labels)
+
+def collate_with_padding(batch):
+    # batch is a list of tuples, so unzip to form lists of np-arrays.
+    obs_features, lane_features, num_lanes, traj_labels = zip(*batch)
+
+    # Stack into numpy arrays.
+    N = len(obs_features)
+    max_num_laneseq = max(num_lanes)
+    # N x 45
+    obs_features = np.concatenate(obs_features, axis=0)
+    # N x max_num_laneseq x 400
+    lane_features_padded = np.zeros((N, max_num_laneseq, 400))
+    for i, lane_fea in enumerate(lane_features):
+        lane_features_padded[i, :num_lanes[i], :] = lane_fea
+    # N x 1
+    num_lanes = np.asarray(num_lanes).reshape((-1))
+    # N x 30
+    traj_labels = np.concatenate(traj_labels, axis=0)
+
+    # Sort in descending order of available number of lanes
+    idx_new = np.argsort(-num_lanes).tolist()
+    obs_features = obs_features[idx_new]
+    lane_features_padded = lane_features_padded[idx_new]
+    num_lanes = num_lanes[idx_new]
+    traj_labels = traj_labels[idx_new]
+
+    # Convert to torch tensors.
+    return (torch.from_numpy(obs_features).view(N, 45), \
+            torch.from_numpy(lane_features_padded).view(N, max_num_laneseq, 400), \
+            torch.from_numpy(num_lanes).float()),\
+           torch.from_numpy(traj_labels).view(N, 30)
+
 
 '''
 @brief Apply zero-padding so that the input tensor to the model is of uniform size.
@@ -47,7 +157,7 @@ def preprocess_features(features):
 
     N = len(features)
     padded_features = np.zeros((N, max_feature_len+1))
-    labels = np.zeros((N, 20))
+    labels = np.zeros((N, 30))
     num_skipped = 0
 
     for i, feature_len in enumerate(feature_lengths):
@@ -71,11 +181,11 @@ def preprocess_features(features):
         padded_features[i-num_skipped, 0:feature_len] = \
             features[i][1:feature_len+1]
         padded_features[i-num_skipped, -1] = num_laneseq[i]
-        for j, point in enumerate(features[i][-30:-20]):
-            labels[i-num_skipped, j], labels[i-num_skipped, j+10] =\
+        for j, point in enumerate(features[i][-30:-15]):
+            labels[i-num_skipped, j], labels[i-num_skipped, j+15] =\
                 world_coord_to_relative_coord(point, features[i][-30])
         # sanity check:
-        if labels[i-num_skipped, 0] != 0.0 or labels[i-num_skipped, 10] != 0.0:
+        if labels[i-num_skipped, 0] != 0.0 or labels[i-num_skipped, 15] != 0.0:
             print ('Future trajectory isn\'t correctly normalized.')
 
     if num_skipped == 0:
@@ -88,6 +198,12 @@ def batch_preprocess(X, y):
     idx_2 = torch.argsort(X[:,-1], descending=True).tolist()
     return X[idx_2], y[idx_2]
 
+
+'''
+========================================================================
+Model definition
+========================================================================
+'''
 class lane_scanning_model(torch.nn.Module):
     def __init__(self):
         super(lane_scanning_model, self).__init__()
@@ -107,6 +223,7 @@ class lane_scanning_model(torch.nn.Module):
         self.single_lane_maxpool = nn.MaxPool1d(4)
         self.single_lane_dropout = nn.Dropout(0.0)
 
+        # TODO(jiacheng): add attention mechanisms to focus on some lanes.
         self.multi_lane_rnn = nn.GRU(
             input_size=50,
             hidden_size=34,
@@ -133,10 +250,134 @@ class lane_scanning_model(torch.nn.Module):
         self.traj_fc = torch.nn.Sequential(
             nn.Linear(79, 46),
             nn.ReLU(),
-            nn.Linear(46, 20)
+            nn.Linear(46, 30)
         )
 
     def forward(self, X):
+        obs_fea, lane_fea, num_laneseq = X
+        N = obs_fea.size(0)
+        max_num_laneseq = lane_fea.size(1)
+
+        # Process each lane-sequence with CNN-1d
+        # N x max_num_laneseq x 400
+        lane_fea = lane_fea.view(N * max_num_laneseq, 100, 4)
+        lane_fea = lane_fea.transpose(1, 2).float()
+        # (N * max_num_laneseq) x 4 x 100
+        lane_fea = self.single_lane_cnn(lane_fea)
+        # (N * max_num_laneseq) x 25 x 8
+        lane_fea = self.single_lane_maxpool(lane_fea)
+        # (N * max_num_laneseq) x 25 x 2
+        lane_fea = lane_fea.view(lane_fea.size(0), -1)
+        lane_fea = self.single_lane_dropout(lane_fea)
+        embed_dim = lane_fea.size(1)
+        # (N * max_num_laneseq) x 50
+        lane_fea = lane_fea.view(N, max_num_laneseq, embed_dim)
+
+        # Process each obstacle's lane-sequences with RNN
+        static_fea = pack_padded_sequence(lane_fea, num_laneseq, batch_first=True)
+        static_fea_states, _ = self.multi_lane_rnn(static_fea)
+        static_fea, _ = pad_packed_sequence(static_fea_states, batch_first=True)
+        # N x max_num_laneseq x 68
+        min_val = torch.min(static_fea)
+        static_fea_maxpool, _ = pad_packed_sequence(
+            static_fea_states, batch_first=True, padding_value=min_val.item()-0.1)
+        static_fea_maxpool, _ = torch.max(static_fea_maxpool, 1)
+        static_fea_avgpool = torch.sum(static_fea, 1) / num_laneseq.reshape(N,1)
+        static_fea_front = static_fea[:,0]
+        idx_1 = np.arange(N).tolist()
+        idx_2 = (num_laneseq-1).int().data.tolist()
+        static_fea_back = static_fea[idx_1, idx_2]
+        static_fea_all = torch.cat((static_fea_maxpool, static_fea_avgpool, \
+            static_fea_front, static_fea_back), 1)
+        # N x (68 * 4) = N x 272
+
+        static_fea_final = self.multi_lane_fc(static_fea_all)
+        obs_fea_final = self.obs_feature_fc(obs_fea.float())
+        fea_all = torch.cat((obs_fea_final, static_fea_final), 1)
+        # N x 79
+
+        # TODO(jiacheng): use RNN decoder to output traj
+        # TODO(jiacheng): multi-modal prediction
+        traj = self.traj_fc(fea_all)
+        # N x 30
+
+        return traj
+
+
+    def forward_dataloader(self, X):
+        obs_fea, lane_fea, num_laneseq = X
+        num_laneseq = num_laneseq.tolist()
+
+        # Process each lane-sequence with CNN-1d
+        # M x (100 * 4)
+        lane_fea = lane_fea.view(lane_fea.size(0), 100, 4)
+        lane_fea = lane_fea.transpose(1, 2).float()
+        # M x 4 x 100
+        lane_fea = self.single_lane_cnn(lane_fea)
+        # M x 25 x 8
+        lane_fea = self.single_lane_maxpool(lane_fea)
+        # M x 25 x 2
+        lane_fea = lane_fea.view(lane_fea.size(0), -1)
+        lane_fea = self.single_lane_dropout(lane_fea)
+        embed_dim = lane_fea.size(1)
+        # M x 50
+
+        # Group the lane-sequences for each obstacle
+        max_num_laneseq = max(num_laneseq)
+        N = obs_fea.size(0)
+        # N x max_num_laneseq x 50
+        lane_fea_rnn = torch.zeros(N, max_num_laneseq, embed_dim).cuda()
+        end_idx = np.cumsum(num_laneseq).tolist()
+        start_idx = ([0] + end_idx)[:-1]
+        start_end_idx = [
+            (start, end) for start, end in zip(start_idx, end_idx)]
+        # list of N tuples
+        for i, (start, end) in enumerate(start_end_idx):
+            lane_fea_rnn[i, :num_laneseq[i], :] = lane_fea[start:end, :]
+        # lane_fea_rnn contains lane_fea with padded zeros.
+
+        # Sort and pack for RNN
+        num_laneseq = torch.from_numpy(np.asarray(num_laneseq)).cuda()
+        idx_new = torch.argsort(num_laneseq, descending=True).tolist()
+        # N x 45
+        obs_fea = obs_fea[idx_new]
+        # N x max_num_laneseq x 50
+        lane_fea_rnn = lane_fea_rnn[idx_new]
+        # N x 1
+        num_laneseq = num_laneseq[idx_new]
+        num_laneseq = num_laneseq.float()
+
+        # Process each obstacle's lane-sequences with RNN
+        static_fea = pack_padded_sequence(lane_fea_rnn, num_laneseq, batch_first=True)
+        static_fea_states, _ = self.multi_lane_rnn(static_fea)
+        static_fea, _ = pad_packed_sequence(static_fea_states, batch_first=True)
+        # N x max_num_laneseq x 68
+        min_val = torch.min(static_fea)
+        static_fea_maxpool, _ = pad_packed_sequence(
+            static_fea_states, batch_first=True, padding_value=min_val.item()-0.1)
+        static_fea_maxpool, _ = torch.max(static_fea_maxpool, 1)
+        static_fea_avgpool = torch.sum(static_fea, 1) / num_laneseq.reshape(N,1)
+        static_fea_front = static_fea[:,0]
+        idx_1 = np.arange(N).tolist()
+        idx_2 = (num_laneseq-1).int().data.tolist()
+        static_fea_back = static_fea[idx_1, idx_2]
+        static_fea_all = torch.cat((static_fea_maxpool, static_fea_avgpool, \
+            static_fea_front, static_fea_back), 1)
+        # N x (68 * 4) = N x 272
+
+        static_fea_final = self.multi_lane_fc(static_fea_all)
+        obs_fea_final = self.obs_feature_fc(obs_fea.float())
+        fea_all = torch.cat((obs_fea_final, static_fea_final), 1)
+        # N x 79
+
+        # TODO(jiacheng): use RNN decoder to output traj
+        # TODO(jiacheng): multi-modal prediction
+        traj = self.traj_fc(fea_all)
+        # N x 30
+
+        return traj
+
+    def forward_vanilla(self, X):
         inputs = X[:,:-1]           # N x (45 + 400 * max_num_laneseq)
         num_laneseq = X[:,-1]       # N
 
@@ -193,9 +434,9 @@ class lane_scanning_model(torch.nn.Module):
 
 class lane_scanning_loss:
     def loss_fn(self, y_pred, y_true):
-        #TODO(jiacheng): elaborate on this
+        #TODO(jiacheng): elaborate on this (e.g. consider final displacement error, etc.)
         loss_func = nn.MSELoss()
-        return loss_func(y_pred, y_true)
+        return loss_func(y_pred, y_true.float())
 
     def loss_info(self, y_pred, y_true):
         return
--- learning_algorithms/models/lane_scanning_model/pipeline_lane_scanning.py
+++ learning_algorithms/models/lane_scanning_model/pipeline_lane_scanning.py
@@ -14,17 +14,17 @@
 # limitations under the License.
 ###############################################################################
 
-
 import argparse
 import logging
 import numpy as np
 import os
 import torch
+import sklearn
 
-from data_preprocessing.features_labels_combining import *
-from models.lane_scanning_model import *
+from data_preprocessing.features_labels_utils import *
+from models.lane_scanning.lane_scanning_model import *
+from utilities.IO_utils import *
 from utilities.train_utils import *
-
 
 cuda_is_available = torch.cuda.is_available()
 logging.basicConfig(filename='testlog.log', level=logging.INFO)
@@ -37,57 +37,107 @@ if __name__ == "__main__":
 
     parser.add_argument('train_file', type=str, help='training data')
     parser.add_argument('valid_file', type=str, help='validation data')
-
     parser.add_argument('-s', '--save-path', type=str, default='./',
                         help='Specify the directory to save trained models.')
+    parser.add_argument('-v', '--vanilla_train', action='store_true', \
+                        help='Don\'t use data loader')
 
     args = parser.parse_args()
 
     # load and preprocess data:
-    # TODO(jiacheng): currently for test-purpose only, must replace with new
-    #                 code later.
-    data_train_all = LoadDataForLearning(args.train_file)
-    data_train = []
-    for item in data_train_all:
-        data_train.append(item.features_for_learning)
-    data_train = preprocess_features(data_train)
+    files_train = GetListOfFiles(args.train_file)
+    files_valid = GetListOfFiles(args.valid_file)
 
-    data_valid_all = LoadDataForLearning(args.valid_file)
-    data_valid = []
-    for item in data_valid_all:
-        data_valid.append(item.features_for_learning)
-    data_valid = preprocess_features(data_valid)
+    if args.vanilla_train:
+        X_train, y_train = None, None
+        for i, file in enumerate(files_train):
+            file_content = np.load(file)
+            X_temp, y_temp = preprocess_features(file_content)
+            if X_train is None:
+                X_train, y_train = X_temp, y_temp
+            else:
+                X_train = np.concatenate((X_train, X_temp), 0)
+                y_train = np.concatenate((y_train, y_temp), 0)
+            print ('Processed {} out of {} training files.'\
+                   .format(i+1, len(files_train)))
+        print (X_train.shape)
+        print (y_train.shape)
+        X_train, _, y_train, _ = sklearn.model_selection.train_test_split(\
+            X_train, y_train, test_size=0.0, random_state=36)
 
-    X_train = data_train
-    y_train = X_train[:,-20:]
-    X_valid = data_valid
-    y_valid = X_valid[:,-20:]
+        X_valid, y_valid = None, None
+        for i, file in enumerate(files_valid):
+            file_content = np.load(file)
+            X_temp, y_temp = preprocess_features(file_content)
+            if X_valid is None:
+                X_valid, y_valid = X_temp, y_temp
+            else:
+                X_valid = np.concatenate((X_valid, X_temp), 0)
+                y_valid = np.concatenate((y_valid, y_temp), 0)
+            print ('Processed {} out of {} validation files.'\
+                   .format(i+1, len(files_valid)))
+        print (X_valid.shape)
+        print (y_valid.shape)
 
-    # Model and training setup
-    model = lane_scanning_model()
-    loss = lane_scanning_loss()
-    print (model)
-    learning_rate = 1e-1
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer, factor=0.3, patience=2, min_lr=1e-9, verbose=True, mode='min')
+        # Model and training setup
+        model = lane_scanning_model()
+        loss = lane_scanning_loss()
+        print (model)
+        learning_rate = 1e-3
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, factor=0.3, patience=2, min_lr=1e-9, verbose=True, mode='min')
 
-    # CUDA setup:
-    if (cuda_is_available):
-        print ("Using CUDA to speed up training.")
-        model.cuda()
-        X_train = Variable(torch.FloatTensor(X_train).cuda())
-        X_valid = Variable(torch.FloatTensor(X_valid).cuda())
-        y_train = Variable(torch.FloatTensor(y_train).cuda())
-        y_valid = Variable(torch.FloatTensor(y_valid).cuda())
+        # CUDA setup:
+        if (cuda_is_available):
+            print ("Using CUDA to speed up training.")
+            model.cuda()
+            X_train = Variable(torch.FloatTensor(X_train).cuda())
+            X_valid = Variable(torch.FloatTensor(X_valid).cuda())
+            y_train = Variable(torch.FloatTensor(y_train).cuda())
+            y_valid = Variable(torch.FloatTensor(y_valid).cuda())
+        else:
+            print ("Not using CUDA.")
+            X_train = Variable(torch.FloatTensor(X_train))
+            X_valid = Variable(torch.FloatTensor(X_valid))
+            y_train = Variable(torch.FloatTensor(y_train))
+            y_valid = Variable(torch.FloatTensor(y_valid))
+
+        # Model training:
+        train_valid_vanilla(X_train, y_train, X_valid, y_valid, model, loss,
+                            optimizer, scheduler, 50, './', batch_preprocess,
+                            train_batch=512, print_period=100, valid_batch=512)
     else:
-        print ("Not using CUDA.")
-        X_train = Variable(torch.FloatTensor(X_train))
-        X_valid = Variable(torch.FloatTensor(X_valid))
-        y_train = Variable(torch.FloatTensor(y_train))
-        y_valid = Variable(torch.FloatTensor(y_valid))
+        train_dataset = LaneScanningDataset(args.train_file)
+        valid_dataset = LaneScanningDataset(args.valid_file)
 
-    # Model training:
-    train_valid_vanilla(X_train, y_train, X_valid, y_valid, model, loss,
-                        optimizer, scheduler, 50, './', batch_preprocess,
-                        train_batch=1024, print_period=100, valid_batch=1024)
+        train_loader = DataLoader(train_dataset, batch_size=2048, shuffle=True,\
+            num_workers=8, collate_fn=collate_with_padding)
+        valid_loader = DataLoader(valid_dataset, batch_size=2048, shuffle=True,\
+            num_workers=8, collate_fn=collate_with_padding)
+
+        # Model and training setup
+        model = lane_scanning_model()
+        loss = lane_scanning_loss()
+        print (model)
+        learning_rate = 3e-4
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, factor=0.3, patience=2, min_lr=1e-9, verbose=True, mode='min')
+
+        # CUDA setup:
+        if (torch.cuda.is_available()):
+            print ("Using CUDA to speed up training.")
+            model.cuda()
+        else:
+            print ("Not using CUDA.")
+
+
+        # Model training:
+        train_valid_dataloader(train_loader, valid_loader, model, loss, optimizer,
+                               scheduler, epochs=100, save_name='./', print_period=10)
+
+
+
+
+
