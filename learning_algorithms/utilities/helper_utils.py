c8f1213c6e6a15d6faf4b573f2f778cdf5069f85
panjiacheng <panjiacheng@yahoo.com>
2019-02-11 11:09:40 -0800

    Tools: added helper_utils containing useful helper functions.

---
+++ learning_algorithms/utilities/helper_utils.py
@@ -0,0 +1,27 @@
+###############################################################################
+# Copyright 2019 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+import math
+import numpy as np
+
+
+def world_coord_to_relative_coord(input_world_coord, ref_world_coord):
+    x_diff = input_world_coord[0] - ref_world_coord[0]
+    y_diff = input_world_coord[1] - ref_world_coord[1]
+    rho = math.sqrt(x_diff ** 2 + y_diff ** 2)
+    theta = math.atan2(y_diff, x_diff) - ref_world_coord[2]
+
+    return math.cos(theta)*rho, math.sin(theta)*rho
