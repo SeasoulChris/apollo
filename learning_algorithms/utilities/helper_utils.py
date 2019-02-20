d7b408c4e54795cffab8ea3214b23ef0a4af5068
Hongyi <henrysun@umich.edu>
2019-02-20 11:56:36 -0800

    Tools: using numpy on helper_utils

--- learning_algorithms/utilities/helper_utils.py
+++ learning_algorithms/utilities/helper_utils.py
@@ -14,14 +14,13 @@
 # limitations under the License.
 ###############################################################################
 
-import math
 import numpy as np
 
 
 def world_coord_to_relative_coord(input_world_coord, ref_world_coord):
     x_diff = input_world_coord[0] - ref_world_coord[0]
     y_diff = input_world_coord[1] - ref_world_coord[1]
-    rho = math.sqrt(x_diff ** 2 + y_diff ** 2)
-    theta = math.atan2(y_diff, x_diff) - ref_world_coord[2]
+    rho = np.sqrt(x_diff ** 2 + y_diff ** 2)
+    theta = np.arctan2(y_diff, x_diff) - ref_world_coord[2]
 
-    return math.cos(theta)*rho, math.sin(theta)*rho
+    return (np.cos(theta)*rho, np.sin(theta)*rho)
