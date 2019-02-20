c0d3a92b1631f1c234eb3716191c992fc7dc2ee2
Longtao Lin <michael4338@hotmail.com>
2019-02-19 21:28:35 -0800

    tools: upload/download tool for bos(client part) (#68)
    * tools: upload/download tool for bos(client part)
    
    * tools: uploading tool for bos
    
    * tools: uploading tool for bos

--- apps/static/README.md
+++ apps/static/README.md
@@ -3,7 +3,7 @@
 ## Upload a single file to BOS
 
 ```bash
-upload_to_bos --from=path/to/local/file.txt --to=path/to/bos/file.txt
+bos_fstool --src=path/to/local/file.txt --dst=path/to/bos/file.txt
 ```
 
 When finished, you'll see the file at /mnt/bos/path/to/bos/file.txt, if your BOS
--- fueling/common/s3_utils.py
+++ fueling/common/s3_utils.py
@@ -13,20 +13,25 @@ import pyspark_utils.helper as spark_helper
 S3_MOUNT_PATH = '/mnt/bos'
 
 
-def s3_client():
+def s3_client(aws_ak=None, aws_sk=None):
     """Get S3 client."""
-    if not os.environ.get('AWS_ACCESS_KEY_ID'):
-        glog.error('No S3 credentials found in system environment.')
+    if aws_ak is None or aws_sk is None:
+        aws_ak = os.environ.get('AWS_ACCESS_KEY_ID')
+        aws_sk = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    if aws_ak is None or aws_sk is None:
+        glog.error('No S3 credentials provided.')
         return None
     return boto3.client('s3', endpoint_url='http://s3.bj.bcebos.com',
                         region_name='bj',
-                        config=botocore.client.Config(signature_version='s3v4'))
+                        config=botocore.client.Config(signature_version='s3v4'),
+                        aws_access_key_id=aws_ak,
+                        aws_secret_access_key=aws_sk)
 
 def abs_path(obj_rel_path):
     """Get absolute mounted path of an S3 object."""
     return os.path.join(S3_MOUNT_PATH, obj_rel_path)
 
-def list_objects(bucket, prefix=''):
+def list_objects(bucket, prefix='', aws_ak=None, aws_sk=None):
     """
     Get a list of objects in format:
     {u'Key': '/path/to/file',
@@ -35,7 +40,7 @@ def list_objects(bucket, prefix=''):
      u'Size': 141022543, ...
     }
     """
-    paginator = s3_client().get_paginator('list_objects')
+    paginator = s3_client(aws_ak, aws_sk).get_paginator('list_objects')
     page_iterator = paginator.paginate(Bucket=bucket, Prefix=prefix)
     for page in page_iterator:
         for obj in page.get('Contents', []):
@@ -55,12 +60,13 @@ def list_dirs(bucket, prefix=''):
         .filter(lambda obj: obj['Key'].endswith('/')) \
         .map(lambda obj: obj['Key'][:-1])  # Remove the trailing slash.
 
-def file_exists(bucket, remote_path):
+def file_exists(bucket, remote_path, aws_ak, aws_sk):
     """Check if specified file is existing"""
-    return len(list(list_objects(bucket, remote_path))) > 0
+    return len(list(list_objects(bucket, remote_path, aws_ak, aws_sk))) > 0
 
-def upload_file(bucket, local_path, remote_path, meta_data=None):
+def upload_file(bucket, local_path, remote_path, aws_keys, meta_data=None):
     """Upload a file from local to BOS"""
+    aws_ak, aws_sk = aws_keys
     # arguments validation
     if not os.path.exists(local_path):
         raise ValueError('No local file/folder found for uploading')
@@ -75,10 +81,10 @@ def upload_file(bucket, local_path, remote_path, meta_data=None):
     if len(sub_paths) < minimal_path_depth or len(sub_paths) > maximal_path_depth:
         raise ValueError('Destination path is either too short or too long')
 
-    if file_exists(bucket, remote_path):
+    if file_exists(bucket, remote_path, aws_ak, aws_sk):
         raise ValueError('Destination already exists')
 
-    if not file_exists(bucket, sub_paths[0]):
+    if not file_exists(bucket, sub_paths[0], aws_ak, aws_sk):
         raise ValueError('Creating folders or files under root is not allowed')
 
     # Set default MetaData if it's not specified
@@ -86,11 +92,25 @@ def upload_file(bucket, local_path, remote_path, meta_data=None):
         meta_data = {'User': 'apollo-user'}
 
     # Actually upload
-    s3_client().upload_file(local_path,
-                            bucket,
-                            remote_path,
-                            ExtraArgs={"Metadata": meta_data})
+    s3_client(aws_ak, aws_sk)\
+        .upload_file(local_path,
+                     bucket,
+                     remote_path,
+                     ExtraArgs={"Metadata": meta_data})
 
-def download_file(bucket, remote_path, local_path):
+def download_file(bucket, remote_path, local_path, aws_keys):
     """Download a file from BOS to local"""
-    s3_client().download_file(bucket, remote_path, local_path)
+    # Support download a dir with all files under it
+    aws_ak, aws_sk = aws_keys
+    if os.path.isdir(local_path):
+        for obj in list_objects(bucket, remote_path, aws_ak, aws_sk):
+            remote_file = obj['Key']
+            # aws_skip the folder itself
+            if remote_file == remote_path or remote_file == remote_path+'/':
+                continue
+            s3_client(aws_ak, aws_sk)\
+                .download_file(bucket,
+                               remote_file,
+                               os.path.join(local_path, os.path.basename(remote_file)))
+    else:
+        s3_client(aws_ak, aws_sk).download_file(bucket, remote_path, local_path)
