a68ac76a19ccfa6a49949e2a08c5abf97d652457
Shu Jiang <shujiang2016@gmail.com>
2019-03-05 17:45:51 -0800

    Control: calibration table feature extraction done

--- fueling/control/cal_tab_feature_ext/feature_ext.py
+++ fueling/control/cal_tab_feature_ext/feature_ext.py
@@ -1,100 +1,141 @@
 #!/usr/bin/env python
-import fueling.common.record_utils as record_utils
-import fueling.control.features.common_cal_tab as commonCalTab
-import fueling.control.features.common_feature_extraction as CommonFE
-from fueling.common.base_pipeline import BasePipeline
+
 """
 This is a module to extraction features from records
 with folder path as part of the key
 """
+
+from collections import Counter
+import operator
 import os
-from scipy.misc import imsave
-import numpy as np
+
 import pyspark_utils.op as spark_op
 
+from fueling.common.base_pipeline import BasePipeline
+import fueling.common.record_utils as record_utils
+import fueling.common.s3_utils as s3_utils
+import fueling.common.colored_glog as glog
+import fueling.control.features.feature_extraction_utils as feature_extraction_utils
+import fueling.control.features.calibration_table_utils as calibration_table_utils
 
-class CalTabFeatureExtPipeline(BasePipeline):
+WANTED_VEHICLE = 'Mkz7'
+MIN_MSG_PER_SEGMENT = 1
+
+
+class CalTabFeatureExt(BasePipeline):
     def __init__(self):
         """ initialize """
         BasePipeline.__init__(self, 'feature_ext')
 
     def run_test(self):
         """Run test."""
-        folder_path = ["/apollo/modules/data/fuel/testdata/control/transient_1",
-                       '/apollo/modules/data/fuel/testdata/control/mkz7_s/']
+        records = ['modules/data/fuel/testdata/control/transit/1.record.00000',
+                   'modules/data/fuel/testdata/control/mkz7_s/20190222134935.record.00000',
+                   'modules/data/fuel/testdata/control/mkz7_s/20190222134935.record.00001',
+                   'modules/data/fuel/testdata/control/mkz7_s/20190222134935.record.00002',
+                   'modules/data/fuel/testdata/control/mkz7_s/20190222134935.record.00003',
+                   'modules/data/fuel/testdata/control/mkz7_s/20190222134935.record.00004']
 
-        spark_context = self.get_spark_context()
-        records_rdd = spark_context.parallelize(folder_path)
+        origin_prefix = 'modules/data/fuel/testdata/control'
+        target_prefix = 'modules/data/fuel/testdata/control/generated'
+        root_dir = '/apollo'
+        dir_to_records = self.get_spark_context().parallelize(
+            records).keyBy(os.path.dirname)
 
-        self.run(records_rdd)
+        self.run(dir_to_records, origin_prefix, target_prefix, root_dir)
 
-    @staticmethod
-    def run(records_rdd):
+    def run_prod(self):
+        """Run prod."""
+        bucket = 'apollo-platform'
+        origin_prefix = 'small-records/2019/'
+        target_prefix = 'modules/control/feature_extraction_hf5/2019/'
+        root_dir = s3_utils.S3_MOUNT_PATH
+
+        files = s3_utils.list_files(bucket, origin_prefix).cache()
+        complete_dirs = files.filter(
+            lambda path: path.endswith('/COMPLETE')).map(os.path.dirname)
+        dir_to_records = files.filter(
+            record_utils.is_record_file).keyBy(os.path.dirname)
+        root_dir = s3_utils.S3_MOUNT_PATH
+        self.run(spark_op.filter_keys(dir_to_records, complete_dirs),
+                 origin_prefix, target_prefix, root_dir)
+
+    def run(self, dir_to_records_rdd, origin_prefix, target_prefix, root_dir):
         """ processing RDD """
+        # -> (dir, record), in absolute path
+        dir_to_records = dir_to_records_rdd.map(lambda x: (os.path.join(root_dir, x[0]),
+                                                           os.path.join(root_dir, x[1])))
 
-        # parameters
-        wanted_vehicle = 'Mkz7'
-        # wanted_vehicle = 'Transit'
-        wanted_chs = ['/apollo/canbus/chassis',
-                      '/apollo/localization/pose']
+        selected_vehicles = (
+            # -> (dir, vehicle)
+            feature_extraction_utils.get_vehicle_of_dirs(dir_to_records)
+            # -> (dir, vehicle), where vehicle is WANTED_VEHICLE
+            .filter(spark_op.filter_value(lambda vehicle: vehicle == WANTED_VEHICLE))
+            # -> dir
+            .keys())
 
-        # choose record for wanted vehicle type
-        folder_vehicle_rdd = (records_rdd
-                              # key as folder path
-                              .keyBy(lambda x: x)
-                              .flatMapValues(CommonFE.folder_to_record)
-                              .flatMapValues(record_utils.read_record(['/apollo/hmi/status']))
-                              # parse message
-                              .mapValues(record_utils.message_to_proto)
-                              .mapValues(lambda elem: elem.current_vehicle)
-                              .filter(lambda elem: elem[1] == wanted_vehicle)
-                              # remove duplication of folders
-                              .distinct()
-                              # choose only folder path
-                              .map(lambda x: x[0]))
-        print folder_vehicle_rdd.count()
+        glog.info('Finished %d selected_vehicles!' % selected_vehicles.count())
+        glog.info('First elem in selected_vehicles is : %s ' %
+                  selected_vehicles.first())
 
-        # record to msg, (folder_dir,msg)
-        channels_rdd = (folder_vehicle_rdd
-                        .keyBy(lambda x: x)
-                        # record path
-                        .flatMapValues(CommonFE.folder_to_record)
-                        # read message
-                        .flatMapValues(record_utils.read_record(wanted_chs))
-                        # parse message
-                        .mapValues(record_utils.message_to_proto))
-        print channels_rdd.count()
+        channels = {record_utils.CHASSIS_CHANNEL,
+                    record_utils.LOCALIZATION_CHANNEL}
+        dir_to_msgs = (
+            spark_op.filter_keys(dir_to_records, selected_vehicles)
+            # -> (dir, msg)
+            .flatMapValues(record_utils.read_record(channels))
+            # -> (dir_segment, msg)
+            .map(feature_extraction_utils.gen_pre_segment))
+        glog.info('Finished %d dir_to_msgs!' % dir_to_msgs.count())
 
-        # aggregate msg to segment
-        pre_segment_rdd = (channels_rdd
-                           # choose time as key, group msg into 1 sec
-                           .map(CommonFE.gen_key)
-                           # combine chassis message and pose message with the same key
-                           .combineByKey(CommonFE.to_list, CommonFE.append, CommonFE.extend)
-                           # msg list(path_key,(chassis,pose))
-                           .mapValues(CommonFE.process_seg))
+        valid_segments = (
+            dir_to_msgs
+            # -> (dir_segment, topic_counter)
+            .mapValues(lambda msg: Counter([msg.topic]))
+            # -> (dir_segment, topic_counter)
+            .reduceByKey(operator.add)
+            # -> (dir_segment, topic_counter)
+            .filter(spark_op.filter_value(
+                    lambda counter:
+                    counter.get(record_utils.CHASSIS_CHANNEL, 0) >= MIN_MSG_PER_SEGMENT and
+                    counter.get(record_utils.LOCALIZATION_CHANNEL, 0) >= MIN_MSG_PER_SEGMENT))
+            # -> dir_segment
+            .keys())
 
-        print pre_segment_rdd.count()
+        dir_to_msgs = spark_op.filter_keys(dir_to_msgs, valid_segments)
 
-        data_rdd = (pre_segment_rdd
+        glog.info('Finished %d valid_segments!' % dir_to_msgs.count())
+
+        data_rdd = (dir_to_msgs
+                    # ((dir,time_stamp_per_min), proto)
+                    .mapValues(record_utils.message_to_proto)
+                    # ((dir,time_stamp_per_min), (chassis_proto or pose_proto))
+                    .combineByKey(feature_extraction_utils.to_list,
+                                  feature_extraction_utils.append, feature_extraction_utils.extend)
+                    # -> (key, (chassis_proto_list, pose_proto_list))
+                    .mapValues(feature_extraction_utils.process_seg)
                     # ((folder,time/min),(chassis,pose))
-                    .mapValues(CommonFE.pair_cs_pose)
-                    # ((folder,time/min),feature_matrix)
-                    # feature generator
-                    .mapValues(commonCalTab.feature_generate)
-                    # process feature: feature filter
-                    .mapValues(commonCalTab.feature_filter)
-                    # process feature: feature cut
-                    .mapValues(commonCalTab.feature_cut)
-                    # process feature: feature distribute
-                    .mapValues(commonCalTab.feature_distribute)
-                    # process feature: feature store
-                    .mapValues(commonCalTab.feature_store)
-                    # write features to hdf5 files
-                    .map(commonCalTab.write_h5_train_test))
+                    .mapValues(feature_extraction_utils.pair_cs_pose))
 
-        print data_rdd.count()
+        # ((folder,time/min),feature_matrix)
+        calibration_table_rdd = (data_rdd
+                                 # feature generator
+                                 .mapValues(calibration_table_utils.feature_generate)
+                                 # process feature: feature filter
+                                 .mapValues(calibration_table_utils.feature_filter)
+                                 # process feature: feature cut
+                                 .mapValues(calibration_table_utils.feature_cut)
+                                 # process feature: feature distribute
+                                 .mapValues(calibration_table_utils.feature_distribute)
+                                 # process feature: feature store
+                                 .mapValues(calibration_table_utils.feature_store)
+                                 # write features to hdf5 files
+                                 .map(lambda elem: calibration_table_utils.write_h5_train_test
+                                      (elem, origin_prefix, target_prefix, WANTED_VEHICLE)))
+
+        glog.info('Finished %d calibration_table_rdd!' %
+                  calibration_table_rdd.count())
 
 
 if __name__ == '__main__':
-    CalTabFeatureExtPipeline().run_test()
+    CalTabFeatureExt().run_test()
