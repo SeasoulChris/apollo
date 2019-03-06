9a6a93ed094b75297063b3d2ceed0b6bef5f46cd
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-03-05 17:05:53 -0800

    Simple code clean. (#124)

--- fueling/control/feature_extraction/sample-set-feature-extraction.py
+++ fueling/control/feature_extraction/sample-set-feature-extraction.py
@@ -7,11 +7,11 @@ from collections import Counter
 import operator
 import os
 
-import glog
 import glob
 import pyspark_utils.op as spark_op
 
 from fueling.common.base_pipeline import BasePipeline
+import fueling.common.colored_glog as glog
 import fueling.common.record_utils as record_utils
 import fueling.common.s3_utils as s3_utils
 import fueling.control.features.feature_extraction_utils as feature_extraction_utils
@@ -29,15 +29,15 @@ class SampleSetFeatureExtraction(BasePipeline):
 
     def run_test(self):
         """Run test."""
-        records = ["modules/data/fuel/testdata/control/left_40_10/1.record.00000",
-                   "modules/data/fuel/testdata/control/transit/1.record.00000"
-                   ]
+        records = [
+            "modules/data/fuel/testdata/control/left_40_10/1.record.00000",
+            "modules/data/fuel/testdata/control/transit/1.record.00000",
+        ]
 
         origin_prefix = 'modules/data/fuel/testdata/control'
         target_prefix = 'modules/data/fuel/testdata/control/generated'
         root_dir = '/apollo'
-        dir_to_records = self.get_spark_context().parallelize(
-            records).keyBy(os.path.dirname)
+        dir_to_records = self.get_spark_context().parallelize(records).keyBy(os.path.dirname)
 
         self.run(dir_to_records, origin_prefix, target_prefix, root_dir)
 
@@ -49,10 +49,8 @@ class SampleSetFeatureExtraction(BasePipeline):
         root_dir = s3_utils.S3_MOUNT_PATH
 
         files = s3_utils.list_files(bucket, origin_prefix).cache()
-        complete_dirs = files.filter(
-            lambda path: path.endswith('/COMPLETE')).map(os.path.dirname)
-        dir_to_records = files.filter(
-            record_utils.is_record_file).keyBy(os.path.dirname)
+        complete_dirs = files.filter(lambda path: path.endswith('/COMPLETE')).map(os.path.dirname)
+        dir_to_records = files.filter(record_utils.is_record_file).keyBy(os.path.dirname)
         root_dir = s3_utils.S3_MOUNT_PATH
         self.run(spark_op.filter_keys(dir_to_records, complete_dirs),
                  origin_prefix, target_prefix, root_dir)
@@ -72,11 +70,9 @@ class SampleSetFeatureExtraction(BasePipeline):
             .keys())
 
         glog.info('Finished %d selected_vehicles!' % selected_vehicles.count())
-        glog.info('First elem in selected_vehicles is : %s ' %
-                  selected_vehicles.first())
+        glog.info('First elem in selected_vehicles is : %s ' % selected_vehicles.first())
 
-        channels = {record_utils.CHASSIS_CHANNEL,
-                    record_utils.LOCALIZATION_CHANNEL}
+        channels = {record_utils.CHASSIS_CHANNEL, record_utils.LOCALIZATION_CHANNEL}
         dir_to_msgs = (
             spark_op.filter_keys(dir_to_records, selected_vehicles)
             # -> (dir, msg)
@@ -93,7 +89,7 @@ class SampleSetFeatureExtraction(BasePipeline):
             .reduceByKey(operator.add)
             # -> (dir_segment, topic_counter)
             .filter(spark_op.filter_value(
-                    lambda counter:
+                lambda counter:
                     counter.get(record_utils.CHASSIS_CHANNEL, 0) >= MIN_MSG_PER_SEGMENT and
                     counter.get(record_utils.LOCALIZATION_CHANNEL, 0) >= MIN_MSG_PER_SEGMENT))
             # -> dir_segment
@@ -109,7 +105,8 @@ class SampleSetFeatureExtraction(BasePipeline):
             # ((dir,time_stamp_per_min), proto)
             .mapValues(record_utils.message_to_proto)
             # ((dir,time_stamp_per_min), (chassis_proto or pose_proto))
-            .combineByKey(feature_extraction_utils.to_list, feature_extraction_utils.append, feature_extraction_utils.extend)
+            .combineByKey(feature_extraction_utils.to_list, feature_extraction_utils.append,
+                          feature_extraction_utils.extend)
             # -> (key, (chassis_proto_list, pose_proto_list))
             .mapValues(feature_extraction_utils.process_seg)
             # ->(key,  (paired_pose_chassis))
@@ -126,18 +123,19 @@ class SampleSetFeatureExtraction(BasePipeline):
             # -> ((dir,feature_key),(time_stamp_sec,data_point))
             .map(feature_extraction_utils.feature_key_value)
             # -> ((dir,feature_key), list of (time_stamp_sec,data_point))
-            .combineByKey(feature_extraction_utils.to_list, feature_extraction_utils.append, feature_extraction_utils.extend))
+            .combineByKey(feature_extraction_utils.to_list, feature_extraction_utils.append,
+                          feature_extraction_utils.extend))
 
         glog.info('Finished %d featured_data_rdd!' % featured_data_rdd.count())
 
-        data_segment_rdd = (featured_data_rdd
-                            # generate segment w.r.t feature keys
-                            .mapValues(feature_extraction_utils.gen_segment)
-                            # write all segment into a hdf5 file
-                            .map(lambda elem:
-                                 feature_extraction_utils.write_h5_with_key(elem, origin_prefix, target_prefix, WANTED_VEHICLE)))
-        glog.info('Finished %d data_segment_rdd!' %
-                  data_segment_rdd.count())
+        data_segment_rdd = (
+            featured_data_rdd
+            # generate segment w.r.t feature keys
+            .mapValues(feature_extraction_utils.gen_segment)
+            # write all segment into a hdf5 file
+            .map(lambda elem: feature_extraction_utils.write_h5_with_key(
+                elem, origin_prefix, target_prefix, WANTED_VEHICLE)))
+        glog.info('Finished %d data_segment_rdd!' % data_segment_rdd.count())
 
 
 if __name__ == '__main__':
