b8981c0d694730630bb61fcc6ede1cda11ff12e0
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-03-11 21:52:02 -0700

    Draft dynamic-model workflow. (#149)

--- fueling/control/dynamic-model.sh
+++ fueling/control/dynamic-model.sh
@@ -10,9 +10,18 @@ set -x
 # Feature extraction.
 JOB="fueling/control/feature_extraction/sample-set-feature-extraction.py"
 ENV="fuel-py27-cyber"
-./tools/submit-job-to-k8s.sh --job "${JOB}" --env "${ENV}"
+./tools/submit-job-to-k8s.sh --job "${JOB}" --env "${ENV}" \
+    --workers 15 --worker_cpu 2 --worker_memory 24g
 
-# Training and evaluation
-JOB="fueling/control/dynamic-model.py"
+# Training.
+# TODO(jiaxuan): Change the job name accordingly.
+JOB="fueling/control/dynamic-model-training.py"
 ENV="fuel-py27"
-./tools/submit-job-to-k8s.sh --job "${JOB}" --env "${ENV}"
+./tools/submit-job-to-k8s.sh --job "${JOB}" --env "${ENV}" \
+    --workers 1 --worker_cpu 24 --worker_memory 200g
+
+# Evaluation
+JOB="fueling/control/dynamic-model-evaluation.py"
+ENV="fuel-py27"
+./tools/submit-job-to-k8s.sh --job "${JOB}" --env "${ENV}" \
+    --workers 15 --worker_cpu 2 --worker_memory 24g
