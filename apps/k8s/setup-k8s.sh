ec61c1490d6c3e034f3613149223723fc261ec8f
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-03-28 15:12:52 -0700

    Move setup-k8s tool to k8s folder. (#262)

--- apps/k8s/setup-k8s.sh
+++ apps/k8s/setup-k8s.sh
@@ -22,5 +22,5 @@ kubectl create secret docker-registry "baidubce" \
     --docker-password="${DOCKER_PASSWORD}" \
     --docker-email="xiaoxiangquan@baidu.com"
 
-SECRET_YAML_FILE="$( dirname "${BASH_SOURCE[0]}" )/../apps/k8s/secrets.yaml"
-kubectl apply -f "${SECRET_YAML_FILE}" 
+# Add secrets.
+kubectl apply -f secrets.yaml
