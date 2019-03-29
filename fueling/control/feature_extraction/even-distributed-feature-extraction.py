d71a0c3cc6fbf2622137aa7933bf7e8316a6638d
Shu Jiang <shujiang2016@gmail.com>
2019-03-29 11:14:08 -0700

    control: add prod test to evenly distributed sample set

--- fueling/control/feature_extraction/even-distributed-feature-extraction.py
+++ fueling/control/feature_extraction/even-distributed-feature-extraction.py
@@ -5,6 +5,7 @@ import os
 import fueling.common.colored_glog as glog
 from fueling.common.base_pipeline import BasePipeline
 import fueling.common.h5_utils as h5_utils
+import fueling.common.s3_utils as s3_utils
 import fueling.control.features.dir_utils as dir_utils
 import fueling.control.features.feature_extraction_utils as feature_extraction_utils
 
@@ -22,14 +23,14 @@ def get_key(file_name):
 def pick_sample(list_of_segment):
     counter = 0
     sample_list = []
-    for i in range(len(list_of_segment)):
-        add_size = list_of_segment[i].shape[0]
+    for segment in list_of_segment:
+        add_size = segment.shape[0]
         if counter + add_size < sample_size:
-            counter += list_of_segment[i].shape[0] # row, data points
-            sample_list.append(list_of_segment[i])
+            counter += segment.shape[0] # row, data points
+            sample_list.append(segment)
         elif counter < sample_size:
             to_add_size = sample_size-counter+1
-            sample_list.append(list_of_segment[i][0:to_add_size,:])
+            sample_list.append(segment[0:to_add_size,:])
             return sample_list
     return sample_list
 
@@ -37,12 +38,11 @@ def write_to_file(target_prefix, elem):
     key, list_of_segment = elem
     total_number = len(list_of_segment)
     file_dir = os.path.join(target_prefix,key)
-    print(file_dir)
-    for i in range(total_number):
-        data = list_of_segment[i]
-        file_name = str(i+1).zfill(4) + "_of_" + str(total_number).zfill(4)
-        print(file_name)
+    counter = 1
+    for data in list_of_segment:
+        file_name = str(counter).zfill(4) + "_of_" + str(total_number).zfill(4)
         h5_utils.write_h5_single_segment(data, file_dir, file_name)
+        counter += 1
     return total_number
             
 class EvenDistributedFeatureExtraction(BasePipeline):
@@ -59,20 +59,29 @@ class EvenDistributedFeatureExtraction(BasePipeline):
                                      WANTED_VEHICLE, 'EvenlyDitributed')
         root_dir = '/apollo'
         files_dir = os.path.join(root_dir, origin_prefix)
-        target_prefix = os.path.join(root_dir, target_prefix)
+        target_dir = os.path.join(root_dir, target_prefix)
         glog.info(files_dir)
         todo_tasks = (
             #RDD(all files)
             self.get_spark_context().parallelize(dir_utils.list_end_files(files_dir))
             #RDD(.hdf5 files)
             .filter(lambda path: path.endswith('.hdf5')))
-        glog.info(todo_tasks.first())
-        self.run(todo_tasks, origin_prefix, target_prefix)
+        glog.info('NUMBER of TODO TASK: %d', todo_tasks.first())
+        self.run(todo_tasks, target_dir)
     
     def run_prod(self):
         """Run prod."""
+        bucket = 'apollo-platform'
+        # same of target prefix of sample-set-feature-extraction
+        origin_prefix = os.path.join('modules/control/feature_extraction_hf5/hdf5_training/',
+                                     WANTED_VEHICLE, 'SampleSet')
+        target_prefix = os.path.join('modules/control/feature_extraction_hf5/hdf5_training/',
+                                     WANTED_VEHICLE, 'EvenlyDistributedSampleSet')
+        root_dir = s3_utils.S3_MOUNT_PATH
+        list_func = (lambda path: s3_utils.list_files(bucket, path))
+
     
-    def run(self, todo_tasks, origin_prefix, target_prefix):
+    def run(self, todo_tasks, target_prefix):
         categorized_segments = (
             # RDD(.hdf5 files with absolute path)
             todo_tasks
@@ -94,11 +103,11 @@ class EvenDistributedFeatureExtraction(BasePipeline):
             categorized_segments
             # PairedRDD(key, sampled segments)
             .mapValues(pick_sample)
-            # dRDD(segment_length)
+            # RDD(segment_length)
             .map(lambda elem: write_to_file(target_prefix, elem))
         )
 
-        testdata = sampled_segments.count()
+        glog.info('Generated %d categories', sampled_segments.count())
 
 if __name__ == '__main__':
     EvenDistributedFeatureExtraction().run_test()
