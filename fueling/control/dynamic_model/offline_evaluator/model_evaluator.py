b6b7d03a7bb6e2b84320c7060fb871853ba1cb03
Jiaxuan Xu <jx.jason.xu@gmail.com>
2019-04-02 18:15:25 -0700

    Dynamic model: refactor evaluation results printing with proto

--- fueling/control/dynamic_model/offline_evaluator/model_evaluator.py
+++ fueling/control/dynamic_model/offline_evaluator/model_evaluator.py
@@ -11,6 +11,7 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 
 from fueling.control.dynamic_model.conf.model_config import feature_config
+from modules.data.fuel.fueling.control.proto.dynamic_model_evaluation_pb2 import EvaluationResults
 import fueling.common.colored_glog as glog
 import fueling.control.dynamic_model.data_generator.data_generator as data_generator
 
@@ -39,41 +40,27 @@ DIM_LSTM_LENGTH = feature_config["sequence_length"]
 DELTA_T = feature_config["delta_t"]
 
 
-def plot_model_output(output_imu, output_fnn, output_point_mass, txt, *pdf):
+def evaluate_direct_output(output_imu, output_fnn, output_point_mass, evaluation_results):
 
-    # plt.figure(figsize=(3,2))
-    # plt.title("Acceleration")
-    # plt.plot(output_imu[:,0], color = 'blue',label = "IMU acceleration")
-    # plt.plot(output_fnn[:,0], color = 'red', label = "Acceleration by learning-based-model")
-    # plt.plot(output_point_mass[:,0], color = 'green',label = "Acceleration by sim_point_mass")
     rmse_fnn_acceleration = sqrt(mean_squared_error(output_imu[:, 0], output_fnn[:, 0]))
     rmse_point_mass_acceleration = sqrt(mean_squared_error(output_imu[:, 0], output_point_mass[:, 0]))
     rms_acceleration = sqrt(sum(n * n for n in output_imu[:, 0]) / len(output_imu[:, 0]))
-    txt.write("Acceleration rmse of learning-based-model: {}, Error Rate: {}".format(
-        rmse_fnn_acceleration, rmse_fnn_acceleration / rms_acceleration))
-    txt.write("\n")
-    txt.write("Acceleration rmse of sim_point_mass: {}, Error Rate: {}".format(
-        rmse_point_mass_acceleration, rmse_point_mass_acceleration / rms_acceleration))
-    txt.write("\n")
-    # pdf.savefig()  # saves the current figure into a pdf page
-    # plt.close()
+    evaluation_results.learning_based_result.acceleration_error = rmse_fnn_acceleration
+    evaluation_results.learning_based_result.acceleration_error_rate = \
+                                             rmse_fnn_acceleration / rms_acceleration
+    evaluation_results.point_mass_result.acceleration_error = rmse_point_mass_acceleration
+    evaluation_results.point_mass_result.acceleration_error_rate = \
+                                             rmse_point_mass_acceleration / rms_acceleration
 
-    # plt.figure(figsize=(3,2))
-    # plt.title("Angular speed")
-    # plt.plot(output_imu[:,1], color = 'blue',label = "IMU angular speed")
-    # plt.plot(output_fnn[:,1], color = 'red', label = "Angular speed by learning-based-model")
-    # plt.plot(output_point_mass[:,1], color = 'green',label = "Angular speed by sim_point_mass")
     rmse_fnn_angular_speed = sqrt(mean_squared_error(output_imu[:, 1], output_fnn[:, 1]))
     rmse_point_mass_angular_speed = sqrt(mean_squared_error(output_imu[:, 1], output_point_mass[:, 1]))
     rms_angular_speed = sqrt(sum(n * n for n in output_imu[:, 1]) / len(output_imu[:, 1]))
-    txt.write("Angular speed rmse of learning-based-model: {}, Error Rate: {}".format(
-        rmse_fnn_angular_speed, rmse_fnn_angular_speed / rms_angular_speed))
-    txt.write("\n")
-    txt.write("Angular speed rmse of sim_point_mass: {}, Error Rate: {}".format(
-        rmse_point_mass_angular_speed, rmse_point_mass_angular_speed / rms_angular_speed))
-    txt.write("\n")
-    # pdf.savefig()  # saves the current figure into a pdf page
-    # plt.close()
+    evaluation_results.learning_based_result.angular_speed_error = rmse_fnn_angular_speed
+    evaluation_results.learning_based_result.angular_speed_error_rate = \
+                                            rmse_fnn_angular_speed / rms_angular_speed
+    evaluation_results.point_mass_result.angular_speed_error = rmse_point_mass_angular_speed
+    evaluation_results.point_mass_result.angular_speed_error_rate = \
+                                            rmse_point_mass_angular_speed / rms_angular_speed
 
 
 def normalize_angle(theta):
@@ -83,7 +70,8 @@ def normalize_angle(theta):
     return theta
 
 
-def plot_first_integral(vehicle_state_gps, output_imu, output_fnn, output_point_mass, txt, *pdf):
+def evaluate_vehicle_state(vehicle_state_gps, output_imu, output_fnn, output_point_mass,
+                            evaluation_results):
     vehicle_state_imu = np.zeros([vehicle_state_gps.shape[0], vehicle_state_gps.shape[1]])
     vehicle_state_fnn = np.zeros([vehicle_state_gps.shape[0], vehicle_state_gps.shape[1]])
     vehicle_state_point_mass = np.zeros([vehicle_state_gps.shape[0], vehicle_state_gps.shape[1]])
@@ -92,40 +80,29 @@ def plot_first_integral(vehicle_state_gps, output_imu, output_fnn, output_point_
     vehicle_state_point_mass[0, :] = vehicle_state_gps[0, :]
 
     for k in range(1, vehicle_state_gps.shape[0]):
-        vehicle_state_imu[k, :] = vehicle_state_imu[k - 1, :] + \
-                                    output_imu[k, :] * DELTA_T  # by imu sensor
+        # vehicle states by imu sensor
+        vehicle_state_imu[k, :] = vehicle_state_imu[k - 1, :] + output_imu[k, :] * DELTA_T
         vehicle_state_imu[k, 1] = normalize_angle(vehicle_state_imu[k, 1])
-        vehicle_state_fnn[k, :] = vehicle_state_fnn[k - 1, :] + \
-                                    output_fnn[k, :] * DELTA_T  # by learning-based-model
+        # vehicle states by learning-based-model
+        vehicle_state_fnn[k, :] = vehicle_state_fnn[k - 1, :] + output_fnn[k, :] * DELTA_T
         vehicle_state_fnn[k, 1] = normalize_angle(vehicle_state_fnn[k, 1])
+        # vehicle states by sim_point_mass
         vehicle_state_point_mass[k, :] = vehicle_state_point_mass[k - 1, :] + \
-                                            output_point_mass[k, :] * DELTA_T  # by sim_point_mass
+                                            output_point_mass[k, :] * DELTA_T
         vehicle_state_point_mass[k, 1] = normalize_angle(vehicle_state_point_mass[k, 1])
 
     rmse_imu_speed = sqrt(mean_squared_error(vehicle_state_imu[:, 0], vehicle_state_gps[:, 0]))
     rmse_fnn_speed = sqrt(mean_squared_error(vehicle_state_fnn[:, 0], vehicle_state_gps[:, 0]))
-    rmse_point_mass_speed = sqrt(mean_squared_error(vehicle_state_point_mass[:, 0], 
-                                                        vehicle_state_gps[:, 0]))
+    rmse_point_mass_speed = sqrt(mean_squared_error(vehicle_state_point_mass[:, 0],
+                                                             vehicle_state_gps[:, 0]))
     rms_speed = sqrt(sum(n * n for n in vehicle_state_gps[:, 0]) / len(vehicle_state_gps[:, 0]))
 
-    # plt.figure(figsize=(3,2))
-    # plt.title("Speed")
-    # plt.plot(vehicle_state_gps[:,0], color = 'blue',label = "Ground-truth speed")
-    # plt.plot(vehicle_state_imu[:,0], color = 'orange',label = "Incremental speed by sensors")
-    # plt.plot(vehicle_state_fnn[:,0], color = 'red', label = "Speed by learning-based-model")
-    # plt.plot(vehicle_state_point_mass[:,0], color = 'green',label = "Speed by sim_point_mass")
-
-    txt.write("Speed rmse of sensor: {}, Error Rate: {}".format(
-        rmse_imu_speed, rmse_imu_speed / rms_speed))
-    txt.write("\n")
-    txt.write("Speed rmse of learning-based-model: {}, Error Rate: {}".format(
-        rmse_fnn_speed, rmse_fnn_speed / rms_speed))
-    txt.write("\n")
-    txt.write("Speed rmse of sim_point_mass: {}, Error Rate: {}".format(
-        rmse_point_mass_speed, rmse_point_mass_speed / rms_speed))
-    txt.write("\n")
-    # pdf.savefig()  # saves the current figure into a pdf page
-    # plt.close()
+    evaluation_results.sensor_error.speed_error = rmse_imu_speed
+    evaluation_results.sensor_error.speed_error_rate = rmse_imu_speed / rms_speed
+    evaluation_results.learning_based_result.speed_error = rmse_fnn_speed
+    evaluation_results.learning_based_result.speed_error_rate = rmse_fnn_speed / rms_speed
+    evaluation_results.point_mass_result.speed_error = rmse_point_mass_speed
+    evaluation_results.point_mass_result.speed_error_rate = rmse_point_mass_speed / rms_speed
 
     rmse_imu_heading = sqrt(mean_squared_error(vehicle_state_imu[:, 1], vehicle_state_gps[:, 1]))
     rmse_fnn_heading = sqrt(mean_squared_error(vehicle_state_fnn[:, 1], vehicle_state_gps[:, 1]))
@@ -133,29 +110,18 @@ def plot_first_integral(vehicle_state_gps, output_imu, output_fnn, output_point_
                                     vehicle_state_gps[:, 1]))
     rms_heading = sqrt(sum(n * n for n in vehicle_state_gps[:, 1]) / len(vehicle_state_gps[:, 1]))
 
-    # plt.figure(figsize=(3,2))
-    # plt.title("Heading")
-    # plt.plot(vehicle_state_gps[:,1], color = 'blue', label = "Ground-truth heading")
-    # plt.plot(vehicle_state_imu[:,1], color = 'orange', label = "Incremental heading by angular speed")
-    # plt.plot(vehicle_state_fnn[:,1], color = 'red', label = "Heading by learning-based-model")
-    # plt.plot(vehicle_state_point_mass[:,1], color = 'green', label = "Heading by sim_point_mass")
+    evaluation_results.sensor_error.speed_error = rmse_imu_heading
+    evaluation_results.sensor_error.speed_error_rate = rmse_imu_heading / rms_heading
+    evaluation_results.learning_based_result.speed_error = rmse_fnn_heading
+    evaluation_results.learning_based_result.speed_error_rate = rmse_fnn_heading / rms_heading
+    evaluation_results.point_mass_result.speed_error = rmse_point_mass_heading
+    evaluation_results.point_mass_result.speed_error_rate = rmse_point_mass_heading / rms_heading
 
-    txt.write("Heading rmse of sensor: {}, Error Rate: {}".format(
-        rmse_imu_heading, rmse_imu_heading / rms_heading))
-    txt.write("\n")
-    txt.write("Heading rmse of learning-based-model: {}, Error Rate: {}".format(
-        rmse_fnn_heading, rmse_fnn_heading / rms_heading))
-    txt.write("\n")
-    txt.write("Heading rmse of sim_point_mass: {}, Error Rate: {}".format(
-        rmse_point_mass_heading, rmse_point_mass_heading / rms_heading))
-    txt.write("\n")
-    # pdf.savefig()  # saves the current figure into a pdf page
-    # plt.close()
     return vehicle_state_imu, vehicle_state_fnn, vehicle_state_point_mass
 
 
-def plot_trajectory(trajectory_gps, 
-                    vehicle_state_imu, vehicle_state_fnn, vehicle_state_point_mass, txt, *pdf):
+def evaluate_trajectory(trajectory_gps, vehicle_state_imu, vehicle_state_fnn, 
+                            vehicle_state_point_mass, evaluation_results):
     trajectory_imu = np.zeros([trajectory_gps.shape[0], trajectory_gps.shape[1]])
     trajectory_fnn = np.zeros([trajectory_gps.shape[0], trajectory_gps.shape[1]])
     trajectory_point_mass = np.zeros([trajectory_gps.shape[0], trajectory_gps.shape[1]])
@@ -180,32 +146,19 @@ def plot_trajectory(trajectory_gps,
         Trajectory_length += np.sqrt((trajectory_gps[k, 0] - trajectory_gps[k - 1, 0]) ** 2 + (
                                         trajectory_gps[k, 1] - trajectory_gps[k - 1, 1]) ** 2)
 
-    # fig = plt.figure(figsize=(3,2))
-    # plt.title("Trajectory")
-    # plt.plot(trajectory_gps[:,0], trajectory_gps[:,1], color = 'blue', label = "Ground-truth Tracjectory")
-    # plt.plot(trajectory_gps[-1,0], trajectory_gps[-1,1], color = 'blue', marker = 'x')
-    # plt.plot(trajectory_imu[:,0], trajectory_imu[:,1], color = 'orange', label = "Generated Tracjectory by IMU")
-    # plt.plot(trajectory_imu[-1,0], trajectory_imu[-1,1], color = 'orange', marker = 'x')
-    # plt.plot(trajectory_fnn[:,0], trajectory_fnn[:,1], color = 'red', label = "Tracjectory by learning-based-model")
-    # plt.plot(trajectory_fnn[-1,0], trajectory_fnn[-1,1],color = 'red', marker = 'x')
-    # plt.plot(trajectory_point_mass[:,0], trajectory_point_mass[:,1], color = 'green',label = "Tracjectory by sim_point_mass")
-    # plt.plot(trajectory_point_mass[-1,0], trajectory_point_mass[-1,1], color = 'green', marker = 'x')
-
     rmse_imu_trajectory = sqrt(mean_squared_error(trajectory_imu, trajectory_gps))
     rmse_fnn_trajectory = sqrt(mean_squared_error(trajectory_fnn, trajectory_gps))
     rmse_point_mass_trajectory = sqrt(mean_squared_error(trajectory_point_mass, trajectory_gps))
 
-    txt.write("Trajectory rmse of sensor: {}, Error Rate: {}".format(
-        rmse_imu_trajectory, rmse_imu_trajectory / Trajectory_length))
-    txt.write("\n")
-    txt.write("Trajectory rmse of learning-based-model: {}, Error Rate: {}".format(
-        rmse_fnn_trajectory, rmse_fnn_trajectory / Trajectory_length))
-    txt.write("\n")
-    txt.write("Trajectory rmse of sim_point_mass: {}, Error Rate: {}".format(
-        rmse_point_mass_trajectory, rmse_point_mass_trajectory / Trajectory_length))
-    txt.write("\n")
-    # pdf.savefig()  # saves the current figure into a pdf page
-    # plt.close()
+    evaluation_results.sensor_error.trajectory_error = rmse_imu_trajectory
+    evaluation_results.sensor_error.trajectory_error_rate = \
+                                            rmse_imu_trajectory / Trajectory_length
+    evaluation_results.learning_based_result.trajectory_error = rmse_fnn_trajectory
+    evaluation_results.learning_based_result.trajectory_error_rate = \
+                                            rmse_fnn_trajectory / Trajectory_length
+    evaluation_results.point_mass_result.trajectory_error = rmse_point_mass_trajectory
+    evaluation_results.point_mass_result.trajectory_error_rate = \
+                                            rmse_point_mass_trajectory / Trajectory_length
 
 
 def evaluate(model_info, dataset_path, platform_path):
@@ -217,14 +170,16 @@ def evaluate(model_info, dataset_path, platform_path):
                     data_generator.generate_evaluation_data(dataset_path, model_info[1], 'lstm')
     else:
         return
-
+    evaluation_results = EvaluationResults()
     evaluation_result_path = os.path.join(platform_path, 
         'evaluation_result/evaluation_metrics_for_' + model_info[0] + '_model.txt')
-    with open(evaluation_result_path, 'a') as txt:
-        txt.write('\n evaluted on model: {} \n'.format(model_info[1]))
-        txt.write('evaluted on record: {} \n'.format(dataset_path))
-        plot_model_output(output_imu, output_fnn, output_point_mass, txt)
-        vehicle_state_imu, vehicle_state_fnn, vehicle_state_point_mass = plot_first_integral(
-            vehicle_state_gps, output_imu, output_fnn, output_point_mass, txt)
-        plot_trajectory(trajectory_gps, vehicle_state_imu, 
-                            vehicle_state_fnn, vehicle_state_point_mass, txt)
+    
+    with open(evaluation_result_path, 'a') as txt_file:
+        txt_file.write('\n evaluted on model: {} \n'.format(model_info[1]))
+        txt_file.write('evaluted on record: {} \n'.format(dataset_path))
+        evaluate_direct_output(output_imu, output_fnn, output_point_mass, evaluation_results)
+        vehicle_state_imu, vehicle_state_fnn, vehicle_state_point_mass = evaluate_vehicle_state(
+            vehicle_state_gps, output_imu, output_fnn, output_point_mass, evaluation_results)
+        evaluate_trajectory(trajectory_gps, vehicle_state_imu, 
+                            vehicle_state_fnn, vehicle_state_point_mass, evaluation_results)
+        txt_file.write(evaluation_results.SerializeToString())
