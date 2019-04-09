475a451e1dd0c66a95772b4902af9d32124f3484
Shu Jiang <shujiang2016@gmail.com>
2019-04-09 14:38:21 -0700

    control: calibration table feature extraction, get conf file from data folder

--- fueling/control/calibration_table/calibration-table-feature-extraction.py
+++ fueling/control/calibration_table/calibration-table-feature-extraction.py
@@ -8,23 +8,35 @@ import os
 import colored_glog as glog
 import pyspark_utils.op as spark_op
 
+from modules.common.configs.proto.vehicle_config_pb2 import VehicleParam
 import common.proto_utils as proto_utils
 
 from fueling.common.base_pipeline import BasePipeline
-from fueling.control.features.feature_extraction_rdd_utils import record_to_msgs_rdd
+from fueling.control.features.feature_extraction_utils import gen_pre_segment
+from fueling.control.features.feature_extraction_utils import pair_cs_pose
 from modules.data.fuel.fueling.control.proto.calibration_table_pb2 import CalibrationTable
 import fueling.common.record_utils as record_utils
 import fueling.common.s3_utils as s3_utils
 import fueling.control.features.calibration_table_utils as calibration_table_utils
 import fueling.control.features.dir_utils as dir_utils
 import fueling.control.features.feature_extraction_rdd_utils as feature_extraction_rdd_utils
-import fueling.control.features.feature_extraction_utils as feature_extraction_utils
 
 channels = {record_utils.CHASSIS_CHANNEL, record_utils.LOCALIZATION_CHANNEL}
-WANTED_VEHICLE = calibration_table_utils.CALIBRATION_TABLE_CONF.vehicle_type
-MIN_MSG_PER_SEGMENT = 1
+MIN_MSG_PER_SEGMENT = 10
 MARKER = 'CompleteCalibrationTable'
 
+def get_single_vehicle_type(data_folder):
+    sub_folders = os.listdir(data_folder)
+    for one_folder in sub_folders:
+        vehicle_type = one_folder.rsplit('/')
+    return vehicle_type[0]
+
+def get_vehicle_param(folder_dir, vehicle_type):
+    # CONF_FOLDER = '/apollo/modules/data/fuel/testdata/control/sourceData/OUT'
+    vehicle_para_conf_filename = 'vehicle_param.pb.txt'
+    conf_file = os.path.join(folder_dir, vehicle_type, vehicle_para_conf_filename)
+    VEHICLE_PARAM_CONF = proto_utils.get_pb_from_text_file(conf_file, VehicleParam())
+    return VEHICLE_PARAM_CONF
 
 class CalibrationTableFeatureExtraction(BasePipeline):
     def __init__(self):
@@ -33,12 +45,17 @@ class CalibrationTableFeatureExtraction(BasePipeline):
 
     def run_test(self):
         """Run test."""
-        origin_prefix = 'modules/data/fuel/testdata/control/sourceData'
+        root_dir = '/apollo'
+        origin_prefix = 'modules/data/fuel/testdata/control/sourceData/OUT'
+        origin_dir = os.path.join(root_dir,origin_prefix)
+        WANTED_VEHICLE = get_single_vehicle_type(origin_dir)
+        VEHICLE_PARAM_CONF = get_vehicle_param(origin_dir, WANTED_VEHICLE)
+      
         target_prefix = os.path.join('modules/data/fuel/testdata/control/generated',
                                      WANTED_VEHICLE, 'CalibrationTable')
+                
         throttle_train_target_prefix = os.path.join(target_prefix, 'throttle', 'train')
-        root_dir = '/apollo'
-
+    
         list_func = (lambda path: self.get_spark_context().parallelize(
             dir_utils.list_end_files(os.path.join(root_dir, path))))
         # RDD(record_dir)
@@ -60,7 +77,8 @@ class CalibrationTableFeatureExtraction(BasePipeline):
 
         glog.info('todo_files: {}'.format(dir_to_records.collect()))
 
-        self.run(dir_to_records, origin_prefix, target_prefix, throttle_train_target_prefix)
+        self.run(dir_to_records, origin_prefix, target_prefix, 
+            throttle_train_target_prefix, VEHICLE_PARAM_CONF)
 
     def run_prod(self):
         """Run prod."""
@@ -75,14 +93,24 @@ class CalibrationTableFeatureExtraction(BasePipeline):
                                                      MARKER)
         self.run(todo_records, origin_prefix, target_prefix, throttle_train_target_prefix)
 
-    def run(self, dir_to_records_rdd, origin_prefix, target_prefix, throttle_train_target_prefix):
+    def run(self, dir_to_records_rdd, origin_prefix, target_prefix, 
+            throttle_train_target_prefix, VEHICLE_PARAM_CONF):
         """ processing RDD """
-        # PairRDD((dir_segment, segment_id), (chassis_msg_list, pose_msg_list))
-        valid_msgs = (feature_extraction_rdd_utils
-                      .record_to_msgs_rdd(dir_to_records_rdd,
-                                          WANTED_VEHICLE, channels, MIN_MSG_PER_SEGMENT, MARKER)
+        # PairRDD((dir, timestamp_per_min), msg)
+        dir_to_msgs = (dir_to_records_rdd
+                      # PairRDD(dir, msg)
+                      .flatMapValues(record_utils.read_record(channels))
+                      # PairRDD((dir, timestamp_per_min), msg)
+                      .map(gen_pre_segment)
                       .cache())
 
+        # RDD(dir, timestamp_per_min)
+        valid_segments = (feature_extraction_rdd_utils.
+                            chassis_localization_segment_rdd(dir_to_msgs, MIN_MSG_PER_SEGMENT))
+
+        # PairRDD((dir_segment, segment_id), msg) 
+        valid_msgs = feature_extraction_rdd_utils.valid_msg_rdd(dir_to_msgs, valid_segments)
+ 
         # PairRDD((dir_segment, segment_id), (chassis_list, pose_list))
         parsed_msgs = feature_extraction_rdd_utils.chassis_localization_parsed_msg_rdd(valid_msgs)
 
@@ -90,20 +118,24 @@ class CalibrationTableFeatureExtraction(BasePipeline):
             # PairRDD((dir_segment, segment_id), (chassis_msg_list, pose_msg_list))
             parsed_msgs
             # PairRDD((dir_segment, segment_id), paired_chassis_msg_pose_msg)
-            .mapValues(feature_extraction_utils.pair_cs_pose)
+            .mapValues(pair_cs_pose)
             # PairRDD((dir_segment, segment_id), features)
-            .mapValues(calibration_table_utils.feature_generate)
+            .mapValues(
+                lambda msgs:calibration_table_utils.feature_generate(msgs, VEHICLE_PARAM_CONF))
             # PairRDD ((dir_segment, segment_id), filtered_features)
             .mapValues(calibration_table_utils.feature_filter)
             # PairRDD ((dir_segment, segment_id), cutted_features)
-            .mapValues(calibration_table_utils.feature_cut)
+            .mapValues(
+                lambda feature: calibration_table_utils.feature_cut(feature, VEHICLE_PARAM_CONF))
             # PairRDD ((dir_segment, segment_id), (grid_dict,cutted_features))
-            .mapValues(calibration_table_utils.feature_distribute)
+            .mapValues(
+                lambda feature: calibration_table_utils.feature_distribute(feature, VEHICLE_PARAM_CONF))
             # PairRDD ((dir_segment, segment_id), one_matrix)
-            .mapValues(calibration_table_utils.feature_store)
+            .mapValues(
+                lambda feature: calibration_table_utils.feature_store(feature, VEHICLE_PARAM_CONF))
             # RDD(feature_numbers)
             .map(lambda elem: calibration_table_utils.write_h5_train_test
-                 (elem, origin_prefix, target_prefix, WANTED_VEHICLE)))
+                 (elem, origin_prefix, target_prefix)))
 
         glog.info('Finished %d calibration_table_rdd!' % calibration_table_rdd.count())
 
--- fueling/control/features/calibration_table_utils.py
+++ fueling/control/features/calibration_table_utils.py
@@ -11,56 +11,67 @@ import numpy as np
 
 import common.proto_utils as proto_utils
 
-
 from fueling.control.features.filters import Filters
-from modules.common.configs.proto import vehicle_config_pb2
+from modules.common.configs.proto.vehicle_config_pb2 import VehicleParam
 from modules.data.fuel.fueling.control.proto.calibration_table_pb2 import CalibrationTable
 import fueling.common.h5_utils as h5_utils
 import fueling.common.file_utils as file_utils
 
-FILENAME_CALIBRATION_TABLE_CONF = \
-    '/apollo/modules/data/fuel/fueling/control/conf/calibration_table_conf.pb.txt'
-CALIBRATION_TABLE_CONF = proto_utils.get_pb_from_text_file(
-    FILENAME_CALIBRATION_TABLE_CONF, CalibrationTable())
 
-# calibration table constant
+
+# # parameters
+# WANTED_VEHICLE = 'Zhongyun'
+# CONF_FOLDER = '/apollo/modules/data/fuel/testdata/control/sourceData/OUT'
+# vehicle_para_conf_filename = 'vehicle_param.pb.txt'
+# conf_file = os.path.join(CONF_FOLDER, WANTED_VEHICLE, vehicle_para_conf_filename)
+# VEHICLE_PARAM_CONF = proto_utils.get_pb_from_text_file(conf_file, VehicleParam())
+
+conf_file_dir = '/apollo/modules/data/fuel/fueling/control/conf'
+conf_filename = 'calibration_table_conf.pb.txt'
+calibration_conf_file = os.path.join(conf_file_dir, conf_filename)
+CALIBRATION_TABLE_CONF = proto_utils.get_pb_from_text_file(
+    calibration_conf_file, CalibrationTable())
+glog.info('Load calibration table conf: %s' % conf_filename)
+
+# calibration table parameters
 steer_condition = CALIBRATION_TABLE_CONF.steer_condition
 curvature_condition = CALIBRATION_TABLE_CONF.curvature_condition
-speed_min_condition = CALIBRATION_TABLE_CONF.speed_min_condition
-speed_max_condition = CALIBRATION_TABLE_CONF.speed_max_condition
-acc_min_condition = CALIBRATION_TABLE_CONF.acc_min
-acc_max_condition = CALIBRATION_TABLE_CONF.acc_max
+
 train_percetage = CALIBRATION_TABLE_CONF.train_percentage
 
-# vehicle param constant
-FILENAME_VEHICLE_PARAM_CONF = '/apollo/modules/common/data/vehicle_param.pb.txt'
-VEHICLE_PARAM_CONF = proto_utils.get_pb_from_text_file(FILENAME_VEHICLE_PARAM_CONF,
-                                            vehicle_config_pb2.VehicleConfig())
-
-THROTTLE_DEADZONE = VEHICLE_PARAM_CONF.vehicle_param.throttle_deadzone
 THROTTLE_MAX = CALIBRATION_TABLE_CONF.throttle_max
-
-BRAKE_DEADZONE = -1 * VEHICLE_PARAM_CONF.vehicle_param.brake_deadzone
 BRAKE_MAX = -1 * CALIBRATION_TABLE_CONF.brake_max
 
+speed_min_condition = CALIBRATION_TABLE_CONF.speed_min_condition
+speed_max_condition = CALIBRATION_TABLE_CONF.speed_max_condition
 
-# transient
-segment_brake_list = np.linspace(
-    BRAKE_MAX, BRAKE_DEADZONE, num=CALIBRATION_TABLE_CONF.brake_segment).tolist()
-segment_throttle_list = np.linspace(
-    THROTTLE_DEADZONE, THROTTLE_MAX, num=CALIBRATION_TABLE_CONF.throttle_segment).tolist()
+segment_store_num = 12
+wanted_driving_mode = "COMPLETE_MANUAL"
+
+# vehicle parameters
+def gen_brake_list(VEHICLE_PARAM_CONF):
+    BRAKE_DEADZONE = -1 * VEHICLE_PARAM_CONF.brake_deadzone
+    return np.linspace(
+        BRAKE_MAX, BRAKE_DEADZONE, num=CALIBRATION_TABLE_CONF.brake_segment).tolist()
+
+def gen_throttle_list(VEHICLE_PARAM_CONF):
+    THROTTLE_DEADZONE = VEHICLE_PARAM_CONF.throttle_deadzone
+    return np.linspace(
+        THROTTLE_DEADZONE, THROTTLE_MAX, num=CALIBRATION_TABLE_CONF.throttle_segment).tolist()
+
+def gen_cmd_list(VEHICLE_PARAM_CONF):
+    segment_brake_list = gen_brake_list(VEHICLE_PARAM_CONF)
+    segment_throttle_list = gen_throttle_list(VEHICLE_PARAM_CONF)
+    return segment_brake_list + segment_throttle_list
+
 
 segment_speed_list = np.linspace(
     CALIBRATION_TABLE_CONF.speed_min, CALIBRATION_TABLE_CONF.speed_max,
     num=CALIBRATION_TABLE_CONF.speed_segment).tolist()
 
-segment_cmd_list = segment_brake_list + segment_throttle_list
-segment_store_num = 12
-
-wanted_driving_mode = "COMPLETE_MANUAL"
-
-
-def decide_cmd(chassis_throttle_val, chassis_brake_val):
+def decide_cmd(chassis_throttle_val, chassis_brake_val, VEHICLE_PARAM_CONF):
+    segment_throttle_list = gen_throttle_list(VEHICLE_PARAM_CONF)
+    segment_brake_list = gen_brake_list(VEHICLE_PARAM_CONF)
     feature_cmd = 0.0
     if chassis_throttle_val > abs(segment_throttle_list[0]):
         feature_cmd = chassis_throttle_val
@@ -71,7 +82,7 @@ def decide_cmd(chassis_throttle_val, chassis_brake_val):
     return feature_cmd
 
 
-def feature_generate(elem):
+def feature_generate(elem, VEHICLE_PARAM_CONF):
     """ extract data from segment """
     res = np.zeros([len(elem), 5])
     for i in range(len(elem)):
@@ -83,7 +94,8 @@ def feature_generate(elem):
         acc_y = pose.linear_acceleration.y
 
         acc = acc_x*math.cos(heading_angle)+acc_y*math.sin(heading_angle)
-        feature_cmd = decide_cmd(chassis.throttle_percentage, chassis.brake_percentage)
+        feature_cmd = decide_cmd(chassis.throttle_percentage, 
+                                 chassis.brake_percentage, VEHICLE_PARAM_CONF)
         driving_mode = (chassis.driving_mode == wanted_driving_mode)
 
         res[i] = np.array([
@@ -116,10 +128,12 @@ def feature_filter(elem, filter_window=20):
     return elem
 
 
-def satisfy_brake_condition(elem, index):
+def satisfy_brake_condition(elem, index, VEHICLE_PARAM_CONF):
     """
     whether satisfy brake condition
     """
+    acc_min_condition = VEHICLE_PARAM_CONF.max_deceleration
+    segment_brake_list = gen_brake_list(VEHICLE_PARAM_CONF)
     condition = abs(elem[index][3]) < steer_condition and \
         elem[index][0] > speed_min_condition and \
         elem[index][0] < speed_max_condition and \
@@ -131,10 +145,12 @@ def satisfy_brake_condition(elem, index):
     return condition
 
 
-def satisfy_throttle_condition(elem, index):
+def satisfy_throttle_condition(elem, index, VEHICLE_PARAM_CONF):
     """
     whether satisfy throttle condition
     """
+    acc_max_condition = VEHICLE_PARAM_CONF.max_acceleration
+    segment_throttle_list = gen_throttle_list(VEHICLE_PARAM_CONF)
     condition = abs(elem[index][3]) < steer_condition and \
         elem[index][0] > speed_min_condition and \
         elem[index][0] < speed_max_condition and \
@@ -146,7 +162,7 @@ def satisfy_throttle_condition(elem, index):
     return condition
 
 
-def feature_cut(elem):
+def feature_cut(elem, VEHICLE_PARAM_CONF):
     """
     get desired feature interval
     """
@@ -154,7 +170,8 @@ def feature_cut(elem):
     num_row = elem.shape[0]
     # find satisfied data
     for i in range(num_row):
-        if satisfy_throttle_condition(elem, i) or satisfy_brake_condition(elem, i):
+        if satisfy_throttle_condition(elem, i, VEHICLE_PARAM_CONF) \
+            or satisfy_brake_condition(elem, i, VEHICLE_PARAM_CONF):
             elem[id_elem][0] = elem[i][0]
             elem[id_elem][1] = elem[i][1]
             elem[id_elem][2] = elem[i][2]
@@ -164,10 +181,11 @@ def feature_cut(elem):
     return elem[0:id_elem, 0:4]
 
 
-def feature_distribute(elem):
+def feature_distribute(elem, VEHICLE_PARAM_CONF):
     """
     distribute feature into each grid
     """
+    segment_cmd_list = gen_cmd_list(VEHICLE_PARAM_CONF)
     # TODO: Use collections.defaultdict(dict)
     grid_dict = {}
     for segment_cmd in segment_cmd_list:
@@ -217,10 +235,11 @@ def feature_distribute(elem):
     return (grid_dict, elem)
 
 
-def feature_store(elem):
+def feature_store(elem, VEHICLE_PARAM_CONF):
     """
     store feature into segment_feature container
     """
+    segment_cmd_list = gen_cmd_list(VEHICLE_PARAM_CONF)
     grid_dict = elem[0]
     feature = elem[1]
     segment_feature = np.zeros([len(feature), 3])
@@ -233,7 +252,7 @@ def feature_store(elem):
                 counter += 1
     return segment_feature[0:counter, :]
 
-def write_h5_train_test(elem, origin_prefix, target_prefix, vehicle_type):
+def write_h5_train_test(elem, origin_prefix, target_prefix):
     """write to h5 file"""
     (file_dir, key), features = elem
     feature_num = features.shape[0] # row
