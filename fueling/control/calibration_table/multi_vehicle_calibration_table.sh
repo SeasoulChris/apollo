ac010925bc61f1af84b215433b23899e38099a7f
Shu Jiang <shujiang2016@gmail.com>
2019-04-29 13:24:50 -0700

    control: multi-vehicle calibration table, added a script for automation

---
+++ fueling/control/calibration_table/multi_vehicle_calibration_table.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Go to apollo-fuel root.
+cd "$( dirname "${BASH_SOURCE[0]}" )/../../.."
+
+set -e
+set -x
+
+# Make sure you are calling run_prod() instead of run_test()!
+# Feature extraction.
+JOB="fueling/control/calibration_table/multi-vehicle-calibration-table-feature-extraction.py"
+ENV="fuel-py27-cyber"
+./tools/submit-job-to-k8s.sh --env ${ENV} --workers 16 --cpu 2 --memory 20g ${JOB}
+
+# Data distribution visualization
+JOB="fueling/control/calibration_table/multi-vehicle-data-visualization.py"
+ENV="fuel-py27"
+./tools/submit-job-to-k8s.sh --env ${ENV} --workers 12 --cpu 2 --memory 20g ${JOB}
+
+# Training.
+JOB="fueling/control/calibration_table/calibration-table-training.py"
+ENV="fuel-py27"
+./tools/submit-job-to-k8s.sh --env ${ENV} --workers 1 --cpu 20 --memory 200g ${JOB}
+
+# Result visualization
+JOB="fueling/control/calibration_table/multi-vehicle-result-visualization.py"
+ENV="fuel-py27"
+./tools/submit-job-to-k8s.sh --env ${ENV} --workers 12 --cpu 2 --memory 20g ${JOB}
