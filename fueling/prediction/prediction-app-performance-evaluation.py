b2493572f26a326b395862c796850e84b43a38f6
kechxu <kechxu89@gmail.com>
2019-05-10 11:34:30 -0700

    Prediction: add prediction-app: performance evaluation

---
+++ fueling/prediction/prediction-app-performance-evaluation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from absl import app
+
+from fueling.prediction.prediction_result import PredictionResult
+from fueling.prediction.evaluate_performance import PerformanceEvaluator
+
+
+def performance_evaluation(argv):
+    PredictionResult().__main__(argv)
+    PerformanceEvaluator().__main__(argv)
+
+if __name__ == '__main__':
+    app.run(performance_evaluation)
