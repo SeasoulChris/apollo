01e5ed1529dbc76215acf315650c4b8ef687752d
Shu Jiang <shujiang2016@gmail.com>
2019-05-17 16:23:05 -0700

    control: calibration table add sanity check for vehicle ID

--- fueling/control/calibration_table/sanity_check.py
+++ fueling/control/calibration_table/sanity_check.py
@@ -2,6 +2,7 @@
 import os
 import math
 
+import colored_glog as glog
 import google.protobuf.text_format as text_format
 
 from cyber_py.record import RecordReader
@@ -32,11 +33,11 @@ def missing_file(path):
         # config file
         conf = os.path.join(path, vehicle, ConfFile)
         if os.path.exists(conf) is False:
-            print('Missing configuration file')
+            glog.error('Missing configuration file')
             return True
     # record file
     if len(list_records(path)) == 0:
-        print('No record files')
+        glog.error('No record files')
         return True
     return False
 
@@ -50,18 +51,29 @@ def parse_error(path):
             proto_utils.get_pb_from_text_file(conf, pb_value)
             return False
         except text_format.ParseError:
-            print('Error: Cannot parse %s as binary or text proto' % conf)
+            glog.error('Error: Cannot parse %s as binary or text proto' % conf)
             return True
 
 
-# TODO: string
+def check_vehicle_id(conf):
+    # print(conf.HasField('vehicle_id.other_unique_id'))
+    vehicle_id = conf.vehicle_param.vehicle_id
+    if vehicle_id.HasField('vin') or vehicle_id.HasField('plate') \
+            or vehicle_id.HasField('other_unique_id'):
+        return True
+    glog.error("Error: No vehicle ID")
+    return False
+
+
 def missing_field(path):
     vehicles = multi_vehicle_utils.get_vehicle(path)
     pb_value = vehicle_config_pb2.VehicleConfig()
     for vehicle in vehicles:
         conf_file = os.path.join(path, vehicle, ConfFile)
         conf = proto_utils.get_pb_from_text_file(conf_file, pb_value)
-        print(conf.vehicle_param)
+        glog.error(conf)
+        if not check_vehicle_id(conf):
+            return False
         # required field
         fields = [conf.vehicle_param.brake_deadzone,
                   conf.vehicle_param.throttle_deadzone,
@@ -70,7 +82,6 @@ def missing_field(path):
         # for value in conf.vehicle_param:
         # has field is always true since a default value is given
         for field in fields:
-            # if(field == 0.0):
             if math.isnan(field):
                 return True
         return False
@@ -87,19 +98,19 @@ def missing_message_data(path, channels=CHANNELS):
 
 def sanity_check(input_folder):
     if missing_file(input_folder):
-        print("One or more files are missing")
+        glog.error("One or more files are missing")
         return False
     elif parse_error(input_folder):
-        print("Confige file cannot be parsed")
+        glog.error("Confige file cannot be parsed")
         return False
     elif missing_field(input_folder):
-        print("One or more field is missing in Confige file")
+        glog.error("One or more field is missing in Confige file")
         return False
     elif missing_message_data(input_folder):
-        print("Messages are missing in records")
+        glog.error("Messages are missing in records")
         return False
     else:
-        print("Passed sanity check.")
+        glog.info("Passed sanity check.")
         return True
 
 
