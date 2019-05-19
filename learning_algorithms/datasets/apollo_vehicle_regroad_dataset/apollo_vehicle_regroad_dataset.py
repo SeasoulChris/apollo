00c22967114f7967d76e526b4852c8b653bfffd5
panjiacheng <panjiacheng@yahoo.com>
2019-05-18 17:39:37 -0700

    Prediction: data cleaning for the apollo vehicle regular road dataset.

--- learning_algorithms/datasets/apollo_vehicle_regroad_dataset/apollo_vehicle_regroad_dataset.py
+++ learning_algorithms/datasets/apollo_vehicle_regroad_dataset/apollo_vehicle_regroad_dataset.py
@@ -34,25 +34,105 @@ from learning_algorithms.utilities.IO_utils import *
 def LoadDataForLearning(filepath):
     list_of_data_for_learning = \
         learning_algorithms.datasets.apollo_pedestrian_dataset.data_for_learning_pb2.ListDataForLearning()
-    with open(filepath, 'rb') as file_in:
-        list_of_data_for_learning.ParseFromString(file_in.read())
-    return list_of_data_for_learning.data_for_learning
+    try:
+        with open(filepath, 'rb') as file_in:
+            list_of_data_for_learning.ParseFromString(file_in.read())
+        return list_of_data_for_learning.data_for_learning
+    except:
+        return None
 
 
-def DataPreprocessing(feature_dir, label_dir):
+def DataPreprocessing(feature_dir, label_dir, pred_len=3.0, stable_window=0.5):
     # Go through all the data_for_learning file, for each data-point, find
     # the corresponding label file, merge them.
+    all_file_paths = GetListOfFiles(feature_dir)
+    num_dirty_data_point = 0
 
+    for path in all_file_paths:
+        # Load the label dict file.
+        dir_path = os.path.dirname(path)
+        label_path = os.path.join(dir_path, 'merged_visited_lane_segment.npy')
+        label_path = label_path.replace('features', 'labels')
+        if not os.path.isfile(label_path):
+            continue
+        dict_labels = np.load(label_path).item()
+        # Load the feature for learning file.
+        vector_data_for_learning = LoadDataForLearning(path)
+        if vector_data_for_learning is None:
+            continue
+
+        output_np_array = []
+        for data_for_learning in vector_data_for_learning:
+            # Skip non-regroad-vehicle data:
+            if data_for_learning.category != 'cruise':
+                continue
+            curr_data_point = []
             # 1. Find in the dict the corresponding visited lane segments.
+            key = '{}@{:.3f}'.format(data_for_learning.id, data_for_learning.timestamp)
+            if key not in dict_labels:
+                continue
+            features_for_learning = data_for_learning.features_for_learning
+            serial_lane_graph = data_for_learning.string_features_for_learning
+            serial_lane_graph.append('|')
+            visited_lane_segments = dict_labels[key]
 
-            # 2. Refactor the data_for_learn into list of lane-sequences.
+            # 2. Deserialize the lane_graph.
+            lane_graph = []
+            lane_sequence = None
+            for lane_seg_id in serial_lane_graph:
+                if lane_seg_id == '|':
+                    if lane_sequence is not None:
+                        lane_graph.append(lane_sequence)
+                    lane_sequence = set()
+                else:
+                    lane_sequence.add(lane_seg_id)
 
-            # 3. Remove those jittering data points.
+            # 3. Based on the lane graph, remove those jittering data points (data cleaning).
+            final_lane_sequences = []
+            for element in visited_lane_segments:
+                timestamp = element[0]
+                lane_seg_id = element[1]
+                if timestamp > pred_len + 0.05:
+                    break
+                if timestamp > pred_len - stable_window + 0.05:
+                    lane_seq_set = set()
+                    for i, lane_sequence in enumerate(lane_graph):
+                        if lane_seg_id in lane_sequence:
+                            lane_seq_set.add(i)
+                    final_lane_sequences.append(lane_seq_set)
+            if len(final_lane_sequences) != int(stable_window/0.1):
+                continue
+            is_dirty_data_point = False
+            end_lane_sequences = final_lane_sequences[-1]
+            for i in end_lane_sequences:
+                for j in final_lane_sequences:
+                    if i not in j:
+                        is_dirty_data_point = True
+                        break
+                if is_dirty_data_point:
+                    break
+            if is_dirty_data_point:
+                num_dirty_data_point += 1
+                continue
 
             # 4. Refactor the label into the format of [1, 1, 0, 0, 0] ... (assume there are five lane sequences)
+            one_hot_encoding_label = []
+            for i in range(len(lane_graph)):
+                if i in end_lane_sequences:
+                    one_hot_encoding_label.append(1)
+                else:
+                    one_hot_encoding_label.append(0)
+            curr_data_point = [len(lane_graph)] + \
+                [features_for_learning[:180+400*len(lane_graph)]] + one_hot_encoding_label
+            output_np_array.append(curr_data_point)
 
     # Save into a local file for training.
+    print ('Total usable data points: {}'.format(len(output_np_array)))
+    print ('Removed dirty data points: {}'.format(num_dirty_data_point))
+    output_np_array = np.array(output_np_array)
+    np.save(os.path.join(dir_path, 'training_data.npy'), output_np_array)
 
+    return output_np_array
 
 
 class ApolloVehicleRegularRoadDataset(Dataset):
@@ -64,3 +144,7 @@ class ApolloVehicleRegularRoadDataset(Dataset):
 
     def __getitem__(self, idx):
         return idx
+
+if __name__ == '__main__':
+    DataPreprocessing('/home/jiacheng/work/apollo/data/apollo_vehicle_regroad_data/features/', 
+                      '/home/jiacheng/work/apollo/data/apollo_vehicle_regroad_data/labels/')
