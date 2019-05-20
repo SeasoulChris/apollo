d9f771eed6540bf175d47d430a2a20f5e12a8306
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-05-20 11:51:29 -0700

    Apps: Upgrade warehouse to py37.

--- apps/k8s/warehouse/deploy/start-server.sh
+++ apps/k8s/warehouse/deploy/start-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source /usr/local/miniconda2/bin/activate fuel-py36
+source /usr/local/miniconda2/bin/activate fuel-py37
 export PYTHONPATH="/apollo/py_proto:/apollo/modules/data/fuel:${PYTHONPATH}"
 
 cd /apollo/modules/data/fuel/apps/k8s/warehouse
