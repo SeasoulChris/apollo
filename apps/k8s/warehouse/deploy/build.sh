75b32c639aaed6b6375a21461de29116277b0076
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-05-22 17:40:21 -0700

    Apps: Simple refactor on warehouse.

--- apps/k8s/warehouse/deploy/build.sh
+++ apps/k8s/warehouse/deploy/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-REPO="hub.baidubce.com/apollo/warehouse"
+DOCKER_REGISTRY="hub.baidubce.com"
+DOCKER_USER="apollo"
+DOCKER_PASSWORD=""
+
+REPO="${DOCKER_REGISTRY}/${DOCKER_USER}/warehouse"
 IMAGE="${REPO}:$(date +%Y%m%d_%H%M)"
 
 echo "Building image: ${IMAGE}"
@@ -16,10 +20,6 @@ cp -r ../apollo/py_proto ./
 
 docker build -t ${IMAGE} --network host -f apps/k8s/warehouse/deploy/Dockerfile .
 
-# Login.
-DOCKER_REGISTRY="hub.baidubce.com"
-DOCKER_USER="apollo"
-DOCKER_PASSWORD=""
 # Please provide password if you want to login automatically.
 if [ ! -z "${DOCKER_PASSWORD}" ]; then
   docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} ${DOCKER_REGISTRY}
--- apps/k8s/warehouse/entrypoint.sh
+++ apps/k8s/warehouse/entrypoint.sh
@@ -4,4 +4,4 @@ source /usr/local/miniconda2/bin/activate fuel-py37
 export PYTHONPATH="/apollo/py_proto:/apollo/modules/data/fuel:${PYTHONPATH}"
 
 cd /apollo/modules/data/fuel/apps/k8s/warehouse
-python main.py
+python index.py
