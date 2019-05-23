7a47b43cec48028e4dcaf7e6df8c6a389b2c798c
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-05-23 16:29:58 -0700

    Apps: Change bae-proxy port to 8080.

--- apps/lambda/bae-proxy/index.py
+++ apps/lambda/bae-proxy/index.py
@@ -10,5 +10,5 @@ api = flask_restful.Api(app)
 api.add_resource(VehicleCalibration, '/vehicle-calibration')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1080, debug=True,
+    app.run(host='0.0.0.0', port=8080, debug=True,
             ssl_context=('deploy/cert.pem', 'deploy/key.pem'))
