62a818c252d5ae0eac7613aae000f661dec6e996
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-05-27 16:40:27 -0700

    Apps: Parse job_config and submit to k8s.

--- apps/lambda/bae-proxy/vehicle_calibration.py
+++ apps/lambda/bae-proxy/vehicle_calibration.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python
 
-import http
+from http import HTTPStatus
+import json
+import os
 
 from flask_restful import Resource
 import flask
+import google.protobuf.json_format as json_format
+
+from modules.tools.fuel_proxy.proto.job_config_pb2 import JobConfig
 
 
 class VehicleCalibration(Resource):
     """Vehicle Calibration restful service"""
 
     def post(self):
-        json = flask.request.get_json()
-        return json, http.HTTPStatus.ACCEPTED
+        """Accept user request, verify and process."""
+        # 1. Parse request.
+        try:
+            job_config = json_format.Parse(flask.request.get_json(), JobConfig())
+        except json_format.ParseError:
+            return self.wrap_json(HTTPStatus.BAD_REQUEST,
+                                  'Failed to parse job_config. Please verify it.')
+        # 2. User authentication.
+        user = 'myself'
+        # 3. Data sanity check.
+        # 4. Job summit.
+        os.system('cd /apollo/modules/data/fuel && '
+                  'nohup bash tools/submit-job-to-k8s.sh -w 1 -c 1 -m 1g -d 1 '
+                  'fueling/demo/count-msg-by-channel.py &')
+
+        return self.wrap_json(
+            HTTPStatus.ACCEPTED,
+            'Your job is in progress now! You will receive a notification in your corresponding '
+            'email when it is finished.')
+
+    @staticmethod
+    def wrap_json(status_code, message):
+        return json.dumps({'message': message}), status_code
