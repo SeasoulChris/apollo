2aaaff5e2073bb2667c2fc67f7d88c5969659c06
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-05-29 18:09:14 -0700

    Apps: Reject frequent requests.

--- apps/lambda/bae-proxy/vehicle_calibration.py
+++ apps/lambda/bae-proxy/vehicle_calibration.py
@@ -4,6 +4,8 @@ from http import HTTPStatus
 import json
 import os
 
+import fueling.common.time_utils as time_utils
+
 
 class VehicleCalibration(object):
     """Vehicle Calibration restful service"""
@@ -13,13 +15,29 @@ class VehicleCalibration(object):
 
     def process(self):
         """Accept user request, verify and process."""
-        # TODO: Data sanity check.
+        task_id_fmt = '%Y-%m-%d-%H-%M-%S' if job_config.partner_id == 'apollo' else '%Y-%m-%d-%H-%M'
+        task_id = time_utils.format_current_time(task_id_fmt)
+        mnt_path = os.path.join('/mnt', job_config.partner_id, task_id)
+        if os.path.exists(mnt_path):
+            msg = 'Please do not submit job too frequently!'
+            return json.dumps({'message': msg}), HTTPStatus.TOO_MANY_REQUESTS
+        os.makedirs(mnt_path)
+
+        # TODO: Bos config sanity check.
+        bos_config = self.job_config.storage.bos
+        # bos_config.bucket
+        # bos_config.endpoint
+        # bos_config.access_key
+        # bos_config.secret_key
+
         # TODO: Job summit.
+        # bos_config.4
+        # job_config.partner_id
+        # task_id
         os.system('cd /apollo/modules/data/fuel && '
                   'nohup bash tools/submit-job-to-k8s.sh -w 1 -c 1 -m 1g -d 1 '
                   'fueling/demo/count-msg-by-channel.py &')
 
-        return json.dumps({
-            'message': 'Your job is in progress now! You will receive a notification in your corresponding '
-            'email when it is finished.'}
-        ), HTTPStatus.ACCEPTED
+        msg = ('Your job {} is in process now! You will receive a notification in your '
+               'corresponding email when it is finished.'.format(task_id))
+        return json.dumps({'message': msg}), HTTPStatus.ACCEPTED
--- fueling/common/time_utils.py
+++ fueling/common/time_utils.py
@@ -2,9 +2,16 @@
 #!/usr/bin/env python
 
 import datetime
+import time
+
 import pytz
+
 
 def msg_time_to_datetime(msg_time, local_tz='America/Los_Angeles', msg_tz='UTC'):
     """Cyber message timestamp is generally in UTC nano seconds."""
     dt = datetime.datetime.fromtimestamp(msg_time / (10 ** 9), pytz.timezone(msg_tz))
     return dt.astimezone(pytz.timezone(local_tz))
+
+def format_current_time(fmt='%Y-%m-%d-%H-%M-%S'):
+    """Format current time."""
+    return datetime.datetime.fromtimestamp(time.time()).strftime(fmt)
