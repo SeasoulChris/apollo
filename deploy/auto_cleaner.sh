6f2a77266293f6f452297b4de6fac43e01f88588
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-05-31 11:04:15 -0700

    Deploy: Delete error pod after 24h.

--- deploy/auto_cleaner.sh
+++ deploy/auto_cleaner.sh
@@ -5,13 +5,20 @@
 
 set -e
 
-CUT_TIMESTAMP=$(date -d '12 hours ago' +%s%3N)
+function DeletePod() {
+  DRIVER_POD_STATUS=$1
+  CUT_TIME=$2
 
-kubectl get pods | grep '\-driver' | grep -e 'Completed' | awk '{print $1}' | \
-while read -r DRIVER_POD; do
-  DRIVER_TIMESTAMP=$(echo "${DRIVER_POD}" | awk -F- '{print $(NF-1)}')
-  if [ "${DRIVER_TIMESTAMP}" -lt "${CUT_TIMESTAMP}" ]; then
-    echo "Delete pod ${DRIVER_POD}"
-    kubectl delete pods "${DRIVER_POD}"
-  fi
-done
+  CUT_TIMESTAMP=$(date -d "${CUT_TIME}" +%s%3N)
+  kubectl get pods | grep '\-driver' | grep "${DRIVER_POD_STATUS}" | awk '{print $1}' | \
+  while read -r DRIVER_POD; do
+    DRIVER_TIMESTAMP=$(echo "${DRIVER_POD}" | awk -F- '{print $(NF-1)}')
+    if [ "${DRIVER_TIMESTAMP}" -lt "${CUT_TIMESTAMP}" ]; then
+      echo "Delete ${DRIVER_POD_STATUS} pod ${DRIVER_POD}"
+      kubectl delete pods "${DRIVER_POD}"
+    fi
+  done
+}
+
+DeletePod 'Completed' '12 hours ago'
+DeletePod 'Error'     '24 hours ago'
