ac8fa1e62527e18fbd28dc72394a6411b47d74d1
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-06-06 11:41:45 -0700

    Apps: Finish partner calibration job.

--- apps/lambda/bae-proxy/vehicle_calibration.sh
+++ apps/lambda/bae-proxy/vehicle_calibration.sh
@@ -9,14 +9,23 @@ export PATH=/usr/local/miniconda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/u
 FUELING="/mnt/bos/modules/data/jobs/deploy/20190531-133701_fueling.zip"
 SUBMITER="/apollo/modules/data/fuel/tools/submit-job-to-k8s.sh --fueling ${FUELING} ${BASH_ARGS}"
 
-# 1. Feature extraction.
-JOB="/apollo/modules/data/fuel/fueling/control/calibration_table/multi-vehicle-feature-extraction.py"
+# Feature extraction.
+JOB="/apollo/modules/data/fuel/fueling/control/calibration_table/multi-job-feature-extraction.py"
 ENV="fuel-py27-cyber"
-${SUBMITER} --env ${ENV} --workers 9 --cpu 2 --memory 20g ${JOB} \
-    ${PY_ARGS} --input_data_path="${INPUT_DATA_PATH}"
+${SUBMITER} --env ${ENV} --workers 10 --cpu 1 --memory 40g ${JOB} ${PY_ARGS} \
+    --input_data_path="${INPUT_DATA_PATH}"
 
-# TODO
-# 2. Training
-# 3. Result visualization
-# 4. Data distribution visualization
-# 5. Notify partner
+# Training.
+JOB="/apollo/modules/data/fuel/fueling/control/calibration_table/multi-job-train.py"
+ENV="fuel-py27"
+${SUBMITER} --env ${ENV} --workers 5 --cpu 5 --memory 60g ${JOB} ${PY_ARGS}
+
+# Result visualization
+JOB="/apollo/modules/data/fuel/fueling/control/calibration_table/multi-job-result-visualization.py"
+ENV="fuel-py27"
+${SUBMITER} --env ${ENV} --workers 10 --cpu 1 --memory 20g ${JOB} ${PY_ARGS}
+
+# Data distribution visualization
+JOB="/apollo/modules/data/fuel/fueling/control/calibration_table/multi-job-data-distribution.py"
+ENV="fuel-py27"
+${SUBMITER} --env ${ENV} --workers 10 --cpu 1 --memory 20g ${JOB} ${PY_ARGS}
