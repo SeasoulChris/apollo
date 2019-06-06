b42c155bf1ee40ef352cae44024f9a43509276a1
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-06-06 11:08:33 -0700

    Deploy: Fix daily job pipeline.

--- deploy/daily_jobs.sh
+++ deploy/daily_jobs.sh
@@ -10,11 +10,11 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 # Job: Data jobs.
 JOB="fueling/data/daily-data-jobs.py"
-./tools/submit-job-to-k8s.sh --fueling ${FUELING} --workers 15 --memory 24g --disk 800 ${JOB}
+./tools/submit-job-to-k8s.sh --workers 15 --memory 24g --disk 800 ${JOB}
 
 # Job: Control profiling.
 JOB="fueling/control/control_profiling/control-profiling-metrics.py"
-./tools/submit-job-to-k8s.sh --workers 16 --memory 24g ${JOB}
+./tools/submit-job-to-k8s.sh --workers 15 --memory 24g ${JOB}
 JOB="fueling/control/control_profiling/control-profiling-visualization.py"
 CONDA_ENV="fuel-py27"
-./tools/submit-job-to-k8s.sh --workers 16 --memory 24g -e ${CONDA_ENV} ${JOB}
+./tools/submit-job-to-k8s.sh --workers 15 --memory 24g -e ${CONDA_ENV} ${JOB}
