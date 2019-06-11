76dcc8e6c6eecda01c0c9cb5f31c1ac9e03a2d10
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-06-10 19:05:42 -0700

    Common: Change evangelist email to machao.

--- fueling/common/partners.py
+++ fueling/common/partners.py
@@ -8,6 +8,6 @@ Partner = collections.namedtuple('Partner', ['name', 'email'])
 
 partners = {
     'apollo': Partner(name='Apollo', email='xiaoxiangquan@baidu.com'),
-    'apollo-evangelist': Partner(name='Evangelist', email='apollo-evangelist@baidu.com'),
+    'apollo-evangelist': Partner(name='Evangelist', email='machao20@baidu.com'),
     'udelv2019': Partner(name='Udelv', email=None),
 }
