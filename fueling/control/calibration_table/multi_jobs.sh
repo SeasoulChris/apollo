7d1a89f3b8c3a8b7a5918d1f19b078848f25906d
Shu Jiang <shujiang2016@gmail.com>
2019-06-11 13:39:03 -0700

    control: calibration table editted bash script

--- fueling/control/calibration_table/multi_jobs.sh
+++ fueling/control/calibration_table/multi_jobs.sh
@@ -4,15 +4,20 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )/../../.."
 
 set -e
-JOB_ID=$(date +%Y-%m-%d-%H)
-
+# JOB_ID=$(date +%Y-%m-%d-%H)
+JOB_ID="task002"
 # Feature extraction.
 JOB="fueling/control/calibration_table/multi-job-feature-extraction.py"
 ENV="fuel-py27-cyber"
-INPUT_DATA_PATH="modules/control/apollo_calibration_table" # Mkz7
+INPUT_DATA_PATH="modules/control/data/task002"
+# INPUT_DATA_PATH="modules/control/apollo_calibration_table" # Mkz7
 # INPUT_DATA_PATH="modules/control/cool_high"
 # INPUT_DATA_PATH="modules/control/calibration_table/records"
-./tools/submit-job-to-k8s.sh --env ${ENV} --workers 5 --cpu 2 --memory 60g ${JOB} \
+./tools/submit-job-to-k8s.sh --partner_bos_region bj \
+--partner_bos_bucket apollo-evangelist \
+--partner_bos_access 378e14ae2b7b4da5bebfa17bf566686f \
+--partner_bos_secret 0caa4af51baa4d768a5e33ce0c384495 \
+--env ${ENV} --workers 5 --cpu 2 --memory 60g ${JOB} \
 --input_data_path="${INPUT_DATA_PATH}" --job_id="${JOB_ID}"
 
 
