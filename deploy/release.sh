904ba2ff585a2e35bf2701bbc4768ea53be3bf69
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-06-12 15:47:29 -0700

    Apps: Make new release to bae-proxy.

--- deploy/release.sh
+++ deploy/release.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
-LOCAL_FUELING_PKG="deploy/fueling.zip"
-REMOTE_FUELING_PKG="modules/data/jobs/deploy/$(date +%Y%m%d-%H%M%S)_fueling.zip"
+LOCAL_FUELING_PKG="deploy/fueling-latest.zip"
 rm -f "${LOCAL_FUELING_PKG}"
 
-set -x
 set -e
 
-# Make new package
 zip -r "${LOCAL_FUELING_PKG}" ./fueling -x *.pyc */__pycache__
-# Upload new package
-./apps/local/bos_fstool -s "${LOCAL_FUELING_PKG}" -d "${REMOTE_FUELING_PKG}"
-# Update usage
-sed -i "s|^FUELING=.*|FUELING=\"/mnt/bos/${REMOTE_FUELING_PKG}\"|g" \
-    ./apps/lambda/bae-proxy/vehicle_calibration.sh
+echo "Now you can push the package deploy/fueling-latest.zip to \
+modules/data/jobs/deploy/fueling-latest.zip"
