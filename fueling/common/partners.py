09ed0e1efe78a07afdde7acc16c37972edd7da71
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-06-14 12:49:30 -0700

    Common: Add QA contact point for test.

--- fueling/common/partners.py
+++ fueling/common/partners.py
@@ -9,5 +9,6 @@ Partner = collections.namedtuple('Partner', ['name', 'email'])
 partners = {
     'apollo': Partner(name='Apollo', email='xiaoxiangquan@baidu.com'),
     'apollo-evangelist': Partner(name='Evangelist', email='machao20@baidu.com'),
+    'apollo-qa': Partner(name='QA', email='taoshengzhao01@baidu.com'),
     'udelv2019': Partner(name='Udelv', email='xiaoxiangquan@baidu.com'),
 }
