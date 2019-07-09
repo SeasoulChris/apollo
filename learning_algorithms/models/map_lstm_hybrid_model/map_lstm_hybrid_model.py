87e203e38e3628b1bdd419a808266a9e012babf6
kechxu <kechxu89@gmail.com>
2019-07-09 11:56:57 -0700

    Prediction: add framework of map_lstm_hybrid_model

---
+++ learning_algorithms/models/map_lstm_hybrid_model/map_lstm_hybrid_model.py
@@ -0,0 +1,48 @@
+###############################################################################
+# Copyright 2019 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+import cv2 as cv
+import glob
+import numpy as np
+import os
+import sys
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset
+
+from torchvision import models
+from torchvision import transforms
+
+sys.path.append('../../utilities')
+
+from helper_utils import *
+
+'''
+========================================================================
+Model definition
+========================================================================
+'''
+class MapLSTMHybridModel(nn.Module):
+    def __init__(self):
+        # TODO(all): implement
+        pass
+
+    def forward(self, x):
+        # TODO(all): implement
+        pass
