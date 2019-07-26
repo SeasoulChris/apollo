a3a73d38543470c156aa090f526ade5b9fc50cf6
panjiacheng <panjiacheng@yahoo.com>
2019-07-26 13:58:01 -0700

    Learning: add unit test for apollo vehicle trajectory dataset.

---
+++ learning_algorithms/prediction/datasets/apollo_vehicle_trajectory_dataset/test_apollo_vehicle_trajectory_dataset.py
@@ -0,0 +1,85 @@
+###############################################################################
+# Copyright 2019 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+import argparse
+import cv2 as cv
+import logging
+import numpy as np
+import os
+import torch
+import math
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+from torch.utils.data import Dataset, DataLoader
+
+import learning_algorithms.datasets.apollo_vehicle_trajectory_dataset.apollo_vehicle_trajectory_dataset as apollo_vehicle_trajectory_dataset
+from apollo_vehicle_trajectory_dataset import ApolloVehicleTrajectoryDataset as ApolloVehicleTrajectoryDataset
+from apollo_vehicle_trajectory_dataset import collate_fn as collate_fn
+
+
+def point_to_idx(point_x, point_y):
+        return (int((point_x + 40)/0.1), int((point_y + 40)/0.1))
+
+def plot_img(obs_features, lane_features, labels, count):
+    # black background
+    img = np.zeros([1000, 800, 3], dtype=np.uint8)
+
+    # draw boundaries
+    cv.circle(img,(400,400),2,color=[255,255,255], thickness=4)
+    cv.line(img, (0,0), (799,0), color=[255, 255, 255])
+    cv.line(img, (799,0), (799,999), color=[255, 255, 255])
+    cv.line(img, (0,999), (0,0), color=[255, 255, 255])
+    cv.line(img, (799,999), (0,999), color=[255, 255, 255])
+    num_lane_seq = lane_features.shape[0]
+
+    for obs_hist_pt in range(20):
+        cv.circle(img, point_to_idx(obs_features[1+obs_hist_pt*2], obs_features[obs_hist_pt*2]), radius=3, color=[128,128,128])
+
+    for lane_idx in range(num_lane_seq):
+        curr_lane = lane_features[lane_idx]
+        color_to_use = [0, 128, 128]
+        for point_idx in range(149):
+            cv.line(img, point_to_idx(curr_lane[point_idx,1], curr_lane[point_idx,0]), \
+                point_to_idx(curr_lane[point_idx+1,1], curr_lane[point_idx+1,0]), \
+                color=color_to_use)
+
+    for obs_future_pt in range(30):
+        cv.circle(img, point_to_idx(labels[1+obs_future_pt*2], labels[obs_future_pt*2]), radius=3, color=[128,128,0])
+
+    cv.imwrite('img={}__laneseq={}.png'.format(count, num_lane_seq), cv.flip(cv.flip(img,0),1))
+
+
+if __name__ == '__main__':
+    dataset_path = '/home/jiacheng/large-data/data_preprocessing/training_data/features/san_mateo/2018/2018-10-01'
+    test_dataset = ApolloVehicleTrajectoryDataset(dataset_path)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=1, collate_fn=collate_fn)
+
+    count = 0
+    for i, (X, y) in enumerate(test_dataloader):
+        if count == 10:
+            break
+        # print ('=======================')
+        # print (y[0])
+        # print (y[1])
+        # print (y[2])
+        plot_img(X[1].numpy().reshape(-1), X[3].numpy(), y[0].numpy().reshape(-1), count)
+        count += 1
