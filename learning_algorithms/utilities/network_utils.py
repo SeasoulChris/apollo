acbfeb0d864e401395e052b63f9b75957a92ce60
panjiacheng <panjiacheng@yahoo.com>
2019-07-29 18:54:35 -0700

    Learning: add LSTM related network generation functions into utils.

--- learning_algorithms/utilities/network_utils.py
+++ learning_algorithms/utilities/network_utils.py
@@ -32,3 +32,24 @@ def generate_mlp(dim_list, last_layer_nonlinear=True ,dropout=0.0):
         return nn.Sequential(*layers)
     else:
         return nn.Sequential(*layers[:-2])
+
+'''  '''
+def generate_lstm_states(hidden_size, bilateral=False):
+    h0 = torch.zeros(1, hidden_size)
+    c0 = torch.zeros(1, hidden_size)
+    nn.init.xavier_normal_(h0, gain=nn.init.calculate_gain('relu'))
+    nn.init.xavier_normal_(c0, gain=nn.init.calculate_gain('relu'))
+    h0 = nn.Parameter(h0, requires_grad=True)
+    c0 = nn.Parameter(c0, requires_grad=True)
+    return h0, c0
+
+'''  '''
+def generate_lstm(input_size, hidden_size, bilateral=False):
+    h0 = torch.zeros(1, hidden_size)
+    c0 = torch.zeros(1, hidden_size)
+    nn.init.xavier_normal_(h0, gain=nn.init.calculate_gain('relu'))
+    nn.init.xavier_normal_(c0, gain=nn.init.calculate_gain('relu'))
+    h0 = nn.Parameter(h0, requires_grad=True)
+    c0 = nn.Parameter(c0, requires_grad=True)
+    lstm = nn.LSTM(input_size, hidden_size, num_layers=1, batch_first=True)
+    return h0, c0, lstm
