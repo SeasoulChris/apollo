a817e4343e5176fb9359a9cabfc78b63138a4f4c
KaWaiTsoiBaidu <kawaitsoi@baidu.com>
2019-08-15 19:06:05 -0700

    Perception: add object_utils

---
+++ fueling/perception/YOLOv3/utils/object_utils.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+
+import collections
+import copy
+import math
+import os
+
+from PIL import Image
+from PIL import ImageDraw
+import cv2
+import numpy as np
+
+
+def inverse_rigid_trans(Tr):
+    """
+    Inverse a rigid body transform matrix (3x4 as [R|t])
+    [R'|-R't; 0|1]
+    """
+    inv_Tr = np.zeros_like(Tr) # 3x4
+    inv_Tr[0:3,0:3] = np.transpose(Tr[0:3,0:3])
+    inv_Tr[0:3,3] = np.dot(-np.transpose(Tr[0:3,0:3]), Tr[0:3,3])
+    return inv_Tr
+
+class Object(object):
+    """ 2D-3D object """
+    def __init__(self, data):
+        """
+        data: [0:obj_type, 1:truncation, 2:occlusion, 3:alpha, 
+               4:xmin, 5:ymin, 6:xmax, 7:ymax, 8:h, 9:w, 10:l,
+               11:X, 12:Y, 13:Z, 14:ry, 15:score]
+              obj_type -> string, other can be float or None.
+        """
+        data[1:] = list(map(lambda x: float(x) if x!=None else None , data[1:]))
+        # extract label, truncation, occlusion
+        self.type = data[0] # 'Car', 'Pedestrian', ...
+        self.truncation = data[1] # truncated pixel ratio [0..1]
+        # 0=visible, 1=partly occluded, 2=fully occluded, 3=unknown
+        self.occlusion = int(data[2]) if data[2]!=None else None
+        self.alpha = data[3] # object observation angle [-pi..pi]
+
+        # extract 2d bounding box in 0-based coordinates
+        self.xmin = data[4] # left
+        self.ymin = data[5] # top
+        self.xmax = data[6] # right
+        self.ymax = data[7] # bottom
+        self.box2d = np.array([self.xmin,self.ymin,self.xmax,self.ymax])
+
+        # extract 3d bounding box information
+        self.h = data[8] # box height
+        self.w = data[9] # box width
+        self.l = data[10] # box length (in meters)
+        self.t = (data[11],data[12],data[13]) # location (x,y,z) in camera coord.
+        self.ry = data[14] # yaw angle (around Y-axis in camera coordinates) [-pi..pi]
+        self.score = 0.0
+
+    def print_object(self):
+        print('Type, truncation, occlusion, alpha: %s, %d, %d, %f' % \
+            (self.type, self.truncation, self.occlusion, self.alpha))
+        print('2d bbox (x0,y0,x1,y1): %f, %f, %f, %f' % \
+            (self.xmin, self.ymin, self.xmax, self.ymax))
+        print('3d bbox h,w,l: %f, %f, %f' % \
+            (self.h, self.w, self.l))
+        print('3d bbox location, ry: (%f, %f, %f), %f' % \
+            (self.t[0],self.t[1],self.t[2],self.ry))
+ 
+       
+class Label_Object(Object):
+    """
+    3d object label
+    """
+    def __init__(self, label_file_line, class_name_id_map):
+        data = label_file_line.split(' ')
+        Object.__init__(self, data)
+        self.type_id = class_name_id_map[self.type]
+
+        
+class Calibration(object):
+    ''' Calibration matrices and utils
+        3d XYZ in <label>.txt are in rect camera coord.
+        2d box xy are in image2 coord
+        Points in <lidar>.bin are in Velodyne coord.
+        y_image2 = P^2_rect * x_rect
+        y_image2 = P^2_rect * R0_rect * Tr_velo_to_cam * x_velo
+        x_ref = Tr_velo_to_cam * x_velo
+        x_rect = R0_rect * x_ref
+        P^2_rect = [f^2_u,  0,      c^2_u,  -f^2_u b^2_x;
+                    0,      f^2_v,  c^2_v,  -f^2_v b^2_y;
+                    0,      0,      1,      0]
+                 = K * [1|t]
+        image2 coord:
+         ----> x-axis (u)
+        |
+        |
+        v y-axis (v)
+        velodyne coord:
+        front x, left y, up z
+        rect/ref camera coord:
+        right x, down y, front z
+        Ref (KITTI paper): http://www.cvlibs.net/publications/Geiger2013IJRR.pdf
+        TODO: do matrix multiplication only once for each projection.
+    '''
+    def __init__(self, calib_filepath):
+        calibs = self.read_calib_file(calib_filepath)
+        # Projection matrix from rect camera coord to image2 coord
+        self.P = calibs['P2']
+        self.P = np.reshape(self.P, [3,4])
+        # Rigid transform from Velodyne coord to reference camera coord
+        self.V2C = calibs['Tr_velo_to_cam']
+        self.V2C = np.reshape(self.V2C, [3,4])
+        self.C2V = inverse_rigid_trans(self.V2C)
+        # Rotation from reference camera coord to rect camera coord
+        self.R0 = calibs['R0_rect']
+        self.R0 = np.reshape(self.R0,[3,3])
+
+        # Camera intrinsics and extrinsics
+        self.c_u = self.P[0,2]
+        self.c_v = self.P[1,2]
+        self.f_u = self.P[0,0]
+        self.f_v = self.P[1,1]
+        self.b_x = self.P[0,3]/(-self.f_u) # relative
+        self.b_y = self.P[1,3]/(-self.f_v)
+
+    def read_calib_file(self, filepath):
+        ''' Read in a calibration file and parse into a dictionary.
+        Ref: https://github.com/utiasSTARS/pykitti/blob/master/pykitti/utils.py
+        '''
+        data = {}
+        with open(filepath, 'r') as f:
+            for line in f.readlines():
+                line = line.rstrip()
+                if len(line)==0: continue
+                key, value = line.split(':', 1)
+                # The only non-float values in these files are dates, which
+                # we don't care about anyway
+                try:
+                    data[key] = np.array([float(x) for x in value.split()])
+                except ValueError:
+                    pass
+
+        return data
+
