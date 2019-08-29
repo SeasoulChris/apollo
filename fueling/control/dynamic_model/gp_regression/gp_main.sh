0b5e32307444e1f5562d3fae4fad9f36d114e6c9
Qi Luo <qi.stephen.luo@gmail.com>
2019-08-28 17:26:19 -0700

    control: fix the user name in scripts

--- fueling/control/dynamic_model/gp_regression/gp_main.sh
+++ fueling/control/dynamic_model/gp_regression/gp_main.sh
@@ -8,7 +8,7 @@ set -x
 
 # Make sure you are calling run_prod() instead of run_test()!
 # Feature extraction.
-export PYTHONPATH=/apollo/modules/data/fuel:/home/luoqi06/.conda/envs/fuel-py36-pyro/bin/python:/apollo/py_proto:/apollo/modules/tools:/apollo/bazel-bin/cyber/py_wrapper:/apollo/cyber/python:/apollo/bazel-bin/cyber/py_wrapper:/apollo/cyber/python
+export PYTHONPATH=/apollo/modules/data/fuel:/home/$USER/.conda/envs/fuel-py36-pyro/bin/python:/apollo/py_proto:/apollo/modules/tools:/apollo/bazel-bin/cyber/py_wrapper:/apollo/cyber/python:/apollo/bazel-bin/cyber/py_wrapper:/apollo/cyber/python
 
 
 python /apollo/modules/data/fuel/fueling/control/dynamic_model/gp_regression/main.py
