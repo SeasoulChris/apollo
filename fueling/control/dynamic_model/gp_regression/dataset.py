73304d03c55e1dddf500c341c6e33617bce1b788
JiaxuanXu <jx.jason.xu@gmail.com>
2019-09-04 16:34:59 -0700

    Dynamic 2.0: add feature smoothing

--- fueling/control/dynamic_model/gp_regression/dataset.py
+++ fueling/control/dynamic_model/gp_regression/dataset.py
@@ -6,6 +6,7 @@ import os
 import pickle
 import sys
 
+from scipy.signal import savgol_filter
 from torch.utils.data.dataset import Dataset
 import colored_glog as glog
 import h5py
@@ -21,7 +22,8 @@ from fueling.control.dynamic_model.gp_regression.model_conf import input_index, 
 INPUT_LENGTH = 100
 DIM_INPUT = feature_config["input_dim"]
 DIM_OUTPUT = feature_config["output_dim"]
-
+WINDOW_SIZE = feature_config["window_size"]
+POLYNOMINAL_ORDER = feature_config["polynomial_order"]
 
 class GPDataSet(Dataset):
 
@@ -42,10 +44,17 @@ class GPDataSet(Dataset):
         for h5_file in datasets:
             glog.debug(os.path.join(h5_file))
             with h5py.File(h5_file, 'r') as model_norms_file:
-                input_segment = torch.tensor(np.array(model_norms_file.get('input_segment')))
-                output_segment = torch.tensor(np.array(model_norms_file.get('output_segment')))
+                # Get input data
+                input_segment = np.array(model_norms_file.get('input_segment'))
+                # Smoothing noisy acceleration data
+                input_segment[:, input_index["a"]] = savgol_filter(input_segment[:, input_index["a"]],
+                                                                   WINDOW_SIZE, POLYNOMINAL_ORDER)
+                input_segment = torch.from_numpy(input_segment)
                 input_segment = input_segment.view(1, INPUT_LENGTH, DIM_INPUT)
+                # Get output data
+                output_segment = torch.tensor(np.array(model_norms_file.get('output_segment')))
                 output_segment = output_segment.view(DIM_OUTPUT, 1)
+                # Stack the data segments
                 input_data = torch.cat((input_data, input_segment.float()), 0)
                 output_data = torch.cat((output_data, output_segment.float()), 1)
         return (input_data, output_data)
@@ -60,10 +69,17 @@ class GPDataSet(Dataset):
         for h5_file in datasets:
             glog.debug(os.path.join(h5_file))
             with h5py.File(h5_file, 'r') as model_norms_file:
-                input_segment = torch.tensor(np.array(model_norms_file.get('input_segment')))
-                gt_res_error = torch.tensor(np.array(model_norms_file.get('output_segment')))
+                # Get input data
+                input_segment = np.array(model_norms_file.get('input_segment'))
+                # Smoothing noisy acceleration data
+                input_segment[:, input_index["a"]] = savgol_filter(input_segment[:, input_index["a"]],
+                                                                   WINDOW_SIZE, POLYNOMINAL_ORDER)
+                input_segment = torch.from_numpy(input_segment)
                 input_segment = input_segment.view(1, INPUT_LENGTH, DIM_INPUT)
+                # Get output data
+                gt_res_error = torch.tensor(np.array(model_norms_file.get('output_segment')))
                 gt_res_error = gt_res_error.view(1, DIM_OUTPUT)
+                # Stack the data segments
                 input_data = torch.cat((input_data, input_segment.float()), 0)
                 gt_data = torch.cat((gt_data, gt_res_error.float()), 0)
         return (input_data, gt_data)
