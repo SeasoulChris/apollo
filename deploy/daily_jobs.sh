354a371f5c970d54c5fdf5b0f0175d93b1bab20b
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-09-09 10:34:23 -0700

    Deploy: Use latest package for data jobs.

--- deploy/daily_jobs.sh
+++ deploy/daily_jobs.sh
@@ -9,14 +9,17 @@ set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 # The bag-to-record job is code-freezed.
+JOB="/apollo/modules/data/fuel/fueling/data/pipelines/bag_to_record.py"
 ./tools/submit-job-to-k8s.sh --workers 10 --memory 24g --disk 800 \
     --image "hub.baidubce.com/apollo/spark:ubuntu-14.04_spark-2.4.0" \
-    --fueling "/mnt/bos/modules/data/jobs/deploy/fueling-20190828.zip" \
-    "/apollo/modules/data/fuel/fueling/data/pipelines/bag_to_record.py"
+    --fueling "/mnt/bos/modules/data/jobs/deploy/fueling-latest.zip" \
+    ${JOB}
 
 # Job: Daily jobs.
 JOB="fueling/daily-jobs.py"
-./tools/submit-job-to-k8s.sh --workers 10 --memory 24g --disk 800 ${JOB}
+./tools/submit-job-to-k8s.sh --workers 10 --memory 24g --disk 800 \
+    --fueling "/mnt/bos/modules/data/jobs/deploy/fueling-latest.zip" \
+    ${JOB}
 
 # Job: Control profiling.
 JOB="fueling/profiling/control-profiling-metrics.py"
