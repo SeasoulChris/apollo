e0d4f1db9ae594bd9fbe1085578d477565af57b0
jiangyifei <jiangyifei@baidu.com>
2019-09-10 16:18:57 -0700

    updated calculation of stability.

--- fueling/planning/stability/imu_speed_acc.py
+++ fueling/planning/stability/imu_speed_acc.py
@@ -14,10 +14,21 @@ class ImuSpeedAcc:
     def add(self, location_est):
         self.imu_speed.add(location_est)
         speed_timestamp_list = self.imu_speed.get_timestamp_list()
-        if len(speed_timestamp_list) > 5:
+
+        index_50ms = len(speed_timestamp_list) - 1
+        found_index_50ms = False
+        last_timestamp = speed_timestamp_list[-1]
+        while index_50ms >= 0:
+            current_timestamp = speed_timestamp_list[index_50ms]
+            if (last_timestamp - current_timestamp) >= 0.05:
+                found_index_50ms = True
+                break
+            index_50ms -= 1
+
+        if found_index_50ms:
             speed_list = self.imu_speed.get_speed_list()
-            acc = (speed_list[-1] - speed_list[-5]) / \
-                  (speed_timestamp_list[-1] - speed_timestamp_list[-5])
+            acc = (speed_list[-1] - speed_list[index_50ms]) / \
+                  (speed_timestamp_list[-1] - speed_timestamp_list[index_50ms])
             self.acc_list.append(acc)
             self.timestamp_list.append(speed_timestamp_list[-1])
 
