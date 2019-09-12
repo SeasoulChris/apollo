6373c7c577d372a68c024f4464381191022d0437
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-09-11 17:28:24 -0700

    Control: Fix site-package to conda env path.

--- fueling/control/dynamic_model/gp_regression/gp_main.sh
+++ fueling/control/dynamic_model/gp_regression/gp_main.sh
@@ -9,7 +9,8 @@ set -x
 # Make sure you are calling run_prod() instead of run_test()!
 # Feature extraction.
 source activate fuel-py36-pyro
-export PYTHONPATH=$(python -m site --user-site):/apollo/modules/data/fuel
+SITE_PACKAGE=$(python -c "import site; print(site.getsitepackages()[0])")
+export PYTHONPATH=${SITE_PACKAGE}:/apollo/modules/data/fuel
 
 # These is another version of torch installed in /usr/local/apollo/libtorch_gpu.
 # We have to remove it from the LD_LIBRARY_PATH so we can use the one in our
