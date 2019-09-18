03d5e3562f6540b10f4461666f761bd282cdbacc
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-09-18 11:22:46 -0700

    Demo: Refactor gpu samples.

---
+++ fueling/demo/gpu_training_with_pytorch.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+"""
+A simple demo PySpark job with GPU training.
+
+Run with:
+    ./tools/submit-job-to-k8s.sh --gpu --env fuel-py36 fueling/demo/gpu_training_with_pytorch.py
+"""
+
+# Standard packages
+import os
+import sys
+import time
+
+# Third-party packages
+import torch
+
+# Apollo-fuel packages
+from fueling.common.base_pipeline import BasePipeline
+import fueling.common.logging as logging
+
+
+class PytorchTraining(BasePipeline):
+    """Demo pipeline."""
+
+    def __init__(self):
+        BasePipeline.__init__(self, 'PytorchTraining')
+
+    def run_test(self):
+        """Run test."""
+        logging.info('nvidia-smi on Driver:')
+        if os.system('nvidia-smi') != 0:
+            logging.fatal('Failed to run nvidia-smi.')
+            sys.exit(-1)
+
+        time_start = time.time()
+        self.to_rdd(range(1)).foreach(self.train)
+        logging.info('Training complete in {} seconds.'.format(time.time() - time_start))
+
+    def run_prod(self):
+        """Same with run_test()."""
+        self.run_test()
+
+    @staticmethod
+    def train(instance_id):
+        """Run training task"""
+        logging.info('nvidia-smi on Executor {}:'.format(instance_id))
+        if os.system('nvidia-smi') != 0:
+            logging.fatal('Failed to run nvidia-smi.')
+            sys.exit(-1)
+
+        logging.info('cuda available? {}'.format(torch.cuda.is_available()))
+        logging.info('cuda version: {}'.format(torch.version.cuda))
+        logging.info('gpu device count: {}'.format(torch.cuda.device_count()))
+
+        dtype = torch.float
+        # Or use 'cpu' for CPU training.
+        device = torch.device('cuda:0')
+
+        # N is batch size; D_in is input dimension;
+        # H is hidden dimension; D_out is output dimension.
+        N, D_in, H, D_out = 64, 1000, 100, 10
+
+        # Create random input and output data
+        x = torch.randn(N, D_in, device=device, dtype=dtype)
+        y = torch.randn(N, D_out, device=device, dtype=dtype)
+
+        # Randomly initialize weights
+        w1 = torch.randn(D_in, H, device=device, dtype=dtype)
+        w2 = torch.randn(H, D_out, device=device, dtype=dtype)
+
+        learning_rate = 1e-6
+        for t in range(100):
+            # Forward pass: compute predicted y
+            h = x.mm(w1)
+            h_relu = h.clamp(min=0)
+            y_pred = h_relu.mm(w2)
+
+            # Compute and print loss
+            loss = (y_pred - y).pow(2).sum().item()
+            logging.info(t, loss)
+
+            # Backprop to compute gradients of w1 and w2 with respect to loss
+            grad_y_pred = 2.0 * (y_pred - y)
+            grad_w2 = h_relu.t().mm(grad_y_pred)
+            grad_h_relu = grad_y_pred.mm(w2.t())
+            grad_h = grad_h_relu.clone()
+            grad_h[h < 0] = 0
+            grad_w1 = x.t().mm(grad_h)
+            # Update weights using gradient descent
+            w1 -= learning_rate * grad_w1
+            w2 -= learning_rate * grad_w2
+
+
+if __name__ == '__main__':
+    PytorchTraining().main()
---
+++ fueling/demo/gpu_training_with_tensorflow.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+"""
+A simple demo PySpark job with GPU training.
+
+Run with:
+    ./tools/submit-job-to-k8s.sh --gpu --env fuel-py36 fueling/demo/gpu_training_with_tensorflow.py
+"""
+
+# Standard packages
+import os
+import sys
+import time
+
+# Third-party packages
+from tensorflow.python.client import device_lib
+import tensorflow as tf
+
+# Apollo-fuel packages
+from fueling.common.base_pipeline import BasePipeline
+import fueling.common.logging as logging
+
+
+class TensorflowTraining(BasePipeline):
+    """Demo pipeline."""
+
+    def __init__(self):
+        BasePipeline.__init__(self, 'TensorflowTraining')
+
+    def run_test(self):
+        """Run test."""
+        logging.info('nvidia-smi on Driver:')
+        if os.system('nvidia-smi') != 0:
+            logging.fatal('Failed to run nvidia-smi.')
+            sys.exit(-1)
+
+        time_start = time.time()
+        self.to_rdd(range(1)).foreach(self.train)
+        logging.info('Training complete in {} seconds.'.format(time.time() - time_start))
+
+    def run_prod(self):
+        """Same with run_test()."""
+        self.run_test()
+
+    @staticmethod
+    def train(instance_id):
+        """Run training task"""
+        logging.info('nvidia-smi on Executor {}:'.format(instance_id))
+        if os.system('nvidia-smi') != 0:
+            logging.fatal('Failed to run nvidia-smi.')
+            sys.exit(-1)
+
+        if tf.test.gpu_device_name():
+            logging.info('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+            logging.info('GPU available? {}'.format(tf.test.is_gpu_available()))
+            logging.info('GPU devices: {}'.format(device_lib.list_local_devices()))
+        else:
+            logging.fatal('Cannot access GPU.')
+            sys.exit(-1)
+
+        mnist = tf.keras.datasets.mnist
+        (x_train, y_train), (x_test, y_test) = mnist.load_data('/mnt/bos/test/datasets/mnist.npz')
+        x_train, x_test = x_train / 255.0, x_test / 255.0
+
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(512, activation=tf.nn.relu),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+        model.fit(x_train, y_train, epochs=5)
+        model.evaluate(x_test, y_test)
+
+
+if __name__ == '__main__':
+    TensorflowTraining().main()
