6b59f315ae0ead74d21eb0c61f30dbc348d0496f
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-09-19 16:34:28 -0700

    Unset PYTHONPATH before activating conda env.

--- fueling/control/dynamic_model/gp_regression/gp_main.sh
+++ fueling/control/dynamic_model/gp_regression/gp_main.sh
@@ -8,9 +8,9 @@ set -x
 
 # Make sure you are calling run_prod() instead of run_test()!
 # Feature extraction.
+unset PYTHONPATH
 source activate fuel-py36-pyro
-SITE_PACKAGE=$(python -c "import site; print(site.getsitepackages()[0])")
-export PYTHONPATH=${SITE_PACKAGE}:/apollo/modules/data/fuel
+export PYTHONPATH=/apollo/modules/data/fuel:${PYTHONPATH}
 
 # These is another version of torch installed in /usr/local/apollo/libtorch_gpu.
 # We have to remove it from the LD_LIBRARY_PATH so we can use the one in our
