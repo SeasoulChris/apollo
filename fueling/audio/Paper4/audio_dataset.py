59635c6942046aa54c39fa6739ada9ea3cf5797e
kechxu <kechxu89@gmail.com>
2019-09-19 15:21:10 -0700

    audio: implement AudioDataset init()

--- fueling/audio/Paper4/audio_dataset.py
+++ fueling/audio/Paper4/audio_dataset.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python
 
 import numpy as np
+import os
 import torch
 from torch.utils.data import Dataset
 
 from fueling.audio.Paper3.pyAudioAnalysis import audioBasicIO
 from fueling.audio.Paper3.pyAudioAnalysis import audioFeatureExtraction
+from fueling.common import file_utils
 
 
 class AudioDataset(Dataset):
-    def __init__(self, data_dir, mode='cnn1d'):
-        # TODO(kechxu): load data from .wav files under data_dir
-        pass
+    def __init__(self, data_dir, mode='cnn1d', win_size=10, step=5):
+        self.features = []  # a list of spectrograms, each: [n_mels, win_size]
+        self.labels = []  # 1: emergency, 0: non-emergency
+        files = file_utils.list_files(data_dir)
+        for file in files:
+            signal, sr = librosa.load(fn, sr=8000)
+            signal = preprocess(signal)
+            S = librosa.feature.melspectrogram(signal, sr=sr, n_mels=128)
+            log_S = librosa.power_to_db(S, ref=np.max)
+            log_S = log_S.t()  # shape: [128, len]
+            label = 1
+            if file.find("nonEmergency") != -1:
+                label = 0
+            start = 0
+            while start + win_size <= log_S.shape[1]:
+                end = start + win_size
+                log_S_segment = log_S[:, start:end]
+                features.append(log_S_segment)
+                labels.append(label)
+                start += step
 
     def __len__(self):
-        # TODO(kechxu): return the size of the dataset
-        pass
+        return len(self.labels)
 
     def __getitem__(self, idx):
         # TODO(kechxu): return the data at idx
