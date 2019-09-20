9deb1d2f23bef91c73e78e71ca1c232852e592ce
kechxu <kechxu89@gmail.com>
2019-09-20 14:00:34 -0700

    audio: code for cnn training pipeline

--- fueling/audio/Paper4/audio_cnn.py
+++ fueling/audio/Paper4/audio_cnn.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import numpy as np
 import os
 
@@ -13,6 +14,10 @@ from torchvision import transforms
 from fueling.audio.Paper3.pyAudioAnalysis import audioBasicIO
 from fueling.audio.Paper3.pyAudioAnalysis import audioFeatureExtraction
 from fueling.common import file_utils
+from fueling.common.learning.train_utils import *
+from learning_algorithms.prediction.datasets.apollo_vehicle_trajectory_dataset.apollo_vehicle_trajectory_dataset import *
+from learning_algorithms.prediction.models.lane_attention_trajectory_model.lane_attention_trajectory_model import *
+from learning_algorithms.prediction.models.semantic_map_model.semantic_map_model import *
 
 
 class AudioDataset(Dataset):
@@ -122,4 +127,51 @@ class AudioCNN2dModel(nn.Module):
 
 
 if __name__ == "__main__":
-    print("hello")
+    # Set-up the GPU to use
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+    MODEL = 'cnn1d'  # cnn1d, cnn2d
+
+    # data parser:
+    parser = argparse.ArgumentParser(description='pipeline')
+    parser.add_argument('train_file', type=str, help='training data')
+    parser.add_argument('valid_file', type=str, help='validation data')
+    parser.add_argument('-s', '--save-path', type=str, default='./',
+                        help='Specify the directory to save trained models.')
+    args = parser.parse_args()
+
+    # Set-up data-loader
+    train_dataset = AudioDataset(args.train_file, MODEL)
+    valid_dataset = AudioDataset(args.valid_file, MODEL)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True,
+                                               num_workers=2, drop_last=True)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=64, shuffle=True,
+                                               num_workers=2, drop_last=True)
+
+    # Model and training setup
+    model = None
+    if MODEL == 'cnn1d':
+        model = AudioCNN1dModel()
+    elif MODEL == 'cnn2d';
+        model = AudioCNN2dModel()
+    print('------ Model Structure -------')
+    print(model)
+
+    loss = AudioLoss()
+
+    learning_rate = 3e-4
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, factor=0.3, patience=3, min_lr=1e-9, verbose=True, mode='min')
+
+    # CUDA setup:
+    if (torch.cuda.is_available()):
+        print ("Using CUDA to speed up training.")
+        model.cuda()
+    else:
+        print ("Not using CUDA.")
+
+    # Model training:
+    train_valid_dataloader(train_loader, valid_loader, model, loss, optimizer,
+                           scheduler, epochs=50, save_name='./', print_period=50)
