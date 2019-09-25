65c2bf2965c0621e30c688e75be9369fd009d615
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-09-25 11:56:14 -0700

    Control: Fix wrong use of os.path.join.

--- fueling/control/dynamic_model/dynamic_model_training.py
+++ fueling/control/dynamic_model/dynamic_model_training.py
@@ -40,9 +40,9 @@ class DynamicModelTraining(BasePipeline):
     def run_prod(self):
         dataset_dir = 'modules/control/learning_based_model/hdf5_training'
         if IS_BACKWARD:
-            prefix = os.path.join(dataset_dir, VEHICLE_ID, '/UniformDistributed/backward')
+            prefix = os.path.join(dataset_dir, VEHICLE_ID, 'UniformDistributed/backward')
         else:
-            prefix = os.path.join(dataset_dir, VEHICLE_ID, '/UniformDistributed/forward')
+            prefix = os.path.join(dataset_dir, VEHICLE_ID, 'UniformDistributed/forward')
         # RDD(file_path) for training dataset
         training_dataset_rdd = self.to_rdd(self.bos().list_files(prefix, '.hdf5'))
         output_dir = bos_client.BosClient().abs_path(
