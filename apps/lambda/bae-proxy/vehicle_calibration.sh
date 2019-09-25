62c605d7792be6a35265a3b4cd379f502892b0d8
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-09-25 11:44:39 -0700

    Apps: Sync vehicle-calibration proxy with HEAD.

--- apps/lambda/bae-proxy/vehicle_calibration.sh
+++ apps/lambda/bae-proxy/vehicle_calibration.sh
@@ -4,10 +4,12 @@ INPUT_DATA_PATH=$1
 BASH_ARGS=$2
 PY_ARGS=$3
 
+# Go to apollo-fuel root.
+cd /apollo/modules/data/fuel
+
 set -e
 
 export PATH=/usr/local/miniconda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-cd /apollo/modules/data/fuel
 
 FUELING="/mnt/bos/modules/data/jobs/deploy/fueling-latest.zip"
 SUBMITER="./tools/submit-job-to-k8s.sh --fueling ${FUELING} ${BASH_ARGS}"
@@ -18,17 +20,7 @@ ENV="fuel-py27-cyber"
 ${SUBMITER} --env ${ENV} --workers 10 --cpu 1 --memory 40g ${JOB} ${PY_ARGS} \
     --input_data_path="${INPUT_DATA_PATH}"
 
-# Training.
-JOB="./fueling/control/calibration_table/multi_job_train.py"
-ENV="fuel-py27"
+# Training, result visualization and distribution.
+JOB="./fueling/control/calibration_table/multi_job_train_vis_dist.py"
+ENV="fuel-py36"
 ${SUBMITER} --env ${ENV} --workers 5 --cpu 5 --memory 60g ${JOB} ${PY_ARGS}
-
-# Result visualization
-JOB="./fueling/control/calibration_table/multi_job_result_visualization.py"
-ENV="fuel-py27"
-${SUBMITER} --env ${ENV} --workers 10 --cpu 1 --memory 20g ${JOB} ${PY_ARGS}
-
-# Data distribution visualization
-JOB="./fueling/control/calibration_table/multi_job_data_distribution.py"
-ENV="fuel-py27"
-${SUBMITER} --env ${ENV} --workers 10 --cpu 1 --memory 20g ${JOB} ${PY_ARGS}
