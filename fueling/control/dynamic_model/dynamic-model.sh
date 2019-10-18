5eeb90d2ac822465c0763587cbc82a866442be4e
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-10-18 11:07:57 -0700

    Conda: Retire unused learning libs from py27-cyber.

--- fueling/control/dynamic_model/dynamic-model.sh
+++ fueling/control/dynamic_model/dynamic-model.sh
@@ -14,15 +14,15 @@ ENV="fuel-py27-cyber"
 
 # Training.
 JOB="fueling/control/dynamic_model/dynamic_model_training.py"
-ENV="fuel-py27"
+ENV="fuel-py36"
 ./tools/submit-job-to-k8s.sh --env ${ENV} --workers 2 --cpu 20 --memory 100g ${JOB}
 
 # Model Evaluation
 JOB="fueling/control/dynamic_model/dynamic_model_evaluation.py"
-ENV="fuel-py27"
+ENV="fuel-py36"
 ./tools/submit-job-to-k8s.sh --env ${ENV} --workers 16 --cpu 2 --memory 20g ${JOB}
 
 # Training Data Visualization
 JOB="fueling/control/dynamic_model/dynamic_model_data_visualization.py"
-ENV="fuel-py27"
+ENV="fuel-py36"
 ./tools/submit-job-to-k8s.sh --env ${ENV} --workers 2 --cpu 20 --memory 100g ${JOB}
