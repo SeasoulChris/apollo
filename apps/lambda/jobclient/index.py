86baa5d481b4a75a9cda99f9f88ddc29abd13703
fengzongbao <fengzongbao@baidu.com>
2019-11-01 10:18:27 +0800

    update two blank lines after header section

--- apps/lambda/jobclient/index.py
+++ apps/lambda/jobclient/index.py
@@ -11,8 +11,8 @@ import requests
 
 from modules.tools.fuel_proxy.proto.job_config_pb2 import BosConfig, JobConfig
 
-app = flask.Flask(__name__)
 
+app = flask.Flask(__name__)
 flags.DEFINE_string('fuel_proxy', 'https://apollofuel0.bceapp.com:8443',
                     'Endpoint of Apollo-Fuel proxy.')
 
