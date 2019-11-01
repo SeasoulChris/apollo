8588881f8f145ec3a629b31f4077368b579ffa0a
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-11-01 11:40:35 -0700

    Apps: Simplify warehouse deployment.

--- apps/k8s/warehouse/deploy/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source /usr/local/miniconda/bin/activate fuel-py37
-export PYTHONPATH="/apollo/py_proto:/apollo/modules/data/fuel:${PYTHONPATH}"
-
-cd /apollo/modules/data/fuel/apps/k8s/warehouse
-python index.py
