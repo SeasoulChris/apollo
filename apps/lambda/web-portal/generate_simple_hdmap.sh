967fea0c309ccc15b601a4e3cf67ca8564d8cf72
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-11-13 14:12:33 -0800

    Apps: Migrate generate_simple_hdmap service to the all-in-one py36 pipeline.

--- apps/lambda/web-portal/generate_simple_hdmap.sh
+++ apps/lambda/web-portal/generate_simple_hdmap.sh
@@ -9,27 +9,11 @@ PY_ARGS=$5
 # Go to apollo-fuel root.
 cd /apollo/modules/data/fuel
 
-set -e
-
 export PATH=/usr/local/miniconda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 FUELING="/mnt/bos/modules/data/jobs/deploy/fueling-latest.zip"
-SUBMITER="tools/submit-job-to-k8s.sh --partner_storage_writable --fueling ${FUELING} ${BASH_ARGS}"
-
-# Job: map generate base_map.txt
-JOB="fueling/map/generate_base_map.py"
-ENV="fuel-py27-cyber"
-${SUBMITER} --env ${ENV} --workers 5 --cpu 5 --memory 40g ${JOB} ${PY_ARGS} \
-    --input_data_path="${INPUT_DATA_PATH}"
-
-# Job: map generate sim_map.txt or routing_map.txt
-JOB="fueling/map/generate_sim_routing_map.py"
-ENV="fuel-py27-cyber"
-${SUBMITER} --env ${ENV} --workers 5 --cpu 5 --memory 60g ${JOB} ${PY_ARGS} \
-    --input_data_path="${INPUT_DATA_PATH}"
-
-# Job: map generate local_map
-JOB="fueling/map/generate_local_map.py"
-ENV="fuel-py27-cyber"
-${SUBMITER} --env ${ENV} --workers 5 --cpu 5 --memory 60g ${JOB} ${PY_ARGS} \
+SUBMITTER="tools/submit-job-to-k8s.sh --partner_storage_writable --fueling ${FUELING} ${BASH_ARGS}"
+JOB="fueling/map/generate_maps.py"
+ENV="fuel-py36"
+${SUBMITTER} --env ${ENV} --workers 5 --cpu 5 --memory 40g ${JOB} ${PY_ARGS} \
     --input_data_path="${INPUT_DATA_PATH}" --zone_id=$ZONE_ID --lidar_type="$LIDAR_TYPE"
