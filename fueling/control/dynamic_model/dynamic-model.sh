749dfea5834035aff7d8b51826a6e63e4b4b9b8e
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-11-13 14:20:15 -0800

    Control: Migrate everything to py36.

--- fueling/control/dynamic_model/dynamic-model.sh
+++ fueling/control/dynamic_model/dynamic-model.sh
@@ -6,20 +6,21 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/../../.."
 set -e
 set -x
 
+SUBMITTER="./tools/submit-job-to-k8s.py --wait"
 # Make sure you are calling run_prod() instead of run_test()!
 # Feature extraction.
 JOB="fueling/control/feature_extraction/sample_set_feature_extraction.py"
 ENV="fuel-py27-cyber"
-./tools/submit-job-to-k8s.py --main=${JOB} --workers=16 --cpu=2 --memory=20 --env=${ENV}
+${SUBMITTER} --main=${JOB} --workers=16 --cpu=2 --memory=20
 
 # Training.
 JOB="fueling/control/dynamic_model/dynamic_model_training.py"
-./tools/submit-job-to-k8s.py --main=${JOB} --workers=2 --cpu=20 --memory=100
+${SUBMITTER} --main=${JOB} --workers=2 --cpu=20 --memory=100
 
 # Model Evaluation
 JOB="fueling/control/dynamic_model/dynamic_model_evaluation.py"
-./tools/submit-job-to-k8s.py --main=${JOB} --workers=16 --cpu=2 --memory=20
+${SUBMITTER} --main=${JOB} --workers=16 --cpu=2 --memory=20
 
 # Training Data Visualization
 JOB="fueling/control/dynamic_model/dynamic_model_data_visualization.py"
-./tools/submit-job-to-k8s.py --main=${JOB} --workers=2 --cpu=20 --memory=100
+${SUBMITTER} --main=${JOB} --workers=2 --cpu=20 --memory=100
--- fueling/control/dynamic_model/feature_extraction/feature_extraction.sh
+++ fueling/control/dynamic_model/feature_extraction/feature_extraction.sh
@@ -6,13 +6,12 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/../../../.."
 set -e
 JOB_ID=$(date +%Y-%m-%d-%H)
 
-SUBMITTER="./tools/submit-job-to-k8s.py --env=fuel-py27-cyber --workers=5 --cpu=2 --memory=60"
+SUBMITTER="./tools/submit-job-to-k8s.py --workers=5 --cpu=2 --memory=60 --wait"
 
 # Feature extraction.
 JOB="fueling/control/dynamic_model/feature_extraction/sample_set.py"
 INPUT_DATA_PATH="modules/control/data/records"
 ${SUBMITTER} --main=${JOB} --flags="--input_data_path=${INPUT_DATA_PATH} --job_id=${JOB_ID}"
-
 
 # Feature extraction.
 JOB="fueling/control/dynamic_model/feature_extraction/uniform_set.py"
