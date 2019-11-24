49e4dced6d9cfb215af2ab3ae21f4d3b1bea4d30
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-11-24 13:18:47 -0800

    Common: Add bos_bucket to partner info.

--- fueling/common/partners.py
+++ fueling/common/partners.py
@@ -3,13 +3,17 @@
 
 import collections
 
-
-Partner = collections.namedtuple('Partner', ['name', 'email'])
+_fields = ['name', 'email', 'bos_bucket', 'blob_container']
+Partner = collections.namedtuple('Partner', _fields)
+Partner.__new__.__defaults__ = (None,) * len(_fields)
 
 partners = {
-    'apollo': Partner(name='Apollo', email='xiaoxiangquan@baidu.com'),
-    'apollo-evangelist': Partner(name='Evangelist', email='machao20@baidu.com'),
-    'apollo-qa': Partner(name='QA', email='taoshengzhao01@baidu.com'),
+    'apollo': Partner(
+        name='Apollo', email='xiaoxiangquan@baidu.com', bos_bucket='apollo-platform'),
+    'apollo-evangelist': Partner(
+        name='Evangelist', email='machao20@baidu.com', bos_bucket='apollo-evangelist'),
+    'apollo-qa': Partner(
+        name='QA', email='taoshengzhao01@baidu.com', bos_bucket='apollo-platform'),
     'udelv2019': Partner(name='Udelv', email='xiaoxiangquan@baidu.com'),
     'coolhigh': Partner(name='Coolhigh', email='mhchenm@coolhigh.com.cn'),
 }
