7c54ca2aa40a703857364081204951b3d1a56f97
michael4338 <michael4338@hotmail.com>
2019-12-02 11:33:56 -0800

    adjust import sequence

--- fueling/perception/YOLOv3/model_training_pipeline_jobs.py
+++ fueling/perception/YOLOv3/model_training_pipeline_jobs.py
@@ -2,9 +2,9 @@
 """Wrapper of perception model training pipeline jobs"""
 
 from fueling.common.base_pipeline import SequentialPipeline
-from fueling.perception.YOLOv3.yolov3_training import Yolov3Training
-from fueling.perception.YOLOv3.yolov3_inference import Yolov3Inference
 from fueling.perception.YOLOv3.yolov3_evaluate import Yolov3Evaluate
+from fueling.perception.YOLOv3.yolov3_inference import Yolov3Inference
+from fueling.perception.YOLOv3.yolov3_training import Yolov3Training
 
 
 if __name__ == '__main__':
