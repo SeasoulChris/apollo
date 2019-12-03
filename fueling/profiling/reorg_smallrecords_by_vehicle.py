f33efe19abf65c801d337729e93adae0da649cfc
fengzongbao <mickeyouou@gmail.com>
2019-12-03 13:53:46 +0800

    Profiling: update rdd logic

--- fueling/profiling/reorg_smallrecords_by_vehicle.py
+++ fueling/profiling/reorg_smallrecords_by_vehicle.py
@@ -31,35 +31,33 @@ class ReorgSmallRecords(BasePipeline):
         self.run(records_dir, origin_prefix, target_prefix)
 
     def run_prod(self):
-        input_prefix = 'small-records/2019'
+        input_prefix = 'small-records/2019/2019-02-25'
         origin_dir = self.our_storage().abs_path(input_prefix)
 
         target_prefix = 'modules/control/small-records'
         target_dir = self.our_storage().abs_path(target_prefix)
 
-        # TODO: test get vehicles by dir mongo_utils, get empty now
-
-        self.get_vehicles(
-            'small-records/2019/2019-11-27/2019-11-27-10-25-44/20191127102544.record')
-
-        self.get_vehicles(
-            'small-records/2019/2019-02-25/2019-02-25-16-24-27/20190225163700.record')
-
-        self.get_vehicles(
-            'small-records/2019/2019-02-25/2019-02-25-16-24-27')
-
-        exit(1)
+        # self.get_vehicles(
+        # '/mnt/bos/small-records/2019/2019-02-25/2019-02-25-16-24-27')
 
         # RDD(small-records dir)
         origin_small_records_dir = spark_helper.cache_and_log(
             'origin_small_records_dir',
+            # RDD(file), start with origin_prefix
             self.to_rdd(self.our_storage().list_files(input_prefix))
+            # RDD(record_file)
             .filter(record_utils.is_record_file)
-            .map(lambda record: os.path.dirname(record))
+            # RDD(record_dir), with record_file inside
+            .map(os.path.dirname)
+            # RDD(record_dir), which is unique
             .distinct()
+            .map(self.get_vehicles)
         )
-        logging.info('origin_small_records_dir %s ' %
-                     origin_small_records_dir.take(10))
+
+        logging.info('origin_small_records_dir {}'.format(
+            origin_small_records_dir.collect()))
+
+        # self.get_vehicles(origin_small_records_dir.collect())
 
         # processed_dirs = spark_helper.cache_and_log(
         #     'processed_dirs',
@@ -84,56 +82,65 @@ class ReorgSmallRecords(BasePipeline):
 
     def run(self, record_dir_rdd, origin_prefix, target_prefix):
         """Run the pipeline with given arguments."""
-        result = (
+        result = spark_helper.cache_and_log(
+            'result',
             # RDD(record_dir)
             record_dir_rdd
             # PairRDD(record_dir, vehicle_name)
-            .mapPartitions(self.get_vehicles)
+            .map(self.get_vehicles)
             # RDD(0/1), 1 for success
             .map(lambda dir_map: self.process_dir(
                 dir_map[0],
                 dir_map[0].replace(origin_prefix,
                                    os.path.join(target_prefix, dir_map[1] + '/'), 1),
                 dir_map[1], origin_prefix))
-            .cache()
         )
 
-        logging.info(
-            'Processed {}/{} tasks'.format(result.reduce(operator.add), result.count()))
+        # logging.info('result %s' % result.collect())
+
+        # logging.info(
+        # 'Processed {}/{} tasks'.format(result.reduce(operator.add), result.count()))
 
     def process_dir(self, record_dir, target_dir, vehicle, origin_dir):
         # record_dir /mnt/bos/small-records/2019/2019-01-03/2019-01-03-15-49-43
         # target_dir /mnt/bos/modules/control/small-records/Mkz7/2019-01-03/2019-01-03-15-49-43
 
+        logging.info('record_dir{}, target_dir{}, vehicle {}'.format(
+            record_dir, target_dir, vehicle))
         # create target vehicles directory
-        file_utils.makedirs(target_dir)
+        # file_utils.makedirs(target_dir)
 
-        # Copy record files
-        shutil.copytree(record_dir, target_dir)
+        # # Copy record files
+        # shutil.copytree(record_dir, target_dir)
 
-        # Copy vehicle_parameter.pb.txt /mnt/bos/modules/control/control_conf/mkz6/vehicle_param.pb.txt
-        control_conf_prefix = '/mnt/bos/modules/control/control_conf'
-        sorce_conf_dir = os.path.join(
-            record_dir.replace(origin_dir, control_conf_prefix, 1), feature_utils.CONF_FILE)
-        target_conf_dir = os.path.join(
-            control_conf_prefix, vehicle, feature_utils.CONF_FILE)
-        shutil.copyfile(sorce_conf_dir, target_conf_dir)
+        # # Copy vehicle_parameter.pb.txt /mnt/bos/modules/control/control_conf/mkz6/vehicle_param.pb.txt
+        # control_conf_prefix = '/mnt/bos/modules/control/control_conf'
+        # sorce_conf_dir = os.path.join(
+        #     record_dir.replace(origin_dir, control_conf_prefix, 1), feature_utils.CONF_FILE)
+        # target_conf_dir = os.path.join(
+        #     control_conf_prefix, vehicle, feature_utils.CONF_FILE)
+        # shutil.copyfile(sorce_conf_dir, target_conf_dir)
 
-        # Touch Reorg tag
-        # TODO(zongbao): need to add filter in multi_job_control_profiling_metrics.py
-        file_utils.touch(os.path.join(target_dir, 'REORG_COMPLETE'))
+        # # Touch Reorg tag
+        # # TODO(zongbao): need to add filter in multi_job_control_profiling_metrics.py
+        # file_utils.touch(os.path.join(target_dir, 'REORG_COMPLETE'))
 
     def get_vehicles(self, record_dirs):
         """Return the (record_dir, vehicle_name) pair"""
+        logging.info('input record_dirs %s' % record_dirs)
         record_dirs = list(record_dirs)
-        collection = Mongo().record_collection()
+        # record_dirs ['/mnt/bos/small-records/2019/2019-02-25/2019-02-25-16-24-27',...]
+        logging.info('after handling record_dirs %s' % record_dirs)
         dir_vehicle_dict = db_backed_utils.lookup_vehicle_for_dirs(
-            record_dirs, collection)
+            record_dirs, self.collection)
+        # dir_vehicle_dict{'/mnt/bos/small-records/2019/2019-02-25/2019-02-25-16-24-27': 'Transit',
+        # '/mnt/bos/small-records/2019/2019-02-25/2019-02-25-16-18-12': 'Transit'}
         logging.info('dir_vehicle_dict%s' % dir_vehicle_dict)
         dir_vehicle_list = []
         for record_dir, vehicle in dir_vehicle_dict.items():
             # record_dir, Mkz7
             dir_vehicle_list.append((record_dir, vehicle))
+        print(dir_vehicle_list)
         return dir_vehicle_list
 
 
