46931237b36d222bd1cfc8560fbf9516eebc46b8
kevin-y-wang <yuwang01@baidu.com>
2019-12-04 13:47:37 -0800

    Profiling: refactor the control profiling base file to cover both metrics/visualization

--- fueling/profiling/multi_jobs_control_profiling.sh
+++ fueling/profiling/multi_jobs_control_profiling.sh
@@ -6,6 +6,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/../.."
 SUBMITTER="./tools/submit-job-to-k8s.py --workers=6 --cpu=4 --memory=60"
 INPUT_DATA_PATH="modules/control/profiling/multi_job"
 
-JOB="fueling/profiling/multi_job_control_profiling_metrics.py"
+JOB="fueling/profiling/control_profiling.py"
 FLAGS="--input_data_path=${INPUT_DATA_PATH}"
 ${SUBMITTER} --main=${JOB} --flags="${FLAGS}"
