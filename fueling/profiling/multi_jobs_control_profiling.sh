ed7b41a13be7dcb80a3e371906176256a53decb8
kevin-y-wang <yuwang01@baidu.com>
2019-12-05 21:15:14 -0800

    Profiling: refactor the bash file to allow positional parameter

--- fueling/profiling/multi_jobs_control_profiling.sh
+++ fueling/profiling/multi_jobs_control_profiling.sh
@@ -4,7 +4,11 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )/../.."
 
 SUBMITTER="./tools/submit-job-to-k8s.py --workers=6 --cpu=4 --memory=60"
-INPUT_DATA_PATH="modules/control/profiling/multi_job"
+if [ "$1" != "" ]; then
+  INPUT_DATA_PATH=$1
+else
+  INPUT_DATA_PATH="modules/control/profiling/multi_job"
+fi
 
 JOB="fueling/profiling/control_profiling.py"
 FLAGS="--input_data_path=${INPUT_DATA_PATH}"
