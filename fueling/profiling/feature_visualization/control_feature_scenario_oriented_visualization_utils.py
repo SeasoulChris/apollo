0168bf78aba95c6f6e676e50ec754f1bdb8b21f9
kevin-y-wang <yuwang01@baidu.com>
2019-12-18 16:40:22 -0800

    Profiling: refactor the scenario-oriented control profiling utils

--- fueling/profiling/feature_visualization/control_feature_scenario_oriented_visualization_utils.py
+++ fueling/profiling/feature_visualization/control_feature_scenario_oriented_visualization_utils.py
@@ -20,9 +20,13 @@ import fueling.common.logging as logging
 # Minimum epsilon value used in compare with zero
 MIN_EPSILON = 0.000001
 # Minimum timestamp used for feature plotting
-MIN_TIME = 0.0
+MIN_TIME = 0
 # Maximum timestamp used for feature plotting
-MAX_TIME = 60.0
+MAX_TIME = 120
+# Customized starting position-x used for feature plotting
+START_POS_X = float('NaN')
+# Customized starting position-y for feature plotting
+START_POS_Y = float('NaN')
 
 
 def generate_segments(h5s):
@@ -32,12 +36,12 @@ def generate_segments(h5s):
         logging.warning('No hdf5 files found under the targeted path.')
         return segments
     for h5 in h5s:
-        logging.info('Loading {}'.format(h5))
+        logging.info(F'Loading {h5}')
         with h5py.File(h5, 'r+') as h5file:
             for value in h5file.values():
                 segments.append(np.array(value))
-    print('Segments width is: ', len(segments[0]))
-    print('Segments length is: ', len(segments))
+    print(F'Segments width is: {len(segments[0])}')
+    print(F'Segments length is: {len(segments)}')
     return segments
 
 
@@ -50,7 +54,7 @@ def generate_data(segments):
     data.append(segments[0])
     for i in range(1, len(segments)):
         data = np.vstack([data, segments[i]])
-    print('Data_Set length is: ', len(data))
+    print(F'Data_Set length is: {len(data)}')
     return data
 
 
@@ -59,7 +63,7 @@ def plot_features_vs_time(data_plot_x, data_plot_y, label_plot, features, title_
     for i in range(0, len(data_plot_x)):
         for j in range(0, len(data_plot_y)):
             plt.plot(data_plot_x[i], data_plot_y[j][i],
-                     label=(label_plot[j][i] + ": " + features[j]), linewidth=0.2)
+                     label=label_plot[j][i], linewidth=0.2)
     label_features = features[0]
     title_features = FEATURE_NAMES[FEATURE_IDX[features[0]]]
     if len(features) > 1:
@@ -81,8 +85,7 @@ def plot_feature_vs_feature(data_plot_x, data_plot_y, label_plot, features, titl
         return
     for i in range(0, len(data_plot_x)):
         plt.plot(data_plot_x[i], data_plot_y[i],
-                 label=(label_plot[i] + ": " + features[0] + " - " + features[1]),
-                 linewidth=0.2)
+                 label=label_plot[i], linewidth=0.2)
     plt.xlabel(features[0])
     plt.ylabel(features[1])
     plt.title(FEATURE_NAMES[FEATURE_IDX[features[0]]] + " v.s. " +
@@ -90,6 +93,16 @@ def plot_feature_vs_feature(data_plot_x, data_plot_y, label_plot, features, titl
               fontsize=8)
     plt.legend(fontsize=6)
     plt.tight_layout()
+
+
+def customize_start_time(array):
+    if np.isnan(START_POS_X) or np.isnan(START_POS_Y):
+        start_time = np.amin(array[:, FEATURE_IDX["timestamp_sec"]], axis=0)
+    else:
+        dist_from_start = ((array[:, FEATURE_IDX["reference_position_x"]] - START_POS_X) ** 2 +
+                           (array[:, FEATURE_IDX["reference_position_y"]] - START_POS_Y) ** 2)
+        start_time = array[np.argmin(dist_from_start, axis=0), FEATURE_IDX["timestamp_sec"]]
+    return start_time
 
 
 def plot_h5_features_per_scenario(data_list):
@@ -100,11 +113,11 @@ def plot_h5_features_per_scenario(data_list):
     for list in data_list:
         dir, array = list
         if len(array) == 0:
-            logging.warning('No data from hdf5 files can be visualized under the path {}'
-                            .format(dir))
+            logging.warning(F'No data from hdf5 files can be visualized under the path {dir}')
             return
-        lower_condition = (array[:, FEATURE_IDX["timestamp_sec"]] >= MIN_TIME)
-        upper_condition = (array[:, FEATURE_IDX["timestamp_sec"]] <= MAX_TIME)
+        timestamp_start = customize_start_time(array)
+        lower_condition = (array[:, FEATURE_IDX["timestamp_sec"]] >= MIN_TIME + timestamp_start)
+        upper_condition = (array[:, FEATURE_IDX["timestamp_sec"]] <= MAX_TIME + timestamp_start)
         array = np.take(array, np.where(lower_condition & upper_condition)[0], axis=0)
         data.append(array[np.argsort(array[:, FEATURE_IDX["timestamp_sec"]])])
         dir_data.append(dir)
@@ -121,14 +134,35 @@ def plot_h5_features_per_scenario(data_list):
     with PdfPages(pdffile) as pdf:
         # Plot features vs timestap
         plot_features = [["station_error"], ["speed_error"], ["lateral_error"],
-                         ["heading_error"], ["steering_cmd", "steering_chassis"]]
+                         ["heading_error"], ["steering_cmd", "steering_chassis"],
+                         ["curvature_reference"]]
         for features in plot_features:
             title_addon = str(len(data)) + " test cases"
-            data_plot_x = [array[:, FEATURE_IDX["timestamp_sec"]] for array in data]
+            data_plot_x = [array[:, FEATURE_IDX["timestamp_sec"]] -
+                           array[0, FEATURE_IDX["timestamp_sec"]] for array in data]
             data_plot_y = []
             label_plot = []
             for feature in features:
                 data_plot_y_sub = [array[:, FEATURE_IDX[feature]] for array in data]
+                data_plot_y.append(data_plot_y_sub)
+                label_plot_sub = [os.path.basename(dir) for dir in dir_data]
+                label_plot.append(label_plot_sub)
+            plt.figure(figsize=(4, 4))
+            plot_features_vs_time(data_plot_x, data_plot_y, label_plot, features, title_addon)
+            pdf.savefig()
+            plt.close()
+        # Plot (time-differential) features vs timestap
+        plot_features = [["curvature_reference"]]
+        for features in plot_features:
+            title_addon = 'rate of change, ' + str(len(data)) + " test cases"
+            data_plot_x = [array[0:-1, FEATURE_IDX["timestamp_sec"]] -
+                           array[0, FEATURE_IDX["timestamp_sec"]] for array in data]
+            data_plot_y = []
+            label_plot = []
+            for feature in features:
+                data_plot_y_sub = [np.diff(array[:, FEATURE_IDX[feature]], axis=0) /
+                                   np.diff(array[:, FEATURE_IDX["timestamp_sec"]], axis=0)
+                                   for array in data]
                 data_plot_y.append(data_plot_y_sub)
                 label_plot_sub = [os.path.basename(dir) for dir in dir_data]
                 label_plot.append(label_plot_sub)
