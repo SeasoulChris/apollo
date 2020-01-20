2a9527e144422e477be786c3968250c97f22b497
Qi Luo <qi.stephen.luo@gmail.com>
2020-01-20 14:11:33 -0800

    AutoTuner: add example interface

--- fueling/autotuner/main_optuna.py
+++ fueling/autotuner/main_optuna.py
@@ -2,13 +2,30 @@ import optuna
 
 
 def objective(trial):
-    x = trial.suggest_uniform('x', -10, 10)
-    return (x - 2) ** 2
+    config = trial.suggest_uniform('x', -10, 10)
+
+    tuner = MracAutoTuner()
+    tuner.set_config(list_of_pairs)  # {path, config}
+    tuner.main()
+    # 1. Config generations
+    # 2. Send / Call replay_engine
+    # 3. Gather bags.
+    # 4. Call profiling.
+    # 5. Return weighted scores.
+    list_of_scores = tuner.get_scores()  # {config, scenario -> score}
+    return weighted_score(scores)
+    # return (x - 2) ** 2
 
 
-study = optuna.create_study()
+def weighted_score(scores):
+    return 1
 
 
+sampler_marc = MRACSampler()
+
+study = optuna.create_study(sampler=sampler_marc)
+
+# Optionally can run in parellel for trainig, using SQLite or MySQL.
 study.optimize(objective, n_trials=100)
 
 
