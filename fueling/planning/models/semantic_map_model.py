d09765c97bc4136a893bbb95b8f702baa296d0c7
Jiangtao Hu <boris.hu@gmail.com>
2020-01-29 15:54:07 -0800

    planning: add semantic map model and pipeline skeleton.

---
+++ fueling/planning/models/semantic_map_model.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import glob
+import os
+
+import cv2 as cv
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torchvision import models
+# from torchvision import transforms
+
+from fueling.common.coord_utils import CoordUtils
+from fueling.common.learning.network_utils import *
+from fueling.common.learning.train_utils import *
+
+'''
+========================================================================
+Model definition
+========================================================================
+'''
+
+
+class SemanticMapModel(nn.Module):
+    def __init__(self, num_pred_points, num_history_points,
+                 cnn_net=models.mobilenet_v2, pretrained=True):
+        super(SemanticMapModel, self).__init__()
+
+        self.cnn = cnn_net(pretrained=pretrained)
+        self.num_pred_points = num_pred_points
+        fc_in_features = self.cnn.fc.in_features
+        self.cnn = nn.Sequential(*list(self.cnn.children())[:-1])
+        for param in self.cnn.parameters():
+            param.requires_grad = True
+
+        self.fc = nn.Sequential(
+            nn.Linear(fc_in_features + num_history_points * 2, 500),
+            nn.Dropout(0.3),
+            nn.Linear(500, 120),
+            nn.Dropout(0.3),
+            nn.Linear(120, num_pred_points * 2)
+        )
+
+    def forward(self, X):
+        img, obs_pos, _, _ = X
+        out = self.cnn(img)
+        out = out.view(out.size(0), -1)
+        obs_pos = obs_pos.view(obs_pos.size(0), -1)
+        out = torch.cat([out, obs_pos], -1)
+        out = self.fc(out)
+        out = out.view(out.size(0), self.num_pred_points, 2)
+        return out
+
+
+class SemanticMapLoss():
+    def loss_fn(self, y_pred, y_true):
+        loss_func = nn.MSELoss()
+        return loss_func(y_pred, y_true)
+
+    def loss_info(self, y_pred, y_true):
+        out = y_pred - y_true
+        out = torch.sqrt(torch.sum(out ** 2, 2))
+        out = torch.mean(out)
+        return out
---
+++ fueling/planning/pipelines/semantic_map_pipeline.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import argparse
+
+import torch
+
+from fueling.common.learning.train_utils import *
+from fueling.common.learning.loss_utils import *
+from fueling.planning.datasets.semantic_map_dataset import *
+from fueling.planning.models.semantic_map_model import *
+
+
+if __name__ == "__main__":
+    # Set-up the GPU to use
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+    IMG_MODE = True
+
+    # data parser:
+    parser = argparse.ArgumentParser(description='pipeline')
+    parser.add_argument('train_file', type=str, help='training data')
+    parser.add_argument('valid_file', type=str, help='validation data')
+    parser.add_argument('-s', '--save-path', type=str, default='./',
+                        help='Specify the directory to save trained models.')
+    args = parser.parse_args()
+
+    # Set-up data-loader
+    train_dataset = SemanticMapDataset(args.train_file, IMG_MODE)
+    valid_dataset = SemanticMapDataset(args.valid_file, IMG_MODE)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True,
+                                               num_workers=16, drop_last=True)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=32, shuffle=True,
+                                               num_workers=16, drop_last=True)
+
+    # Model and training setup
+    model = SemanticMapModel(30, 20, num_modes=2)
+    loss = SemanticMapLoss()
+
+    learning_rate = 3e-4
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, factor=0.3, patience=3, min_lr=1e-9, verbose=True, mode='min')
+
+    # CUDA setup:
+    if (torch.cuda.is_available()):
+        print ("Using CUDA to speed up training.")
+        model.cuda()
+    else:
+        print ("Not using CUDA.")
+
+    # Model training:
+    train_valid_dataloader(train_loader, valid_loader, model, loss, optimizer,
+                           scheduler, epochs=50, save_name='./', print_period=50)
