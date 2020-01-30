e53021abf56016253cca03b3ca1d93fe1e423afe
kevin-y-wang <yuwang01@baidu.com>
2020-01-29 16:20:16 -0800

    Auto Tuner: simplest control profiling score calculation logic

--- fueling/autotuner/cost_computation/mrac_cost_computation.py
+++ fueling/autotuner/cost_computation/mrac_cost_computation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-import random
 from datetime import datetime
+import glob
+import json
+import os
 
 import modules.data.fuel.fueling.autotuner.proto.cost_computation_service_pb2 as cost_service_pb2
 
@@ -14,6 +16,8 @@ class MracCostComputation(BaseCostComputation):
     def __init__(self):
         BaseCostComputation.__init__(self)
         self.model_configs = None
+        # TODO(Yu): add judgement to decide which submit-job tool is needed
+        self.submit_job_cmd = "python ./tools/submit-job-to-local.py"
 
     def init(self):
         BaseCostComputation.init(self)
@@ -40,11 +44,31 @@ class MracCostComputation(BaseCostComputation):
         return self.to_rdd(self.model_configs)
 
     def calculate_individual_score(self, input):
-        # TODO: implement me
         (key, bag_path) = input
         logging.info(f"Calculating score for: {bag_path}")
-        random.seed(datetime.now())
-        return (key, [random.random(), random.random()])
+        # submit the profiling job
+        profiling_func = f"fueling/profiling/control/multi_job_control_profiling_metrics.py"
+        profiling_flags = (f"--ctl_metrics_input_path_local={bag_path} "
+                           f"--ctl_metrics_output_path_local={bag_path} "
+                           f"--ctl_metrics_simulation_only_test='True' ")
+        profiling_cmd = (f"{self.submit_job_cmd} "
+                         f"--main={profiling_func} --flags=\"{profiling_flags}\"")
+        # verify exit status of the profiling job
+        exit_code = os.system(profiling_cmd)
+        if os.WEXITSTATUS(exit_code) != 0:
+            logging.error(f"Fail to submit the control profiling job with "
+                          f"error code {os.WEXITSTATUS(exit_code)}")
+            return (key, [float('nan'), 0])
+        # extract the profiling score of the individual scenario
+        profiling_grading_dir = glob.glob(os.path.join(bag_path, '*/*/*/*grading.json'))
+        logging.info(f"Score file storage path: {profiling_grading_dir}")
+        with open(profiling_grading_dir[0], 'r') as grading_json:
+            grading = json.load(grading_json)
+        #TODO(Yu): implement weighting under the AutoTuner instead of directly reading
+        profiling_score = grading['weighted_score']
+        logging.info(f"Profiling score for individual scenario: "
+                     f"score={profiling_score[0]}, sample={profiling_score[1]}")
+        return (key, profiling_score)
 
     def calculate_weighted_score(self, config_2_score):
         # TODO: implement me
