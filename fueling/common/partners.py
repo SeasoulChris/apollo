dc9d51a68c8d7ddbde9354c0621eda94933291b0
Diego Hu <kuanghu@aliyun.com>
2020-02-13 05:42:00 +0800

    Register new partners in fueling

--- fueling/common/partners.py
+++ fueling/common/partners.py
@@ -16,4 +16,10 @@ partners = {
                          bos_bucket='apollo-platform', bos_region='bj'),
     'udelv2019': Partner(name='Udelv', email='xiaoxiangquan@baidu.com'),
     'coolhigh': Partner(name='Coolhigh', email='mhchenm@coolhigh.com.cn'),
+    'd-kit-htrob': Partner(name='HTROB', email='hance_htrob@163.com',
+                      bos_bucket='ht-rob', bos_region='bj'),
+    'd-kit-hytz': Partner(name='HYTZ', email='zhangxingtao2@jd.com',
+                      bos_bucket='jdd-robot', bos_region='bj'),
+    'd-kit-jzy': Partner(name='JZY', email='1714348719@qq.com',
+                      bos_bucket='jzy-apollo', bos_region='su'),
 }
