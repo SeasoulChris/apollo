49c1fad2e140478a22a654e293e6879e045b7dd3
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2020-02-13 14:47:40 -0800

    Apps: Retire unused web-portal and google_image_downloader.

--- apps/lambda/README.md
+++
@@ -1,5 +0,0 @@
-# Lambda Web Apps
-
-Standalone web apps which run in cloud app engines like
-[Baidu BAE](https://cloud.baidu.com/product/bae.html) or
-[AWS Lambda](https://aws.amazon.com/lambda).
--- apps/lambda/web-portal/deploy/package-code.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$( dirname "${BASH_SOURCE[0]}" )/.."
-
-TIME=$(date +%Y%m%d-%H%M)
-PACKAGE="deploy/web-portal_${TIME}.zip"
-
-zip -r "${PACKAGE}" *.py *.sh templates/ ssl_keys/
-echo "Packaged ${PACKAGE}"
