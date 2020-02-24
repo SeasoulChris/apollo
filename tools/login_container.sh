7ed0c923e26de82d3fd6ddfe43b97a1cb433f655
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2020-02-24 15:26:40 -0800

    Tools: Mount apollo-map if it exists.

--- tools/login_container.sh
+++ tools/login_container.sh
@@ -20,7 +20,11 @@ docker ps -a --format "{{.Names}}" | grep ${CONTAINER} > /dev/null
 if [ $? -eq 0 ]; then
   echo "Found existing container. If you need a fresh one, run 'docker rm -f ${CONTAINER}' first."
 else
+  required_volumes="-v $(pwd):/fuel"
+
   APOLLO_ROOT="$(cd ../apollo-bazel2.x; pwd)"
+  required_volumes="-v ${APOLLO_ROOT}:/apollo ${required_volumes}"
+
   USER_ID=$(id -u)
   GRP=$(id -g -n)
   GRP_ID=$(id -g)
@@ -28,14 +32,21 @@ else
   CACHE_DIR="/home/.bazel_cache"
   sudo mkdir -p ${CACHE_DIR}
   sudo chown ${GRP}:${USER} ${CACHE_DIR}
+  required_volumes="-v ${CACHE_DIR}:${CACHE_DIR} ${required_volumes}"
+
+  optional_volumes=""
+  APOLLO_MAP="../baidu/adu-lab/apollo-map"
+  if [ -d "${APOLLO_MAP}" ]; then
+    APOLLO_MAP="$(cd ${APOLLO_MAP}; pwd)"
+    optional_volumes="-v ${APOLLO_MAP}:/apollo/modules/map/data ${optional_volumes}"
+  fi
 
   ${DOCKER_RUN} -it -d --privileged \
       --net host \
       --name ${CONTAINER} \
       --hostname ${CONTAINER} --add-host ${CONTAINER}:127.0.0.1 \
-      -v "$(pwd):/fuel" \
-      -v "${APOLLO_ROOT}:/apollo" \
-      -v "${CACHE_DIR}:${CACHE_DIR}" \
+      ${required_volumes} \
+      ${optional_volumes} \
       -w /fuel \
       -e DOCKER_USER=$USER -e DOCKER_USER_ID=$USER_ID \
       -e DOCKER_GRP=$GRP -e DOCKER_GRP_ID=$GRP_ID \
