44d79531e5d5c92dee74f5bac9aa038beb2b6328
kechxu <kechxu89@gmail.com>
2020-02-24 14:56:58 -0800

    kinglong: convertion tool works on single machine

--- fueling/common/record/kinglong/cybertron/python/convert.py
+++ fueling/common/record/kinglong/cybertron/python/convert.py
@@ -1,95 +1,122 @@
 import bag
 
+from cyber.proto import record_pb2
+from cyber_py3 import record
+
 import fueling.common.record.kinglong.proto.modules.localization_pose_pb2 as localization_pose_pb2
 import fueling.common.record.kinglong.proto.modules.perception_obstacle_pb2 as perception_obstacle_pb2
 
 import modules.common.proto.geometry_pb2 as apollo_geometry_pb2
-import modules.perception.proto.perception_obstacle_pb2 as apollo_perception_obstacle_pb2
 import modules.localization.proto.pose_pb2 as apollo_pose_pb2
+import modules.localization.proto.localization_pb2 as apollo_localization_pb2
+import modules.perception.proto.perception_obstacle_pb2 as apollo_perception_obstacle_pb2
 
-def transfer_localization_pose(loc_pose):
-    apollo_pose = apollo_pose_pb2.Pose()
-    apollo_pose.position.x = loc_pose.position.x
-    apollo_pose.position.y = loc_pose.position.y
-    apollo_pose.position.z = loc_pose.position.z
+SOURCE_KINGLONG_RECORD = "/fuel/kl.record"
+DESTINATION_APOLLO_RECORD = "/fuel/apollo.record"
 
-    apollo_pose.orientation.qw = loc_pose.orientation.qw
-    apollo_pose.orientation.qx = loc_pose.orientation.qx
-    apollo_pose.orientation.qy = loc_pose.orientation.qy
-    apollo_pose.orientation.qz = loc_pose.orientation.qz
+def transfer_localization_estimate(loc_pose):
+    apollo_loc = apollo_localization_pb2.LocalizationEstimate()
 
-    apollo_pose.linear_velocity.x = loc_pose.linear_velocity.x
-    apollo_pose.linear_velocity.y = loc_pose.linear_velocity.y
-    apollo_pose.linear_velocity.z = loc_pose.linear_velocity.z
+    apollo_loc.pose.position.x = loc_pose.position.x
+    apollo_loc.pose.position.y = loc_pose.position.y
+    apollo_loc.pose.position.z = loc_pose.position.z
+
+    apollo_loc.pose.orientation.qw = loc_pose.orientation.qw
+    apollo_loc.pose.orientation.qx = loc_pose.orientation.qx
+    apollo_loc.pose.orientation.qy = loc_pose.orientation.qy
+    apollo_loc.pose.orientation.qz = loc_pose.orientation.qz
+
+    apollo_loc.pose.linear_velocity.x = loc_pose.linear_velocity.x
+    apollo_loc.pose.linear_velocity.y = loc_pose.linear_velocity.y
+    apollo_loc.pose.linear_velocity.z = loc_pose.linear_velocity.z
+
+    return apollo_loc
 
 
-def transfer_perception_obstacle(obstacle):
-    apollo_obstacle = apollo_perception_obstacle_pb2.PerceptionObstacle()
-    apollo_obstacle.id = obstacle.id
-    apollo_obstacle.position.x = obstacle.position.x
-    apollo_obstacle.position.y = obstacle.position.y
-    apollo_obstacle.position.z = obstacle.position.z
+def transfer_perception_obstacles(obstacles):
+    apollo_perception_obstacles = apollo_perception_obstacle_pb2.PerceptionObstacles()
+    for obstacle in obstacles:
+        apollo_obstacle = apollo_perception_obstacle_pb2.PerceptionObstacle()
+        apollo_obstacle.id = obstacle.id
+        apollo_obstacle.position.x = obstacle.position.x
+        apollo_obstacle.position.y = obstacle.position.y
+        apollo_obstacle.position.z = obstacle.position.z
 
-    apollo_obstacle.theta = obstacle.theta
+        apollo_obstacle.theta = obstacle.theta
 
-    apollo_obstacle.velocity.x = obstacle.velocity.x
-    apollo_obstacle.velocity.y = obstacle.velocity.y
-    apollo_obstacle.velocity.z = obstacle.velocity.z
+        apollo_obstacle.velocity.x = obstacle.velocity.x
+        apollo_obstacle.velocity.y = obstacle.velocity.y
+        apollo_obstacle.velocity.z = obstacle.velocity.z
 
-    apollo_obstacle.length = obstacle.length
-    apollo_obstacle.width = obstacle.width
-    apollo_obstacle.height = obstacle.height
+        apollo_obstacle.length = obstacle.length
+        apollo_obstacle.width = obstacle.width
+        apollo_obstacle.height = obstacle.height
 
-    for point in obstacle.polygon_point:
-        apollo_point = apollo_geometry_pb2.Point3D()
-        apollo_point.x = point.x
-        apollo_point.y = point.y
-        apollo_point.z = point.z
-        apollo_obstacle.polygon_point.add().CopyFrom(apollo_point)
+        for point in obstacle.polygon_point:
+            apollo_point = apollo_geometry_pb2.Point3D()
+            apollo_point.x = point.x
+            apollo_point.y = point.y
+            apollo_point.z = point.z
+            apollo_obstacle.polygon_point.add().CopyFrom(apollo_point)
 
-    if obstacle.type == perception_obstacle_pb2.PerceptionObstacle.UNKNOWN:
-        apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.UNKNOWN
-    elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.UNKNOWN_MOVABLE:
-        apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.UNKNOWN_MOVABLE
-    elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.UNKNOWN_UNMOVABLE:
-        apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.UNKNOWN_UNMOVABLE
-    elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.PEDESTRIAN:
-        apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.PEDESTRIAN
-    elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.BICYCLE:
-        apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.BICYCLE
-    elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.VEHICLE:
-        apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.VEHICLE
+        if obstacle.type == perception_obstacle_pb2.PerceptionObstacle.UNKNOWN:
+            apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.UNKNOWN
+        elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.UNKNOWN_MOVABLE:
+            apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.UNKNOWN_MOVABLE
+        elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.UNKNOWN_UNMOVABLE:
+            apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.UNKNOWN_UNMOVABLE
+        elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.PEDESTRIAN:
+            apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.PEDESTRIAN
+        elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.BICYCLE:
+            apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.BICYCLE
+        elif obstacle.type == perception_obstacle_pb2.PerceptionObstacle.VEHICLE:
+            apollo_obstacle.type = apollo_perception_obstacle_pb2.PerceptionObstacle.VEHICLE
 
-    return apollo_obstacle
+        apollo_perception_obstacles.perception_obstacle.append(apollo_obstacle)
+
+    return apollo_perception_obstacles
 
 
 if __name__ == "__main__":
 
-    f = bag.Bag("/fuel/kl.record", False, False)
+    freader = bag.Bag(SOURCE_KINGLONG_RECORD, False, False)
     topics = ['/perception/obstacles', \
               '/perception/traffic_lights', \
               '/localization/100hz/localization_pose', \
               '/pnc/planning']
 
-    print("file name: %s" %(f.get_file_name()))
-    print("total count: %d" %(f.get_message_count()))
+    print("file name: %s" %(freader.get_file_name()))
+    print("total count: %d" %(freader.get_message_count()))
 
-    print("message start time: %ld" %(f.get_start_time()))
-    print("message end time: %ld" %(f.get_end_time()))
+    print("message start time: %ld" %(freader.get_start_time()))
+    print("message end time: %ld" %(freader.get_end_time()))
 
-    for topic, msg, msgtype, timestamp in f.read_messages(topics):
+    fwriter = record.RecordWriter(0, 0)
+    fwriter.open(DESTINATION_APOLLO_RECORD)
+
+    apollo_pose_topic = "/apollo/localization/pose"
+    apollo_percption_obstacle_topic = "/apollo/perception/obstacles"
+
+    fwriter.write_channel(apollo_pose_topic,
+                          "apollo.localization.LocalizationEstimate",
+                          "some descriptions")
+    fwriter.write_channel(apollo_percption_obstacle_topic,
+                          "apollo.perception.PerceptionObstacles",
+                          "some descriptions")
+
+    for topic, msg, msgtype, timestamp in freader.read_messages(topics):
         print(topic)
-        # print(topic, msgtype)
         if topic == "/localization/100hz/localization_pose":
             loc_pose = localization_pose_pb2.Pose()
-            loc_pose.ParseFromString(msg)
-            apollo_pose = transfer_localization_pose(loc_pose)
+            loc_pose.ParseFromString(msg.encode('utf-8', 'surrogateescape'))
+            apollo_loc = transfer_localization_estimate(loc_pose)
+            fwriter.write_message(apollo_pose_topic, apollo_loc.SerializeToString(), timestamp)
         elif topic == "/perception/obstacles":
             perception_obstacles = perception_obstacle_pb2.PerceptionObstacles()
-            perception_obstacles.ParseFromString(msg)
-            for obstacle in perception_obstacles.perception_obstacle:
-                apollo_obstacle = transfer_perception_obstacle(obstacle)
-                # print(apollo_obstacle)
+            perception_obstacles.ParseFromString(msg.encode('utf-8', 'surrogateescape'))
+            apollo_obstacles = transfer_perception_obstacles(perception_obstacles.perception_obstacle)
+            fwriter.write_message(apollo_percption_obstacle_topic,
+                                  apollo_obstacles.SerializeToString(), timestamp)
 
-    f.close()
-
+    freader.close()
+    fwriter.close()
