2bb3a2d03494bd253ca8b90cf132ff03ccb2a12d
jmtao <taojm99@gmail.com>
2020-02-27 16:35:19 -0800

    planning: update dump_learning_data script

--- fueling/planning/datasets/dump_learning_data.py
+++ fueling/planning/datasets/dump_learning_data.py
@@ -7,19 +7,19 @@ import fueling.common.file_utils as file_utils
 import fueling.common.logging as logging
 import fueling.common.record_utils as record_utils
 
-class DumpFeatureProto(BasePipeline):
+class DumpLearningData(BasePipeline):
     """Records to feature proto pipeline."""
 
     def __init__(self):
         self.src_prefixs = [
-            'modules/planning/cleaned_data/',
+            'modules/planning/cleaned_data/ver_20200219_213417',
         ]
         self.dest_prefix = 'modules/planning/learning_data'
 
     def run_test(self):
         """Run test."""
         self.src_prefixs = [
-            '/apollo/data/cleaned_data/ver_20200219_213417',
+            '/apollo/data/cleaned_data/ver_20200219_213417/task_1',
         ]
         self.dest_prefix = '/apollo/data/learning_data'
 
@@ -43,16 +43,23 @@ class DumpFeatureProto(BasePipeline):
     def process_record(self, src_record_fn):
         """ Process Records """
         src_record_fn_elements = src_record_fn.split("/")
-        fn = src_record_fn_elements[-1]
-        if (not fn.startswith("ver_")):
-            fn = "ver_" + datetime.date.today().strftime("%Y%m%d_%H%M%S") + "/"
+        timestamp = src_record_fn_elements[-2]
+        dest_record_dir_elements = []
+        if (not timestamp.startswith("ver_")):
+            # casual format folder layout
+            timestamp = "ver_" + datetime.date.today().strftime("%Y%m%d_%H%M%S") + "/"
+            dest_record_dir_elements = self.dest_prefix
+            dest_record_dir_elements.append(timestamp)
+        else:
+            # pipeline format folder layout
+            for dir_name in src_record_fn_elements:
+                if dir_name == "cleaned_data":
+                    dest_record_dir_elements.append("learning_data");
+                else:
+                    dest_record_dir_elements.append(dir_name);
+        dest_record_dir = "/".join(dest_record_dir_elements)
 
-        dest_record_fn_elements = src_record_fn_elements[:-5]
-        dest_record_fn_elements.append(self.dest_prefix)
-        dest_record_fn_elements.append(fn)
-        dest_record_fn = "/".join(dest_record_fn_elements)
-
-        file_utils.makedirs(os.path.dirname(dest_record_fn))
+        file_utils.makedirs(os.path.dirname(dest_record_dir))
 
         """Call planning C+pro+ code."""
         map_name = "sunnyvale"
@@ -60,15 +67,15 @@ class DumpFeatureProto(BasePipeline):
             'cd /apollo && sudo bash '
             'modules/tools/planning/data_pipelines/scripts/'
             'records_to_data_for_learning.sh '
-            '"{}" "{}" "{}"'.format(src_record_fn, dest_record_fn, map_name))
+            '"{}" "{}" "{}"'.format(src_record_fn, dest_record_dir, map_name))
         if os.system(command) == 0:
             logging.info('Successfully processed {} to {}'.format(src_record_fn,
-                                                                  dest_record_fn))
+                                                                  dest_record_dir))
             return 1
         else:
             logging.error('Failed to process {} to {}'.format(src_record_fn,
-                                                              dest_record_fn))
+                                                              dest_record_dir))
         return 0
 
 if __name__ == '__main__':
-    DumpFeatureProto().main()
+    DumpLearningData().main()
