2f4ff011f0881904fb95c7815270e9b19093367b
sjiang2018 <shujiang2016@gmail.com>
2020-03-04 17:29:53 -0800

    learning_algorithm/planning: implemented label generator.

--- learning_algorithms/planning/data_preprocessing/label_generation/label_generator.py
+++ learning_algorithms/planning/data_preprocessing/label_generation/label_generator.py
@@ -1,82 +1,114 @@
-# offline_features_pb2
+#!/usr/bin/env python
+
+
+import numpy as np
+
+from modules.planning.proto import learning_data_pb2
+import fueling.common.proto_utils as proto_utils
 
 
 class LabelGenerator(object):
     def __init__(self):
         self.filepath = None
         '''
-            feature_dict contains the organized Feature in the following way:
-                obstacle_ID --> [Feature1, Feature2, Feature3, ...] (sequentially sorted)
-            '''
-        self.feature_dict = dict()
-
-        def LoadFeaturePBAndSaveLabelFiles(self, input_filepath):
-            '''
-            This function will be used to replace all the functionalities in
-            generate_cruise_label.py
-            '''
-            self.filepath = input_filepath
-            offline_features = offline_features_pb2.Features()
-            offline_features = proto_utils.get_pb_from_bin_file(self.filepath, offline_features)
-            feature_sequences = offline_features.feature
-            self.OrganizeFeatures(feature_sequences)
-            del feature_sequences  # Try to free up some memory
-            self.ObserveAllFeatureSequences()
-
+        observation_dict contains the important observations of the subsequent
+        Features for each obstacle at every timestamp:
+            obstacle_ID@timestamp --> dictionary of observations
+        where dictionary of observations contains:
+            'adu_traj': the trajectory points (x, y, vel_heading) up to
+                        max_observation_time this trajectory point must
+                        be consecutive (0.1sec sampling period)
+            'adu_traj_len': length of trajectory points
+            'is_jittering':
+            'total_observed_time_span':
+        This observation_dict, once constructed, can be reused by various labeling
+        functions.
         '''
-        @brief: organize the features by obstacle IDs first, then sort each
-                obstacle's feature according to time sequence.
-        @input features: the unorganized features
-        @output: organized (meaning: grouped by obstacle ID and sorted by time)
-                features.
-        '''
+        self.observation_dict = dict()
 
-        def OrganizeFeatures(self, features):
-            # Organize Feature by obstacle_ID (put those belonging to the same obstacle together)
-            for feature in features:
-                if feature.id in self.feature_dict.keys():
-                    self.feature_dict[feature.id].append(feature)
-                else:
-                    self.feature_dict[feature.id] = [feature]
+    def LoadFeaturePBAndSaveLabelFiles(self, input_filepath):
+        self.filepath = input_filepath
+        offline_features = learning_data_pb2.LearningData()
+        offline_features = proto_utils.get_pb_from_bin_file(self.filepath, offline_features)
+        feature_sequence = offline_features.learning_data
+        # [Feature1, Feature2, Feature3, ...] (sequentially sorted)
+        feature_sequence.sort(key=lambda x: x.timestamp_sec)
+        # return feature_sequences[-1].timestamp_sec
+        return self.ObserveFeatureSequence(feature_sequence, 0)
 
-            # For the same obstacle, sort the Feature sequentially.
-            for obs_id in self.feature_dict.keys():
-                if len(self.feature_dict[obs_id]) < 2:
-                    del self.feature_dict[obs_id]
-                    continue
-                self.feature_dict[obs_id].sort(key=lambda x: x.timestamp)
+    '''
+    @brief: observe all feature sequences and build observation_dict.
+    @output: the complete observation_dict.
+    '''
 
-        '''
-        @brief: observe all feature sequences and build observation_dict.
-        @output: the complete observation_dict.
-        '''
+    def ObserveAllFeatureSequences(self, feature_sequence):
+        for idx, feature in enumerate(feature_sequence):
+            self.ObserveFeatureSequence(feature_sequence)
+        np.save(self.filepath + '.npy', self.observation_dict)
 
-        def ObserveAllFeatureSequences(self):
-            for obs_id, feature_sequence in self.feature_dict.items():
-                for idx, feature in enumerate(feature_sequence):
-                    if not feature.HasField('lane') or \
-                            not feature.lane.HasField('lane_feature'):
-                        # print('No lane feature, cancel labeling')
-                        continue
-                    self.ObserveFeatureSequence(feature_sequence, idx)
-            np.save(self.filepath + '.npy', self.observation_dict)
+    def ObserveFeatureSequence(self, feature_sequence, idx_curr):
+        # Initialization.
+        feature_curr = feature_sequence[idx_curr]
+        dict_key = "ADU@{:.3f}".format(feature_curr.timestamp_sec)
+        if dict_key in self.observation_dict.keys():
+            return
+        # Declare needed varables.
+        is_jittering = False
+        feature_seq_len = len(feature_sequence)
+        prev_timestamp = -1.0
+        adu_traj = []
+        total_observed_time_span = 0.0
+        maximum_observation_time = 8.0
 
-        def ObserveFeatureSequence(self, feature_sequence, idx_curr):
-            # Initialization.
-            feature_curr = feature_sequence[idx_curr]
-            dict_key = "{}@{:.3f}".format(feature_curr.id, feature_curr.timestamp)
-            if dict_key in self.observation_dict.keys():
-                return
+        # This goes through all the subsequent features in this sequence
+        # of features up to the maximum_observation_time.
+        for j in range(idx_curr, feature_seq_len):
+            # If timespan exceeds max. observation time, then end observing.
+            time_span = feature_sequence[j].timestamp_sec - feature_curr.timestamp_sec
+            if time_span > maximum_observation_time:
+                break
+            total_observed_time_span = time_span
 
-        def LabelTrajectory(self, period_of_interest=3.0):
-            output_features = offline_features_pb2.Features()
-            for obs_id, feature_sequence in self.feature_dict.items():
-                for idx, feature in enumerate(feature_sequence):
-                    # Observe the subsequent Features
-                    if "{}@{:.3f}".format(feature.id, feature.timestamp) not in self.observation_dict:
-                        continue
-                    observed_val = self.observation_dict["{}@{:.3f}".format(
-                        feature.id, feature.timestamp)]
-                    key = "{}@{:.3f}".format(feature.id, feature.timestamp)
-                    self.future_status_dict[key] = observed_val['obs_traj']
-            np.save(self.filepath + '.future_status.npy', self.future_status_dict)
+            #####################################################################
+            # Update the ADU trajectory:
+            # Only update for consecutive (sampling rate = 0.1sec) points.
+            # IMPORTANT NOTE: APPEND ONLY to add new items
+            adu_traj.append((feature_sequence[j].localization.position.x,
+                             feature_sequence[j].localization.position.y,
+                             feature_sequence[j].localization.heading,
+                             feature_sequence[j].localization.linear_velocity.x,
+                             feature_sequence[j].localization.linear_velocity.y,
+                             feature_sequence[j].localization.linear_acceleration.x,
+                             feature_sequence[j].localization.linear_acceleration.y,
+                             feature_sequence[j].localization.angular_velocity.z,
+                             feature_sequence[j].timestamp_sec))
+        # Update the observation_dict:
+        dict_val = dict()
+        dict_val['adu_traj'] = adu_traj
+        dict_val['adu_traj_len'] = len(adu_traj)
+        dict_val['is_jittering'] = is_jittering
+        dict_val['total_observed_time_span'] = total_observed_time_span
+        self.observation_dict["@{:.3f}".format(feature_curr.timestamp_sec)] = dict_val
+        np.save(self.filepath + '.npy', self.observation_dict)
+        return dict_val
+
+    # def LabelTrajectory(self, period_of_interest=3.0):
+    #     output_features = learning_data_pb2.Features()
+    #     for feature_sequence in self.feature_dict.items():
+    #         for idx, feature in enumerate(feature_sequence):
+    #             # Observe the subsequent Features
+    #             if "{}@{:.3f}".format(feature.id, feature.timestamp) not in self.observation_dict:
+    #                 continue
+    #             observed_val = self.observation_dict["{}@{:.3f}".format(
+    #                 feature.id, feature.timestamp)]
+    #             key = "{}@{:.3f}".format(feature.id, feature.timestamp)
+    #             self.future_status_dict[key] = observed_val['obs_traj']
+    #     np.save(self.filepath + '.future_status.npy', self.future_status_dict)
+
+
+# demo
+if __name__ == '__main__':
+    FILE = '/apollo/data/learning_based_planning/bin_result/learning_data.2.bin'
+    label_gen = LabelGenerator()
+    result = label_gen.LoadFeaturePBAndSaveLabelFiles(FILE)
+    print(result)
