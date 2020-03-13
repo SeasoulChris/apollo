bf80c4e9632eb11759f650bdb63a85cac5688b94
Jiaming Tao <taojm99@gmail.com>
2020-03-13 14:03:25 -0700

    planning: some code refactory in generate_training_data.py (#1532)
    * planning: some code refactory in generate_training_data.py
    
    * planning: add chassis and localization feature
    
    * planning: add obstacle trajectory feature
    
    * planning: add obstacle feature
    
    * planning: add routing_response and traffic_light features
    
    * planning: updated and completed label data

--- fueling/planning/datasets/generate_training_data.py
+++ fueling/planning/datasets/generate_training_data.py
@@ -14,11 +14,11 @@ LABEL_TRAJECTORY_POINT_NUM = 20
 
 
 def LoadInstances(filepath):
-    instances = LearningData()
+    learning_data = LearningData()
     try:
         with open(filepath, 'rb') as file_in:
-            instances.ParseFromString(file_in.read())
-        return instances
+            learning_data.ParseFromString(file_in.read())
+        return learning_data
     except BaseException:
         return None
 
@@ -67,11 +67,166 @@ class GenerateTrainingData(BasePipeline):
 
         logging.info('Processed {} records'.format(processed_records.count()))
 
+    def add_chassis_feature(self, learning_data_frame, current_features):
+        chassis_feature = learning_data_frame.chassis
+
+        current_features.append(chassis_feature.speed_mps)
+        current_features.append(chassis_feature.throttle_percentage)
+        current_features.append(chassis_feature.brake_percentage)
+        current_features.append(chassis_feature.steering_percentage)
+        current_features.append(chassis_feature.gear_location)
+
+        return current_features
+
+    def add_localization_feature(self, learning_data_frame, current_features):
+        localization_feature = learning_data_frame.localization
+
+        current_features.append(localization_feature.position.x)
+        current_features.append(localization_feature.position.y)
+        current_features.append(localization_feature.position.z)
+
+        current_features.append(localization_feature.heading)
+
+        current_features.append(localization_feature.linear_velocity.x)
+        current_features.append(localization_feature.linear_velocity.y)
+        current_features.append(localization_feature.linear_velocity.z)
+
+        current_features.append(localization_feature.linear_acceleration.x)
+        current_features.append(localization_feature.linear_acceleration.y)
+        current_features.append(localization_feature.linear_acceleration.z)
+
+        current_features.append(localization_feature.angular_velocity.x)
+        current_features.append(localization_feature.angular_velocity.y)
+        current_features.append(localization_feature.angular_velocity.z)
+
+        return current_features
+
+    def add_obstacle_feature(self, learning_data_frame, current_features):
+        obstacle_feature = learning_data_frame.obstacle
+
+        # obstacle
+        for obstacle in obstacle_feature:
+            current_features.append(obstacle.id)
+            current_features.append(obstacle.length)
+            current_features.append(obstacle.width)
+            current_features.append(obstacle.height)
+            current_features.append(obstacle.type)
+
+            # obstacle_trajectory_point
+            for trajectory_point in obstacle.obstacle_trajectory_point:
+                current_features.append(trajectory_point.position.x)
+                current_features.append(trajectory_point.position.y)
+                current_features.append(trajectory_point.position.z)
+                current_features.append(trajectory_point.theta)
+                current_features.append(trajectory_point.velocity.x)
+                current_features.append(trajectory_point.velocity.y)
+                current_features.append(trajectory_point.velocity.z)
+                current_features.append(trajectory_point.acceleration.x)
+                current_features.append(trajectory_point.acceleration.y)
+                current_features.append(trajectory_point.acceleration.z)
+
+                for point in trajectory_point.polygon_point:
+                    current_features.append(point.x)
+                    current_features.append(point.y)
+                    current_features.append(point.z)
+
+            # obstacle_prediction
+            current_features.append(obstacle.obstacle_prediction.predicted_period)
+            current_features.append(obstacle.obstacle_prediction.intent.type)
+            current_features.append(obstacle.obstacle_prediction.priority.priority)
+            current_features.append(obstacle.obstacle_prediction.is_static)
+
+            for tj in obstacle.obstacle_prediction.trajectory:
+                current_features.append(tj.probability)
+                for point in tj.trajectory_point:
+                    current_features.append(point.path_point.x)
+                    current_features.append(point.path_point.y)
+                    current_features.append(point.path_point.z)
+                    current_features.append(point.path_point.theta)
+                    current_features.append(point.path_point.s)
+                    current_features.append(point.path_point.lane_id)
+
+                    current_features.append(point.v)
+                    current_features.append(point.a)
+                    current_features.append(point.relative_time)
+                    current_features.append(point.gaussian_info.sigma_x)
+                    current_features.append(point.gaussian_info.sigma_y)
+                    current_features.append(point.gaussian_info.correlation)
+                    current_features.append(point.gaussian_info.area_probability)
+                    current_features.append(point.gaussian_info.ellipse_a)
+                    current_features.append(point.gaussian_info.ellipse_b)
+                    current_features.append(point.gaussian_info.theta_a)
+        return current_features
+
+    def add_routing_response_feature(self, learning_data_frame, current_features):
+        routing_response_feature = learning_data_frame.routing_response
+        for lane_id in routing_response_feature.lane_id:
+            current_features.append(lane_id)
+        return current_features
+
+    def add_traffic_light_feature(self, learning_data_frame, current_features):
+        traffic_light_feature = learning_data_frame.traffic_light
+        for traffic_light in traffic_light_feature:
+            current_features.append(traffic_light.id)
+            current_features.append(traffic_light.color)
+        return current_features
+
+    def generate_label(self, learning_data_frame, current_label):
+        adc_trajectory_point = learning_data_frame.adc_trajectory_point
+        # TODO(all): how to make sure all label points have the same number
+        for adc_trajectory_point in adc_trajectory_point:
+            current_label.append(adc_trajectory_point.timestamp_sec)
+            current_label.append(adc_trajectory_point.trajectory_point.path_point.x)
+            current_label.append(adc_trajectory_point.trajectory_point.path_point.y)
+            current_label.append(adc_trajectory_point.trajectory_point.path_point.z)
+            current_label.append(adc_trajectory_point.trajectory_point.v)
+            current_label.append(adc_trajectory_point.trajectory_point.a)
+            current_label.append(adc_trajectory_point.trajectory_point.relative_time)
+            current_label.append(adc_trajectory_point.trajectory_point.da)
+            current_label.append(adc_trajectory_point.trajectory_point.steer)
+            current_label.append(adc_trajectory_point.trajectory_point.gaussian_info.sigma_x)
+            current_label.append(adc_trajectory_point.trajectory_point.gaussian_info.sigma_y)
+            current_label.append(adc_trajectory_point.trajectory_point.gaussian_info.correlation)
+            current_label.append(adc_trajectory_point.trajectory_point.gaussian_info.area_probability)
+            current_label.append(adc_trajectory_point.trajectory_point.gaussian_info.ellipse_a)
+            current_label.append(adc_trajectory_point.trajectory_point.gaussian_info.ellipse_b)
+            current_label.append(adc_trajectory_point.trajectory_point.gaussian_info.theta_a)
+        return current_label
+
+    def process_learning_data_frame(self, learning_data_frame):
+        current_features = []
+        current_label = []
+
+        # chassis
+        self.add_chassis_feature(learning_data_frame, current_features)
+
+        # localization
+        self.add_localization_feature(learning_data_frame, current_features)
+
+        # obstacle
+        self.add_obstacle_feature(learning_data_frame, current_features)
+
+        # routing_response
+        self.add_routing_response_feature(learning_data_frame, current_features)
+
+        # traffic_light
+        self.add_traffic_light_feature(learning_data_frame, current_features)
+
+        # label
+        self.generate_label(learning_data_frame, current_label)
+
+        # if (LABEL_TRAJECTORY_POINT_NUM != len(current_label)):
+        #     logging.warn("label point number:{}".format(len(current_label)))
+        #     continue
+        current_data_point = [current_features, current_label]
+        return current_data_point
+
     def process_learning_data(self, src_dir):
         """ Process learning data: learning datas -> training data """
         # Go through all the learning_data file, for each data-point, find
         # the corresponding label file, merge them.
         all_file_paths = file_utils.list_files(src_dir)
+
         # sort by filenames numerically: learning_data.<int>.bin.training_data.npy
         all_file_paths.sort(
             key=lambda var: [int(x) if x.isdigit() else x for x in re.findall(r'[^0-9]|[0-9]+', var)])
@@ -94,7 +249,7 @@ class GenerateTrainingData(BasePipeline):
             else:
                 dest_dir = "/".join(src_dir_elements)
 
-            file_utils.makedirs(os.path.dirname(dest_dir))
+            file_utils.makedirs(dest_dir)
 
             delete_filelist = glob.glob(dest_dir + '/' + file_name + '.training_data.npy')
             for delete_file in delete_filelist:
@@ -106,49 +261,16 @@ class GenerateTrainingData(BasePipeline):
                                                             file_count, len(all_file_paths)))
 
             # Load the feature for learning file.
-            instances = LoadInstances(file_path)
-            if instances is None:
-                print('Failed to read instances file: {}.'.format(file_path))
+            learning_data = LoadInstances(file_path)
+            if learning_data is None:
+                logging.info('Failed to read learning_data file: {}.'.format(file_path))
                 continue
 
             # Go through the entries in this feature file.
-            total_num_data_points += len(instances.learning_data)
+            total_num_data_points += len(learning_data.learning_data)
             output_np_array = []
-            for instance in instances.learning_data:
-                current_features = []
-                current_label = []
-
-                localization_feature = instance.localization
-                current_features += [localization_feature.position.x]
-                current_features += [localization_feature.position.y]
-                current_features += [localization_feature.position.z]
-
-                current_features += [localization_feature.heading]
-                current_features += [localization_feature.linear_velocity.x]
-                current_features += [localization_feature.linear_velocity.y]
-                current_features += [localization_feature.linear_velocity.z]
-
-                current_features += [localization_feature.linear_acceleration.x]
-                current_features += [localization_feature.linear_acceleration.y]
-                current_features += [localization_feature.linear_acceleration.z]
-
-                chassis_feature = instance.chassis
-                current_features += [chassis_feature.speed_mps]
-                current_features += [chassis_feature.throttle_percentage]
-                current_features += [chassis_feature.brake_percentage]
-                current_features += [chassis_feature.steering_percentage]
-
-                adc_trajectory_point = instance.adc_trajectory_point
-                # TODO(all): how to make sure all label points have the same number
-                for adc_trajectory_point in adc_trajectory_point:
-                    current_label += [adc_trajectory_point.timestamp_sec]
-                    current_label += [adc_trajectory_point.trajectory_point.path_point.x]
-                    current_label += [adc_trajectory_point.trajectory_point.path_point.y]
-
-                # if (LABEL_TRAJECTORY_POINT_NUM != len(current_label)):
-                #     logging.warn("label point number:{}".format(len(current_label)))
-                #     continue
-                current_data_point = [current_features, current_label]
+            for learning_data_frame in learning_data.learning_data:
+                current_data_point = self.process_learning_data_frame(learning_data_frame)
                 # Update into the output_np_array.
                 output_np_array.append(current_data_point)
 
@@ -157,7 +279,7 @@ class GenerateTrainingData(BasePipeline):
             # Save into a local file for training.
             try:
                 logging.info('Total usable data points: {} out of {}.'.format(
-                    num_usable_data_points, len(instances.learning_data)))
+                    num_usable_data_points, len(learning_data.learning_data)))
                 output_np_array = np.array(output_np_array)
                 new_file_path = dest_dir + '/' + file_name + '.training_data.npy'
                 np.save(new_file_path, output_np_array)
