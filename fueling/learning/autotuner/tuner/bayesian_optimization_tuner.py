4a905ae49d23275d8b60f2db6562a87628e321e0
kevin-y-wang <yuwang01@baidu.com>
2020-03-23 15:15:07 -0700

    Autotuner: upgrade visual_utils: step-by-step auto-monitoring, auto-saving

--- fueling/learning/autotuner/tuner/bayesian_optimization_tuner.py
+++ fueling/learning/autotuner/tuner/bayesian_optimization_tuner.py
@@ -13,7 +13,7 @@ from modules.control.proto.mrac_conf_pb2 import MracConf
 
 from fueling.learning.autotuner.client.cost_computation_client import CostComputationClient
 from fueling.learning.autotuner.proto.tuner_param_config_pb2 import TunerConfigs
-import fueling.learning.autotuner.tuner.bayesian_optimization_visual_utils as visual_utils
+from fueling.learning.autotuner.tuner.bayesian_optimization_visual_utils import BayesianOptimizationVisual
 import fueling.common.file_utils as file_utils
 import fueling.common.logging as logging
 import fueling.common.proto_utils as proto_utils
@@ -43,7 +43,7 @@ def black_box_function(tuner_param_config_pb, algorithm_conf_pb):
         },
     )
     logging.info(f"Received score for {training_id}")
-    return weighted_score[config_id]
+    return training_id, weighted_score[config_id]
 
 
 class BayesianOptimizationTuner():
@@ -106,6 +106,7 @@ class BayesianOptimizationTuner():
 
     def optimize(self, n_iter=5):
         self.n_iter = n_iter
+        visual = BayesianOptimizationVisual()
         for i in range(n_iter):
             for flag in self.tuner_param_config_pb.tuner_parameters.flag:
                 self.algorithm_conf_pb.lat_controller_conf.MergeFrom(
@@ -119,12 +120,14 @@ class BayesianOptimizationTuner():
             logging.info(
                 f"New MRAC Conf files {self.algorithm_conf_pb.lat_controller_conf.steer_mrac_conf}")
 
-            score = black_box_function(self.tuner_param_config_pb, self.algorithm_conf_pb)
+            training_id, score = black_box_function(self.tuner_param_config_pb, self.algorithm_conf_pb)
             target = score if self.opt_max else -score
             self.optimizer.register(params=next_point, target=target)
-            for param_name in self.pbounds.keys():
-                visual_utils.plot_gp(self.optimizer, param_name, self.pbounds[param_name][0],
-                                     self.pbounds[param_name][1], self.utility)
+
+            if len(self.pbounds.keys()) == 1:
+                param_name = list(self.pbounds.keys())[0]
+                visual.plot_gp(self.optimizer, self.utility, self.pbounds, param_name, training_id)
+
             logging.info(f"optimizer iteration: {i}, target value: {target}, config point: {next_point}")
 
     def get_result(self):
--- fueling/learning/autotuner/tuner/bayesian_optimization_visual_utils.py
+++ fueling/learning/autotuner/tuner/bayesian_optimization_visual_utils.py
@@ -5,64 +5,86 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
-import time
 
 from bayes_opt import BayesianOptimization
 from bayes_opt import UtilityFunction
 import numpy as np
+import os
+import time
 
+TMP_ROOT_DIR = "/mnt/bos/autotuner"
 
-def posterior(optimizer, x_obs, y_obs, grid):
-    """Predict the mean and variance based on the known data"""
-    optimizer._gp.fit(x_obs, y_obs)
+class BayesianOptimizationVisual():
+    """Basic functionality for Bayesian optimization visualization."""
 
-    mu, sigma = optimizer._gp.predict(grid, return_std=True)
-    return mu, sigma
+    def __init__(self):
+        """Initialize the optimization visualization"""
+        self.figure = plt.figure(figsize=(16, 10))
+        plt.show(block=False)
+        self.figure.suptitle(
+            f'Gaussian Process and Utility Function Initialization, Waiting ...',
+        )
+        plt.draw()
+        plt.pause(1)
 
-def plot_gp(optimizer, param_name, param_min, param_max, utility_function):
-    """Plot the single-param Gaussian Process and Utility function"""
-    param_grid = np.linspace(param_min, param_max, 10000).reshape(-1, 1)
+    def posterior(self, optimizer, x_obs, y_obs, grid):
+        """Predict the mean and variance based on the known data"""
+        optimizer._gp.fit(x_obs, y_obs)
 
-    fig = plt.figure(figsize=(16, 10))
-    steps = len(optimizer.space)
-    fig.suptitle(
-        f'Gaussian Process and Utility Function After {steps} Steps',
-        fontdict={'size':30}
-    )
+        mu, sigma = optimizer._gp.predict(grid, return_std=True)
+        return mu, sigma
 
-    gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1])
-    axis = plt.subplot(gs[0])
-    acq = plt.subplot(gs[1])
+    def plot_gp(self, optimizer, utility_function, pbounds, param_name, training_id):
+        """Plot the single-param Gaussian Process and Utility function"""
+        param_min = pbounds[param_name][0]
+        param_max = pbounds[param_name][1]
+        param_grid = np.linspace(param_min, param_max, 10000).reshape(-1, 1)
 
-    x_obs = np.array([[res["params"][param_name]] for res in optimizer.res])
-    y_obs = np.array([res["target"] for res in optimizer.res])
+        steps = len(optimizer.space)
+        self.figure.suptitle(
+            f'Gaussian Process and Utility Function After {steps} Steps',
+            fontdict={'size':30}
+        )
 
-    mu, sigma = posterior(optimizer, x_obs, y_obs, param_grid)
-    axis.plot(x_obs.flatten(), y_obs, 'D', markersize=8, label=u'Observations', color='r')
-    axis.plot(param_grid, mu, '--', color='k', label='Prediction')
+        gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1])
+        axis = plt.subplot(gs[0])
+        acq = plt.subplot(gs[1])
 
-    axis.fill(np.concatenate([param_grid, param_grid[::-1]]),
-              np.concatenate([mu - 1.9600 * sigma, (mu + 1.9600 * sigma)[::-1]]),
-        alpha=.6, fc='c', ec='None', label='95% confidence interval')
+        x_obs = np.array([[res["params"][param_name]] for res in optimizer.res])
+        y_obs = np.array([res["target"] for res in optimizer.res])
 
-    axis.set_xlim((param_min, param_max))
-    axis.set_ylim((None, None))
-    axis.set_ylabel('Target', fontdict={'size':20})
-    axis.set_xlabel(param_name, fontdict={'size':20})
+        mu, sigma = self.posterior(optimizer, x_obs, y_obs, param_grid)
+        axis.plot(x_obs.flatten(), y_obs, 'D', markersize=8, label=u'Observations', color='r')
+        axis.plot(param_grid, mu, '--', color='k', label='Prediction')
 
-    utility = utility_function.utility(param_grid, optimizer._gp, 0)
-    acq.plot(param_grid, utility, label='Utility Function', color='purple')
-    acq.plot(param_grid[np.argmax(utility)], np.max(utility), '*', markersize=15,
-             label=u'Next Best Guess', markerfacecolor='gold', markeredgecolor='k',
-             markeredgewidth=1)
-    acq.set_xlim((param_min, param_max))
-    acq.set_ylim((0, np.max(utility) + 0.5))
-    acq.set_ylabel('Utility', fontdict={'size':20})
-    acq.set_xlabel(param_name, fontdict={'size':20})
+        axis.fill(np.concatenate([param_grid, param_grid[::-1]]),
+                  np.concatenate([mu - 1.9600 * sigma, (mu + 1.9600 * sigma)[::-1]]),
+                  alpha=.6, fc='c', ec='None', label='95% confidence interval')
 
-    axis.legend(loc=2, bbox_to_anchor=(1.01, 1), borderaxespad=0.)
-    acq.legend(loc=2, bbox_to_anchor=(1.01, 1), borderaxespad=0.)
+        axis.set_xlim((param_min, param_max))
+        axis.set_ylim((None, None))
+        axis.set_ylabel('Target', fontdict={'size':20})
+        axis.set_xlabel(param_name, fontdict={'size':20})
 
-    plt.show(block=False)
-    plt.pause(8)
-    plt.close()
+        utility = utility_function.utility(param_grid, optimizer._gp, 0)
+        acq.plot(param_grid, utility, label='Utility Function', color='purple')
+        acq.plot(param_grid[np.argmax(utility)], np.max(utility), '*', markersize=15,
+                 label=u'Next Best Guess', markerfacecolor='gold', markeredgecolor='k',
+                 markeredgewidth=1)
+        acq.set_xlim((param_min, param_max))
+        acq.set_ylim((-np.max(utility) - 0.5, np.max(utility) + 0.5))
+        acq.set_ylabel('Utility', fontdict={'size':20})
+        acq.set_xlabel(param_name, fontdict={'size':20})
+
+        axis.legend(loc=2, bbox_to_anchor=(1.01, 1), borderaxespad=0.)
+        acq.legend(loc=2, bbox_to_anchor=(1.01, 1), borderaxespad=0.)
+
+        plt.draw()
+        plt.pause(1)
+
+        if os.path.isdir(f'{TMP_ROOT_DIR}/{training_id}'):
+            save_path = f'{TMP_ROOT_DIR}/{training_id}'
+        else:
+            save_path = f'testdata/autotuner/{training_id}'
+            os.makedirs(save_path)
+        plt.savefig(f'{save_path}/gaussian_process.png')
