731bec2c180a0dd84e7e42edba1afe3a7be90371
Longtao Lin <michael4338@hotmail.com>
2020-03-27 13:06:33 -0700

    v2 migration: fix a couple of bugs for cloud jobs (#1600)
    * v2 migration: fix a couple of bugs for cloud jobs
    
    * revert local change

--- fueling/perception/sensor_calibration/calibration_multi_sensors.py
+++ fueling/perception/sensor_calibration/calibration_multi_sensors.py
@@ -92,12 +92,10 @@ class SensorCalibrationPipeline(BasePipeline):
             logging.error(e)
 
         # Send result to job owner.
-        # receivers = email_utils.PERCEPTION_TEAM + email_utils.DATA_TEAM + email_utils.D_KIT_TEAM
-        receivers = []
+        receivers = email_utils.PERCEPTION_TEAM + email_utils.DATA_TEAM + email_utils.D_KIT_TEAM
         partner = partners.get(job_owner)
         if partner:
             receivers.append(partner.email)
-        receivers = ['longtaolin@baidu.com']
 
         if result_files:
             title = 'Your sensor calibration job is done!'
