3dd4500611e68de62a8ca073fc91a1815f01d249
Capri2014 <qi.stephen.luo@gmail.com>
2020-04-08 15:17:40 -0700

    Autotuner: add initial working optuna tuner example

--- fueling/learning/autotuner/tuner/optuna_tuner.py
+++ fueling/learning/autotuner/tuner/optuna_tuner.py
@@ -1,36 +1,39 @@
+"""
+Optuna example that optimizes a simple quadratic function.
+In this example, we optimize a simple quadratic function. We also demonstrate how to continue an
+optimization and to use timeouts.
+We have the following two ways to execute this example:
+(1) Execute this code directly.
+    $ python quadratic_simple.py
+(2) Execute through CLI.
+    $ STUDY_NAME=`optuna create-study --storage sqlite:///example.db`
+    $ optuna study optimize quadratic_simple.py objective --n-trials=100 --study $STUDY_NAME \
+      --storage sqlite:///example.db
+"""
+
 import optuna
 
 
+# Define a simple 2-dimensional objective function whose minimum value is -1 when (x, y) = (0, -1).
 def objective(trial):
-    config = trial.suggest_uniform('x', -10, 10)
-
-    tuner = MracAutoTuner()
-    tuner.set_config(list_of_pairs)  # {path, config}
-    tuner.main()
-    # 1. Config generations
-    # 2. Send / Call replay_engine
-    # 3. Gather bags.
-    # 4. Call profiling.
-    # 5. Return weighted scores.
-    list_of_scores = tuner.get_scores()  # {config, scenario -> score}
-    return weighted_score(scores)
-    # return (x - 2) ** 2
+    x = trial.suggest_uniform("x", -100, 100)
+    y = trial.suggest_categorical("y", [-1, 0, 1])
+    return x ** 2 + y
 
 
-def weighted_score(scores):
-    return 1
+if __name__ == "__main__":
+    # Let us minimize the objective function above.
+    print("Running 10 trials...")
+    study = optuna.create_study()
+    study.optimize(objective, n_trials=10)
+    print("Best value: {} (params: {})\n".format(study.best_value, study.best_params))
 
+    # We can continue the optimization as follows.
+    print("Running 20 additional trials...")
+    study.optimize(objective, n_trials=20)
+    print("Best value: {} (params: {})\n".format(study.best_value, study.best_params))
 
-sampler_marc = MRACSampler()
-
-study = optuna.create_study(sampler=sampler_marc)
-
-# Optionally can run in parellel for trainig, using SQLite or MySQL.
-study.optimize(objective, n_trials=100)
-
-
-study.best_params
-
-study.best_value
-
-study.best_trial
+    # We can specify the timeout instead of a number of trials.
+    print("Running additional trials in 2 seconds...")
+    study.optimize(objective, timeout=2.0)
+    print("Best value: {} (params: {})\n".format(study.best_value, study.best_params))
