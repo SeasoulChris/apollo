7a4e41f0e10ba37c3e0d110bef161cccdb4ce8e3
Xiao(Andrew) Wei <andrew.wei.2019@gmail.com>
2020-04-15 09:04:34 +0800

    Data: add afs server/client with adbsdk support (#1698)
    * feat: afs server/client
    
    * format the code according to pep8

--- apps/afs_data_service/server.py
+++ apps/afs_data_service/server.py
@@ -9,8 +9,8 @@ import grpc
 from adbsdk import cmm
 from adbsdk.adb_client import AdbClient
 from adbsdk.dump.record_dump import RecordDump
-import afs_data_service_pb2 as afs_data_service_pb2
-import afs_data_service_pb2_grpc as afs_data_service_pb2_grpc
+import afs_data_service_pb2
+import afs_data_service_pb2_grpc
 
 
 class AfsDataTransfer(afs_data_service_pb2_grpc.AfsDataTransferServicer):
@@ -20,43 +20,74 @@ class AfsDataTransfer(afs_data_service_pb2_grpc.AfsDataTransferServicer):
         """Init"""
         print('Running AfsDataTransfer server.')
         self.adb_client = AdbClient()
-        self.adb_client.set_config('adb.server.bns', 'adb-server-online.IDG.yq')
+        # TODO(weixiao): Replace IP with a list
+        self.adb_client.set_config('adb.server.hosts', '10.197.199.17:8010')
+        self.adb_client.set_config(
+            'adb.export_server.hosts',
+            '10.90.222.37:8000')
         self.adb_client.set_config('adb.user', os.environ.get('ADB_SDK_USER'))
-        self.adb_client.set_config('adb.pass', os.environ.get('ADB_SDK_PASSWD'))
+        self.adb_client.set_config(
+            'adb.pass', os.environ.get('ADB_SDK_PASSWD'))
+
         print('AfsDataTransfer server running with adbsdk client setup.')
 
     def Scan(self, request, context):
         """Scan"""
-        scan = cmm.Scan(table_name='kinglong/auto_car/cyberecord',
-                        where='date = 20200330')
+        scan = cmm.Scan(table_name=request.table_name,
+                        where=request.where,
+                        columns=request.columns if request.columns else '*',
+                        order=request.order,
+                        limit=request.limit)
         scan_result_iterator = self.adb_client.scan(scan)
+        response = afs_data_service_pb2.ScanResponse()
         for scan_result in scan_result_iterator:
-            if scan_result.success is not True:
+            if not scan_result.success:
                 #raise Exception('exception occurred: %s' % (scan_result.errMessage, ))
                 print('exception occurred: {}'.format(scan_result.errMessage))
+                continue
             print('rowKey:{}, task_id: {}, stime: {}, etime: {}'.format(
                   scan_result.rowKey,
-                  scan_result.meta['task_id'],
-                  scan_result.meta['start_time'],
-                  scan_result.meta['end_time']))
-        response = afs_data_service_pb2.ScanResponse()
+                  scan_result.meta['task_id'].str,
+                  scan_result.meta['start_time'].str,
+                  scan_result.meta['end_time'].str))
+            responseItem = afs_data_service_pb2.ScanResponseItem()
+            responseItem.rowKey = scan_result.rowKey
+            responseItem.task_id = scan_result.meta['task_id'].str
+            responseItem.start_time = int(
+                float((scan_result.meta['start_time'].str)))
+            responseItem.end_time = int(
+                float((scan_result.meta['end_time'].str)))
+            response.records.append(responseItem)
         return response
 
     def ReadMessages(self, request, context):
         """Dump and ReadMessages"""
-        for x in range(10):
-            response = afs_data_service_pb2.ReadMessagesResponse(status=x)
+        record_dump = RecordDump(self.adb_client)
+        messages = record_dump.read_messages(
+            request.task_id,
+            request.topics if request.topics else '*',
+            request.start_time_second,
+            request.end_time_second,
+            request.table_name)
+        for topic, message, data_type, timestamp in messages:
+            print('task_id:{}, topic:{}, data_type:{}, timestamp:{}'.format(
+                request.task_id, topic, data_type, timestamp))
+            response = afs_data_service_pb2.ReadMessagesResponse()
+            response.topic = topic
+            response.message = message.decode(
+                'latin1').encode('utf-8', 'surrogateescape')
+            response.data_type = data_type
+            response.timestamp = timestamp
             yield response
 
 
 def __main__(argv):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     afs_data_service_pb2_grpc.add_AfsDataTransferServicer_to_server(
-        AfsDataTransfer(), server
-    )
+        AfsDataTransfer(), server)
     server.add_insecure_port('[::]:50053')
     server.start()
-    
+
     ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
     try:
--- fueling/data/afs_client/client.py
+++ fueling/data/afs_client/client.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import time
-
 from absl import flags
 import grpc
 
@@ -13,6 +12,7 @@ import fueling.common.logging as logging
 
 class AfsClient(BasePipeline):
     """Afs client."""
+
     def execute(self, task_id):
         """Connect to gRPC server, issue requests and get responses"""
         SERVER_URL = '180.76.53.252:50053'
@@ -20,27 +20,46 @@ class AfsClient(BasePipeline):
         with grpc.insecure_channel(SERVER_URL) as channel:
             # Get scan result, it could be a list of record files
             stub = afs_data_service_pb2_grpc.AfsDataTransferStub(channel)
-            request = afs_data_service_pb2.ScanRequest(dates=['123', '456'])
+            request = afs_data_service_pb2.ScanRequest(
+                table_name='kinglong/auto_car/cyberecord',
+                where='task_id = KL056_20200316141342')
             response = stub.Scan(request)
-            logging.info(F'task id: {task_id}, scan response: {response}')
+            for resItem in response.records:
+                rowKey = resItem.rowKey
+                task_id = resItem.task_id
+                start_time = resItem.start_time
+                end_time = resItem.end_time
+                logging.info(
+                    F'rowKey: {rowKey}, task id: {task_id}, start_time: {start_time}, end_time: {end_time}')
 
             # Get ReadMessages result, it could be a stream of record messages
-            request = afs_data_service_pb2.ReadMessagesRequest(record_files=['record1', 'record2'])
+            request = afs_data_service_pb2.ReadMessagesRequest(
+                task_id='KL056_20200326111402',
+                start_time_second=1585193067,
+                end_time_second=1585193068,
+                table_name='kinglong/auto_car')
             response = stub.ReadMessages(request)
             for message in response:
-                logging.info(F'task id: {task_id}, msg: {message.status}')
+                topic = message.topic
+                messagetext = message.message
+                data_type = message.data_type
+                timestamp = message.timestamp
+                logging.info(
+                    F'task id: {topic}, data_type: {data_type}, timestamp: {timestamp}')
                 messages_num += 1
-        # Wait a while for checking executor logs, and return message number for this executor
-        logging.info(F'executor {task_id}: got {messages_num} messages')
+        # Wait a while for checking executor logs, and return message number
+        # for this executor
+        logging.info(F'got {messages_num} messages')
         time.sleep(10)
         return messages_num
 
-        
     def run(self):
         """Run."""
         # Multiple tasks to get messages from gRPC service in parallel
-        tasks_count = 100
-        total_messages_num = self.to_rdd(range(tasks_count)).map(self.execute).sum()
+        tasks_count = 1
+        total_messages_num = self.to_rdd(
+            range(tasks_count)).map(
+            self.execute).sum()
         logging.info(F'total messages number: {total_messages_num}')
 
 
