655b42e2ddb849d3383f97a387fc3e1a6b38e950
Jiaming Tao <taojm99@gmail.com>
2020-04-24 10:37:26 -0700

    planning: update conver_bin_to_pb_txt to convert folders (#1760)

--- fueling/planning/common/convert_bin_to_pb_txt.py
+++ fueling/planning/common/convert_bin_to_pb_txt.py
@@ -10,33 +10,52 @@ from modules.planning.proto import learning_data_pb2
 
 
 class ConvertBinToPBTxt(BasePipeline):
-    """Records to feature proto pipeline."""
-
     def __init__(self):
-        self.src_file = 'modules/planning/learning_data/2019-10-17-13-36-41/learning_data.0.bin'
-        # self.src_file = '/apollo/data/learning_data/task_2/learning_data.0.bin'
-        # self.src_file = '/apollo/data/output_data/learning_data.0.bin.future_status.npy'
-
-    def run_test(self):
-        pass
+        self.src_dir_prefixs = [
+            'modules/planning/output_data/test/',
+        ]
 
     def run(self):
+        """Run Test"""
+        self.src_dir_prefixs = [
+            '/fuel/data/output_data/test/',
+        ]
+
+        src_dirs_set = set([])
+        for prefix in self.src_dir_prefixs:
+            for root, dirs, files in os.walk(prefix):
+                for file in files:
+                    src_dirs_set.add(root)
+
+        processed_dirs = self.to_rdd(src_dirs_set).map(self.process_dir)
+
+        logging.info('Processed {}/{} folders'.format(processed_dirs.count(),
+                                                      len(src_dirs_set)))
+        return 0
+    '''
+    def run(self):
         """Run"""
-        self.process_file(self.src_file)
-        logging.info('Processed: {}'.format(self.src_file))
+        records_rdd = BasePipeline.SPARK_CONTEXT.union([
+            self.to_rdd(self.our_storage().list_files(prefix))
+                .filter(record_utils.is_record_file)
+                .map(os.path.dirname)
+                .distinct()
+            for prefix in self.src_dir_prefixs])
 
-    def process_file(self, src_file):
-        """ Process File"""
-        dest_dir, dest_filename = os.path.split(src_file)
-        dest_filename += ".txt.tmp"
-        dest_file = src_file + ".txt.tmp"
+        processed_dirs = records_rdd.map(self.process_dir)
 
-        logging.info("src [{}]; dest[{}]".format(src_file, dest_file))
+        logging.info('Processed {} folders'.format(processed_dir.count()))
+    '''
+    def process_dir(self, src_dir):
+        for filename in os.listdir(src_dir):
+            if filename.endswith(".bin"):
+                src_file = os.path.join(src_dir, filename)
+                dest_file = src_file + ".txt"
+                pb = learning_data_pb2.LearningData()
+                pb = proto_utils.get_pb_from_bin_file(src_file, pb)
+                proto_utils.write_pb_to_text_file(pb, dest_file)
 
-        pb = learning_data_pb2.LearningData()
-        pb = proto_utils.get_pb_from_bin_file(src_file, pb)
-        proto_utils.write_pb_to_text_file(pb, dest_file)
-
+                logging.info("src [{}]; dest[{}]".format(src_file, dest_file))
         return 0
 
 
