767dbc9372e30f4931e8bcd3770a43e220f25263
michael4338 <michael4338@hotmail.com>
2020-05-01 09:47:25 -0700

    control: make general feature extraction work on cloud side

--- fueling/control/dynamic_model_2_0/feature_extraction/feature_extraction.py
+++ fueling/control/dynamic_model_2_0/feature_extraction/feature_extraction.py
@@ -13,50 +13,30 @@ from fueling.control.features.feature_extraction_utils import pair_cs_pose
 import fueling.common.h5_utils as h5_utils
 import fueling.common.logging as logging
 import fueling.common.record_utils as record_utils
-import fueling.control.features.feature_extraction_rdd_utils as feature_extraction_rdd_utils
 
 SEGMENT_LEN = 100 * 2
-SEGMENT_INTERVAL = 10 * 2  # 90% overlaping
+# 90% overlaping
+SEGMENT_INTERVAL = 10 * 2
 FINAL_SEGMENT_LEN = 100
-# PROD_INPUT_DIR = 'modules/control/data/records/Mkz7/2019-04-25'
-# PROD_TARGET_DIR = 'modules/control/DM2/test'
-PROD_INPUT_DIR = 'fueling/control/dynamic_model_2_0/testdata/golden_set/2_3'
-PROD_TARGET_DIR = 'fueling/control/dynamic_model_2_0/testdata/2_3'
 
 
 def write_segment(elem):
     """Write current data list to hdf5 file"""
     (folder_path, file_name), data_set = elem
     h5_utils.write_h5_single_segment(data_set, folder_path, file_name)
-    return folder_path
 
-
-def count_msgs(dir_msgRDD):
+def count_messages_filter(messages_rdd, topic):
     """Count messages from chassis and localization topic"""
-    folder, messages = dir_msgRDD
-    count_chassis = 0
-    count_localization = 0
-    for message in messages:
-        if message.topic == record_utils.CHASSIS_CHANNEL:
-            count_chassis += 1
-        elif message.topic == record_utils.LOCALIZATION_CHANNEL:
-            count_localization += 1
-    logging.info('{} chassis messages for record folder {}'.format(count_chassis, folder))
-    logging.info('{} localization messages for record folder {}'.format(count_localization, folder))
-    if count_chassis < SEGMENT_LEN / 2 or count_localization < SEGMENT_LEN / 2:
-        return (folder, [])
-    return dir_msgRDD
+    return messages_rdd.filter(lambda message: message.topic == topic).count() >= SEGMENT_LEN // 2
 
-
-def partition_data(target_msgs, segment_len=SEGMENT_LEN, segment_int=SEGMENT_INTERVAL):
+def partition_data(target_msgs):
     """Divide the messages to groups each of which has exact number of messages"""
     target, msgs = target_msgs
     logging.info('partition data for {} messages in target {}'.format(len(msgs), target))
     msgs = sorted(msgs, key=lambda msgs: msgs.timestamp)
-    msgs_groups = [msgs[idx: idx + segment_len]
-                   for idx in range(0, len(msgs), segment_int)]
+    msgs_groups = [msgs[idx: idx + SEGMENT_LEN]
+                   for idx in range(0, len(msgs), SEGMENT_INTERVAL)]
     return [(target, group_id, group) for group_id, group in enumerate(msgs_groups)]
-
 
 def get_datapoints(elem):
     """Generate data points from localization and chassis"""
@@ -68,95 +48,60 @@ def get_datapoints(elem):
     return np.hstack((data_point, time_stamp / 10**9))
 
 
-def count_pair_msgs(elem):
-    """ count paired msgs """
-    (segment_dir, group_id), data_list = elem
-    logging.info('{} data points for record folder {} segment {}'.format(
-        len(data_list), segment_dir, group_id))
-
-
 class FeatureExtraction(BasePipeline):
 
-    def run_test(self):
-        """Run test."""
-        # test data dir (folder/record_files)
-        test_data_dirs = '/fuel/apps/local/DM2'
-        test_data_dirs = 'fueling/control/dynamic_model_2_0/testdata/golden_set/2_3'
-        all_dirs = glob.glob(os.path.join(test_data_dirs, '*'))
-        target_dir = '/fuel/testdata/control/DM2_OUT/0816'
-        # RDD(tasks)
-        task = self.to_rdd(all_dirs)
-        self.run_internal(task, test_data_dirs, target_dir)
-
     def run(self):
-        """Run prod."""
-        # get subfolder of all records
-        task = self.to_rdd([PROD_INPUT_DIR])
-        task = spark_helper.cache_and_log(
-            'todo_jobs',
-            # RDD(relative_path_to_vehicle_type)
-            task
+        """Run."""
+        input_data_path = (self.FLAGS.get('input_data_path') or 
+            'modules/control/data/records/Mkz7/2019-06-04')
+        output_data_path = self.our_storage().abs_path(self.FLAGS.get('output_data_path') or
+            'modules/control/dynamic_model_2_0/features/2019-06-04')
+
+        logging.info(F'input_data_path: {input_data_path}, output_data_path: {output_data_path}')
+
+        dir_msgs_rdd = (
             # RDD(files)
-            .flatMap(self.our_storage().list_files)
-            # RDD(folders)
-            .map(os.path.dirname)
-            # RDD(distinct folders)
-            .distinct(), 1)
-        self.run_internal(task, PROD_INPUT_DIR, PROD_TARGET_DIR)
+            self.to_rdd(self.our_storage().list_files(input_data_path))
+            # RDD(record files)
+            .filter(record_utils.is_record_file)
+            # RDD(message), control and chassis message
+            .flatMap(record_utils.read_record([record_utils.CHASSIS_CHANNEL,
+                                               record_utils.LOCALIZATION_CHANNEL])))
 
-    def run_internal(self, task, original_prefix, target_prefix):
-        # configurable segments
-        dir_msgs_rdd = spark_helper.cache_and_log(
-            'record_to_msgs',
-            # RDD(tasks), with absolute paths
-            task
-            # PairRDD(target_dir, task), the map of target dirs and source dirs
-            .keyBy(lambda source: source.replace(original_prefix, target_prefix, 1))
-            # PairRDD(target_dir, record_file)
-            .flatMapValues(lambda task: glob.glob(os.path.join(task, '*record*')))
-            # PairRDD(target_dir, record_file), filter out unqualified files
-            # .filter(spark_op.filter_value(lambda file: record_utils.is_record_file(file)))
-            # PairRDD(target_dir, message), control and chassis message
-            .flatMapValues(record_utils.read_record([record_utils.CHASSIS_CHANNEL,
-                                                     record_utils.LOCALIZATION_CHANNEL]))
-            # PairRDD(target_dir, (message)s)
-            .groupByKey(), 0)
+        # Check if qualified for extraction 
+        if (not count_messages_filter(dir_msgs_rdd, record_utils.CHASSIS_CHANNEL) or
+            not count_messages_filter(dir_msgs_rdd, record_utils.LOCALIZATION_CHANNEL)):
+            logging.info('not qualified for extraction, quit')
+            return
 
-        hdf5_dir = spark_helper.cache_and_log(
-            'write_datapoint_to_hdf5',
-            # PairRDD(valid target_dir, (message)s)
-            dir_msgs_rdd
-            # PairRDD(valid target_dir, (message)s or [])
-            .map(count_msgs)
-            # PairRDD(valid target_dir, (message)s)
-            .filter(spark_op.filter_value(lambda msgs: len(msgs) > 0))
-            # RDD(target_dir, segment_id, group of (message)s), divide messages into groups
+        # RDD(messages)
+        (dir_msgs_rdd
+            # PairRDD(target_dir, message)
+            .map(lambda message: (output_data_path, message))
+            # PairRDD(target_dir, (messages))
+            .groupByKey()
+            # PairRDD(target_dir, (segment_id, messages))
             .flatMap(partition_data)
             # PairRDD((target_dir, segment_id), messages)
             .map(lambda args: ((args[0], args[1]), args[2]))
             # PairRDD((target_dir, segment_id), proto_dict)
             .mapValues(record_utils.messages_to_proto_dict())
-            # # PairRDD((dir_segment, segment_id), (chassis_list, pose_list))
+            # PairRDD((target_dir, segment_id), (chassis_list, pose_list))
             .mapValues(lambda proto_dict: (proto_dict[record_utils.CHASSIS_CHANNEL],
                                            proto_dict[record_utils.LOCALIZATION_CHANNEL]))
-            # PairRDD(target_dir, group_id, a single message),
+            # PairRDD((target_dir, group_id), a single message),
             .flatMapValues(pair_cs_pose)
-            # PairRDD(target_dir, group_id, a data point),
+            # PairRDD((target_dir, group_id), a data point),
             .mapValues(get_datapoints)
-            # PairRDD((target_dir, group_id), data_point RDD)
+            # PairRDD((target_dir, group_id), data_points)
             .groupByKey()
-            # PairRDD((target_dir, group_id), list of data_point)
-            .mapValues(list), 0)
-
-        # PairRDD((target_dir, group_id), len of list)
-        hdf5_dir.foreach(count_pair_msgs)
-
-        (hdf5_dir
-         # PairRDD((target_dir, group_id), list of data_point)
-         .filter(spark_op.filter_value(lambda msgs: len(msgs) == FINAL_SEGMENT_LEN))
-         # RDD(hdf5 file dir)
-         .map(write_segment)).count()
-
+            # PairRDD((target_dir, group_id), list of data_points)
+            .mapValues(list)
+            # Write each data points
+            .foreach(write_segment))
+ 
+        logging.info(F'extracted features to target dir {output_data_path}')
+        
 
 if __name__ == '__main__':
     FeatureExtraction().main()
