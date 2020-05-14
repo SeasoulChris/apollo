26cf4528320848cd6e4fb82e89d5426b98b90911
Yifei Jiang <jiangyifei@gmail.com>
2020-05-14 15:48:23 -0700

    planning: updated data cleanning source data

--- fueling/planning/cleaner/run_data_cleaner.sh
+++ fueling/planning/cleaner/run_data_cleaner.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bazel run //fueling/planning/cleaner:data_cleaner  -- --cloud --memory=5 --disk=100 --workers=40
+bazel run //fueling/planning/cleaner:data_cleaner  -- --cloud --memory=5 --disk=100 --workers=3
