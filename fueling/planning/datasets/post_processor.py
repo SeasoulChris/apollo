a0faa11375d1d72077cb584cdb249a4e0bc6206b
sjiang2018 <shujiang2016@gmail.com>
2020-05-21 10:50:16 -0700

    Planning: updated post process naming rule.

--- fueling/planning/datasets/post_processor.py
+++ fueling/planning/datasets/post_processor.py
@@ -17,10 +17,10 @@ class PostProcessor(BasePipeline):
     def run(self):
         if self.is_local():
             # for Titan
-            # self.src_dir_prefix = 'data/output_data_evaluated'
-            # self.dst_dir_prefix = 'data/output_data_categorized'
-            self.src_dir_prefix = 'titan'
-            self.dst_dir_prefix = 'apollo/data/output_data'
+            self.src_dir_prefix = 'data/output_data_evaluated'
+            self.dst_dir_prefix = 'data/output_data_categorized'
+            # self.src_dir_prefix = 'titan'
+            # self.dst_dir_prefix = 'apollo/data/output_data'
             for data_folder in os.listdir(self.our_storage().abs_path(self.src_dir_prefix)):
                 src_dir_prefix = os.path.join(self.src_dir_prefix, data_folder)
                 logging.info(f'processing data folder {src_dir_prefix}')
@@ -35,51 +35,39 @@ class PostProcessor(BasePipeline):
         bin_files = (
             # RDD(bin_files)
             self.to_rdd(self.our_storage().list_files(src_dir, '.bin'))
-            # PairedRDD(dir, bin_files)
-            .keyBy(lambda src_file: os.path.dirname(src_file)))
-        todo_bin_files = bin_files
+            # PairedRDD((dir, original_bin_file_name), bin_files)
+            .keyBy(lambda src_file: (os.path.dirname(src_file), os.path.basename(src_file))))
         logging.info(bin_files.first())
-        # PairedRDD(dir,data_frame)
+
+
+        # PairedRDD((dir, origin_file_name), data_frame))
         data_frames = bin_files.flatMapValues(self._get_data_frame)
         logging.debug(data_frames.first())
-        # PairedRDD(dir, ((tag, tag_id), data_frame))
+
+        # PairedRDD((dir, origin_file_name), ((tag, tag_id), data_frame))
         tag_data_frames = data_frames.flatMapValues(self._key_by_tag)
         logging.debug(tag_data_frames.first())
-        tag_data_frames = (tag_data_frames
-                           # remove 'complete' from path_dir
-                           # pre_fix/record_dir/complete -> pre_fix/record_dir
-                           # PairedRDD(dst_file_path, ((tag, tag_id), data_frame))
-                           .map(lambda elem: (os.path.split(elem[0])[0], elem[1]))
-                           # pre_fix/record_dir/complete -> dst_fix/record_dir
-                           # PairedRDD(dst_file_path, ((tag, tag_id), data_frame))
-                           .map(lambda elem: (elem[0].replace(self.src_dir_prefix, self.dst_dir_prefix), elem[1]))
-                           #PairedRDD(dst_file_path / tag / tag_id, data_frame)
-                           .map(self._tagged_folder))
-        logging.debug(tag_data_frames.first())
+
+        #PairedRDD((dst_file_path, origin_file_name), data_frame)
+        tag_data_frames = (tag_data_frames.map(lambda elem: self._tagged_folder(elem, self.src_dir_prefix, self.dst_dir_prefix)))
         logging.info(tag_data_frames.keys().first())
-        # collect data according to folder
-        # if self.is_local():
-        #     # write single_frame to each bin to reduce memory usage
+
+        # write single_frame to each bin to reduce memory usage
         tagged_folders = tag_data_frames.map(self._write_single_data_frame)
-        # else:
-        # tagged_folders = tag_data_frames.groupByKey().map(self._write_single_data_frame)
         logging.info(tagged_folders.count())
 
     @staticmethod
-    def _write_single_data_frame(folder_data_frame, is_debug=False):
-        # learning_data = learning_data_pb2.LearningData()
-        dst_dir, data_frames = folder_data_frame
-        if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)
-        logging.debug(data_frames.message_timestamp_sec)
-        file_name = f'{data_frames.message_timestamp_sec}.bin'
+    def _write_single_data_frame(folder_filename_data, is_debug=False):
+        (dst_dir, origin_filename), data = folder_filename_data
+        file_utils.makedirs(dst_dir)
+        logging.debug(data.message_timestamp_sec)
+        file_name = f'{origin_filename}_{data.message_timestamp_sec}.bin'
         logging.info(f'start writing to folder {dst_dir}')
         with open(os.path.join(dst_dir, file_name), 'wb') as bin_f:
-            bin_f.write(data_frames.SerializeToString())
+            bin_f.write(data.SerializeToString())
         if is_debug:
             txt_file_name = f'{file_name}.txt'
-            proto_utils.write_pb_to_text_file(data_frames,
-                                                      os.path.join(dst_dir, txt_file_name))
+            proto_utils.write_pb_to_text_file(data, os.path.join(dst_dir, txt_file_name))
 
     @staticmethod
     def _write_data_frame(folder_data_frame, frame_len=100, is_debug=False):
@@ -113,10 +101,6 @@ class PostProcessor(BasePipeline):
             file_name = f'{file_count}.bin'
             with open(os.path.join(dst_dir, file_name), 'wb') as bin_f:
                 bin_f.write(learning_data.SerializeToString())
-            # for debug only
-            # txt_file_name = f'{file_count}.txt'
-            # proto_utils.write_pb_to_text_file(learning_data.learning_data[0],
-            #                                   os.path.join(dst_dir, txt_file_name))
         return file_count
 
     @staticmethod
@@ -126,7 +110,7 @@ class PostProcessor(BasePipeline):
             bin_file, learning_data_pb2.LearningData())
         # get learning data sequence
         learning_data_sequence = offline_features.learning_data
-        logging.debug(len(learning_data_sequence))
+        logging.info(len(learning_data_sequence))
         return learning_data_sequence
 
     @staticmethod
@@ -148,18 +132,20 @@ class PostProcessor(BasePipeline):
         return tag_data_frames
 
     @staticmethod
-    def _tagged_folder(tag_data_frames, is_contain_date=True):
-        file_path, ((tag, tag_id), data_frame) = tag_data_frames
+    def _tagged_folder(dir_tag_data, src_dir, dst_dir):
+        (file_path, origin_file_name), ((tag, tag_id), data) = dir_tag_data
         logging.debug(tag)
         # id & distance
         logging.debug(tag_id)
-        if is_contain_date:
-            # dst_dir/date/ -> dst_dir/tag/tag_id/date
-            new_file_path, date = os.path.split(file_path)
-            dst_file_path = os.path.join(new_file_path, tag, tag_id, date)
-        else:
-            dst_file_path = os.path.join(file_path, tag, tag_id)
-        return (dst_file_path, data_frame)
+        # remove complete
+        # pre_fix/record_dir/complete -> pre_fix/record_dir
+        origin_file_path = os.path.split(file_path)[0]
+        # dst_dir dst_pre_fix/tag/tag_id
+        dst_dir = os.path.join(dst_dir, tag, tag_id)
+        # replace: pre_fix/record_dir -> dst_pre_fix/tag/tag_id/record_dir
+        dst_file_path = origin_file_path.replace(src_dir, dst_dir)
+        logging.debug(f'dst file dir: {dst_file_path}')
+        return ((dst_file_path, origin_file_name), data)
 
 
 
