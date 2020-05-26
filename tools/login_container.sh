703485dfffd95b2bce68f56513c669473e28e752
liuxu <759587231@qq.com>
2020-05-26 15:05:47 +0800

    change relative path to absolute path

--- tools/login_container.sh
+++ tools/login_container.sh
@@ -27,8 +27,7 @@ if [ $? -eq 0 ]; then
 else
   # Mount required volumes.
   required_volumes="-v $(pwd):/fuel"
-  echo "pwd: "`pwd`
-  APOLLO_ROOT="$(cd ../apollo-bazel2.x; pwd)"
+  APOLLO_ROOT="$(cd /home/apollo/apollo-bazel2.x; pwd)"
   required_volumes="-v ${APOLLO_ROOT}:/apollo ${required_volumes}"
 
   USER_ID=$(id -u)
