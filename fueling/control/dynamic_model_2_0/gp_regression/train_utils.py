12ee3d2452edd9af4cf9280c65a0c6fb7226c650
sjiang2018 <shujiang2016@gmail.com>
2020-06-02 12:39:18 -0700

    DM2.0: updated train_utils

--- fueling/control/dynamic_model_2_0/gp_regression/train_utils.py
+++ fueling/control/dynamic_model_2_0/gp_regression/train_utils.py
@@ -27,12 +27,14 @@ def init_train(inducing_points, encoder_net_model, output_dim, total_train_numbe
     return model, likelihood, optimizer, loss
 
 
-def basic_train_loop(train_loader, model, loss, optimizer):
+def basic_train_loop(train_loader, model, loss, optimizer, is_transpose=False):
     loss_history = []
     for x_batch, y_batch in train_loader:
         # **[window_size, batch_size, channel]
         # training process
         optimizer.zero_grad()
+        if is_transpose:
+            x_batch = torch.transpose(x_batch, 0, 1).type(torch.FloatTensor)
         output = model(x_batch)
         # train loss
         train_loss = -loss(output, y_batch)
