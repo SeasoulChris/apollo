203444fe05d7b26632973ee54f5a665e2dc65ff3
liuxu <759587231@qq.com>
2020-07-09 05:22:39 -0700

    bazel build: add distdir

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+TOP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+
 if [ -z "$1" ]; then
   TARGET="//..."
 else
@@ -14,11 +16,11 @@ set -e
 # Build widely-used apollo modules.
 pushd /apollo
   ./apollo.sh build_py
-  bazel build -c opt //cyber/py_wrapper:_cyber_record_py3.so
+  bazel build --distdir="/apollo/.cache/distdir" -c opt //cyber/py_wrapper:_cyber_record_py3.so
 popd
 
 if [ -f "WORKSPACE.bazel" ]; then
   echo "###### You are building with local pip-cache! ######"
 fi
 
-bazel build ${TARGET}
+bazel build --distdir="${TOP_DIR}/.cache/distdir" ${TARGET}
