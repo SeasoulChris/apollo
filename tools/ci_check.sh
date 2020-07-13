722309ca7cfe0003b8146d8decb1cf2486852680
liuxu <759587231@qq.com>
2020-07-13 00:09:44 -0700

    fuel CI: conda active

--- tools/ci_check.sh
+++ tools/ci_check.sh
@@ -6,4 +6,6 @@ if [ -e "/apollo/scripts/apollo_base.sh" ]; then
   source /apollo/scripts/apollo_base.sh
 fi
 
+source /usr/local/miniconda/bin/activate fuel
+source /usr/local/lib/bazel/bin/bazel-complete.bash
 bash /fuel/tools/check.sh
