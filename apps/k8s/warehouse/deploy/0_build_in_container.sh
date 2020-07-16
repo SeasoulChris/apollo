c4ddf9c1989d9dd32cdb30203a96239b43b36bfe
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2020-07-16 12:37:31 -0700

    Apps: Retire py_proto.

--- apps/k8s/warehouse/deploy/0_build_in_container.sh
+++ apps/k8s/warehouse/deploy/0_build_in_container.sh
@@ -2,10 +2,6 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-# Build Apollo.
-/apollo/apollo.sh build_py
-cp -r /apollo/py_proto ./
-
 # Build the app.
 bazel build //apps/k8s/warehouse:index
 cp -f /fuel/bazel-bin/apps/k8s/warehouse/index.zip ./
