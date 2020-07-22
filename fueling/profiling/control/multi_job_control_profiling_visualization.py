212fa6f39a6b44502b90cdd1c2804699e6d62b2d
kevin-y-wang <yuwang01@baidu.com>
2020-07-22 15:08:10 -0700

    Profiling: refactor control profiling visualization to meet Fuel2.0 and Dreamland2.0

--- fueling/profiling/control/multi_job_control_profiling_visualization.py
+++ fueling/profiling/control/multi_job_control_profiling_visualization.py
@@ -24,118 +24,166 @@ import fueling.profiling.control.feature_visualization.control_feature_visualiza
     as visual_utils
 
 
-flags.DEFINE_string('ctl_visual_input_path_local',
-                    '/fuel/testdata/profiling/multi_job_genanrated',
-                    'input data directory for local run_test')
-flags.DEFINE_string('ctl_visual_output_path_local',
-                    '/fuel/testdata/profiling/multi_job_genanrated',
-                    'output data directory for local run_test')
-flags.DEFINE_string('ctl_visual_todo_tasks_local', '',
-                    'todo_taks directory for local run_test')
 flags.DEFINE_boolean('ctl_visual_simulation_only_test', False,
                      'if simulation-only, then generate .json data file; otherwise, plotting')
-flags.DEFINE_string('ctl_visual_input_path_k8s', 'modules/control/tmp/results',
-                    'input data directory for run_prod')
-flags.DEFINE_string('ctl_visual_output_path_k8s', 'modules/control/tmp/results',
-                    'output data directory for run_pod')
+flags.DEFINE_string('ctl_visual_simulation_vehicle', 'Mkz7',
+                    'if simulation-only, then manually define the vehicle type in simulation')
 
 
 class MultiJobControlProfilingVisualization(BasePipeline):
     """ Control Profiling: Visualize Control Features"""
 
     def run(self):
-        """Work on actual road test data. Expect a single input directory"""
-        job_owner = self.FLAGS.get('job_owner')
-        # Use year as the job_id if data from apollo-platform, to avoid
-        # processing same data repeatedly
-        job_id = self.FLAGS.get('job_id') if self.is_partner_job() else self.FLAGS.get('job_id')[:4]
-        job_email = partners.get(job_owner).email if self.is_partner_job() else ''
-        logging.info(F'email address of job owner: {job_email}')
+        """Visualization for both external and internal applications"""
+        tic_start = time.perf_counter()
 
-        # same origin and target prefix
-        original_prefix = os.path.join(
-            flags.FLAGS.ctl_visual_input_path_k8s, job_owner, job_id)
-        target_prefix = os.path.join(
-            flags.FLAGS.ctl_visual_output_path_k8s, job_owner, job_id)
+        if flags.FLAGS.ctl_visual_simulation_only_test:
+            """Control Profiling: works on the 'auto-tuner + simulation' mode"""
 
-        # In visualization application, the object_storage always points to apollo storage.
-        object_storage = self.our_storage()
-        target_dir = object_storage.abs_path(target_prefix)
-        logging.info(F'target_dir: {target_dir}')
+            """Step 1: Path initialization to generate the input/output paths:"""
+            #   (1) target_dir: abs path where the base_dir is task_name
+            object_storage = self.our_storage()
+            target_dir = object_storage.abs_path(flags.FLAGS.output_data_path)
+            vehicle_type = flags.FLAGS.ctl_visual_simulation_vehicle
 
-        origin_dir = object_storage.abs_path(original_prefix)
-        logging.info(F'origin_dir: {origin_dir}')
+            """Step 2: Traverse files under input paths and generate todo_task paths:"""
+            #   todo_tasks: key: vehicle_type
+            #               value: abs path where the base_dir is task timestamp
+            # PairRDD(target files)
+            target_files = spark_helper.cache_and_log(
+                'target_files',
+                self.to_rdd([target_dir])
+                # PairRDD(vehicle_type, [vehicle_type])
+                .keyBy(lambda path: vehicle_type)
+                # PairRDD(vehicle_type, file_path_under_vehicle_type)
+                .flatMapValues(object_storage.list_files)
+            )
+            logging.info(F'target_files: {target_files.collect()}')
 
-        if not os.path.isdir(origin_dir):
-            error_msg = 'No visualization results: the source data path does not exist.'
-            summarize_tasks([], origin_dir, target_dir, job_email, error_msg)
-            logging.info('Control Profiling Visualization: No Results, PROD')
-            return
+            # PairRDD(todo profiled dirs)
+            todo_tasks = spark_helper.cache_and_log(
+                'todo_tasks',
+                target_files
+                # PairRDD(vehicle_type, path)
+                .mapValues(os.path.dirname)
+                .distinct()
+            )
+            # if dirs have been graded before, then list them as follows
+            logging.info(F'todo_tasks to run: {todo_tasks.collect()}')
 
-        # PairRDD(target files)
-        target_files = spark_helper.cache_and_log(
-            'target_files',
-            self.to_rdd([target_dir])
-            # RDD([vehicle_type])
-            .flatMap(multi_vehicle_utils.get_vehicle)
-            # PairRDD(vehicle_type, [vehicle_type])
-            .keyBy(lambda vehicle_type: vehicle_type)
-            # PairRDD(vehicle_type, path_to_vehicle_type)
-            .mapValues(lambda vehicle_type: os.path.join(target_prefix, vehicle_type))
-            # PairRDD(vehicle_type, records)
-            .flatMapValues(object_storage.list_files)
-        )
-        logging.info(F'target_files: {target_files.collect()}')
+            if not todo_tasks.collect():
+                logging.info('Control Profiling Visualization: No Results')
+                return
 
-        # PairRDD(processed plot dirs)
-        processed_dirs = spark_helper.cache_and_log(
-            'processed_dirs',
-            target_files
-            # PairRDD(vehicle_type, file endwith COMPLETE_PLOT)
-            .filter(lambda key_path: key_path[1].endswith('COMPLETE_PLOT'))
-            # PairRDD(vehicle_type, path)
-            .mapValues(os.path.dirname)
-            .distinct()
-        )
-        # if dir processed before, just like
-        # /mnt/bos/modules/control/tmp/results/apollo/2019-11-25-10-47-19
-        # /Mkz7/Lon_Lat_Controller/Road_Test-2019-05-01/20190501110414'
-        logging.info(F'processed_dirs: {processed_dirs.collect()}')
+            """Step 3: Process data with profiling algorithm"""
+            self.process(todo_tasks.values())
 
-        # PairRDD(todo profiled dirs)
-        todo_tasks = spark_helper.cache_and_log(
-            'todo_tasks',
-            target_files
-            # PairRDD(vehicle_type, file endwith COMPLETE)
-            .filter(lambda key_path: key_path[1].endswith('COMPLETE'))
-            # PairRDD(vehicle_type, path)
-            .mapValues(os.path.dirname)
-            .distinct()
-        )
-        logging.info(F'todo_tasks before filtering: {todo_tasks.collect()}')
+        else:
+            """Control Visualization: works on the 'external/internal-user road-test' mode"""
 
-        if not todo_tasks.collect():
-            error_msg = 'No visualization results: no new qualified data uploaded.'
-            summarize_tasks([], origin_dir, target_dir, job_email, error_msg)
-            logging.info('Control Profiling Visualization: No Results, PROD')
-            return
+            """Step 1: Path initialization to generate the input/output paths:"""
+            #   (1) origin_dir: usually the same as target_dir
+            #   (2) target_dir: abs path where the base_dir is data-processing date
+            job_owner = self.FLAGS.get('job_owner')
+            # Use year as the job_id if data from apollo-platform, to avoid
+            # processing same data repeatedly
+            job_id = (self.FLAGS.get('job_id') if self.is_partner_job() else
+                      self.FLAGS.get('job_id')[:4])
+            job_email = partners.get(job_owner).email if self.is_partner_job() else ''
+            logging.info(F'email address of job owner: {job_email}')
 
-        if not processed_dirs.isEmpty():
-            todo_tasks = todo_tasks.subtract(processed_dirs)
+            # obtain the target prefix
+            output_prefix = (self.FLAGS.get('output_data_path')
+                             or 'modules/control/tmp/results')
+            target_prefix = os.path.join(output_prefix, job_owner, job_id)
 
-        logging.info(F'todo_tasks to run: {todo_tasks.values().collect()}')
+            # For visualization, the object_storage always points to apollo storage.
+            # by default, the target_dir is set to be the same as original_prefix
+            object_storage = self.our_storage()
+            target_dir = object_storage.abs_path(target_prefix)
+            origin_dir = target_dir
+            logging.info(F'target_dir: {target_dir}')
 
-        if not todo_tasks.collect():
-            error_msg = 'No visualization results: all the uploaded data has been processed before.'
-            summarize_tasks([], origin_dir, target_dir, job_email, error_msg)
-            logging.info('Control Profiling Visualization: No Results, PROD')
-            return
+            if not os.path.isdir(origin_dir):
+                error_msg = 'No visualization results: the source data path does not exist.'
+                summarize_tasks([], origin_dir, target_dir, job_email, error_msg)
+                logging.info('Control Profiling Visualization: No Results, PROD')
+                return
 
-        self.run_in(todo_tasks.values(), origin_dir, target_dir)
-        summarize_tasks(todo_tasks.values().collect(), origin_dir, target_dir, job_email)
+            """Step 2: Traverse files under input paths and generate todo_task paths:"""
+            #   todo_tasks: key: vehicle_type
+            #               value: abs path where the base_dir is task timestamp
+            # PairRDD(target files)
+            target_files = spark_helper.cache_and_log(
+                'target_files',
+                self.to_rdd([target_dir])
+                # RDD([vehicle_type])
+                .flatMap(multi_vehicle_utils.get_vehicle)
+                # PairRDD(vehicle_type, [vehicle_type])
+                .keyBy(lambda vehicle_type: vehicle_type)
+                # PairRDD(vehicle_type, data_path_to_vehicle_type)
+                .mapValues(lambda vehicle_type: os.path.join(target_prefix, vehicle_type))
+                # PairRDD(vehicle_type, file_path_under_vehicle_type)
+                .flatMapValues(object_storage.list_files)
+            )
+            logging.info(F'target_files: {target_files.collect()}')
+
+            # PairRDD(processed plot dirs)
+            processed_dirs = spark_helper.cache_and_log(
+                'processed_dirs',
+                target_files
+                # PairRDD(vehicle_type, file endwith COMPLETE_PLOT)
+                .filter(lambda key_path: key_path[1].endswith('COMPLETE_PLOT'))
+                # PairRDD(vehicle_type, path)
+                .mapValues(os.path.dirname)
+                .distinct()
+            )
+            # if dirs have been visualized before, then list them as follows
+            logging.info(F'processed_dirs: {processed_dirs.collect()}')
+
+            # PairRDD(todo profiled dirs)
+            todo_tasks = spark_helper.cache_and_log(
+                'todo_tasks',
+                target_files
+                # PairRDD(vehicle_type, file endwith COMPLETE)
+                .filter(lambda key_path: key_path[1].endswith('COMPLETE'))
+                # PairRDD(vehicle_type, path)
+                .mapValues(os.path.dirname)
+                .distinct()
+            )
+            # if dirs have been graded before, then list them as follows
+            logging.info(F'todo_tasks before filtering: {todo_tasks.collect()}')
+
+            if not todo_tasks.collect():
+                error_msg = 'No visualization results: no new qualified data uploaded.'
+                summarize_tasks([], origin_dir, target_dir, job_email, error_msg)
+                logging.info('Control Profiling Visualization: No Results, PROD')
+                return
+
+            # todo_tasks = graded tasks - visualized tasks
+            if not processed_dirs.isEmpty():
+                todo_tasks = todo_tasks.subtract(processed_dirs)
+            # if dirs have been graded but not visualized, then list them as:
+            # /mnt/bos/modules/control/tmp/results/apollo/2019-11-25-10-47-19
+            # /Mkz7/Lon_Lat_Controller/Road_Test-2019-05-01/20190501110414'
+            logging.info(F'todo_tasks to run: {todo_tasks.collect()}')
+
+            if not todo_tasks.collect():
+                error_msg = 'No visualization results: all the data have been processed before.'
+                summarize_tasks([], origin_dir, target_dir, job_email, error_msg)
+                logging.info('Control Profiling Visualization: No Results, PROD')
+                return
+
+            """Step 3: Process data with profiling algorithm"""
+            self.process(todo_tasks.values())
+
+            """Step 4: Summarize by scanning the target directory and send out emails"""
+            summarize_tasks(todo_tasks.values().collect(), origin_dir, target_dir, job_email)
+
+        logging.info(f"Timer: total run() - {time.perf_counter() - tic_start: 0.04f} sec")
         logging.info('Control Profiling Visualization: All Done, PROD')
 
-    def run_in(self, todo_tasks, original_prefix, target_prefix):
+    def process(self, todo_tasks):
         """Run the pipeline with given parameters"""
         # RDD(tasks), with absolute paths
         data_rdd = (todo_tasks
