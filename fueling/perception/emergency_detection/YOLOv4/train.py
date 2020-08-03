bc5e7096f6be3de5225c4ced1bfc504d77e63f82
Shen Yu <lastseed@live.cn>
2020-08-03 18:16:32 -0400

    M. Code clean

--- fueling/perception/emergency_detection/YOLOv4/train.py
+++ fueling/perception/emergency_detection/YOLOv4/train.py
@@ -282,7 +282,7 @@ def collate(batch):
     return images, bboxes
 
 
-def train(model, device, config, epochs=5, batch_size=1, save_cp_step=10, log_step=1, img_scale=0.5):
+def train(model, device, config, epochs=5, batch_size=1, save_cp_step=10, log_step=20, img_scale=0.5):
     train_dataset = Yolo_dataset(config.train_label, config, train=True)
     val_dataset = Yolo_dataset(config.val_label, config, train=False)
 
@@ -521,70 +521,7 @@ def evaluate(model, data_loader, cfg, device, logger=None, **kwargs):
     coco_evaluator.summarize()
 
     return coco_evaluator
-
-
-def get_args(**kwargs):
-    cfg = kwargs
     
-    '''
-    parser = argparse.ArgumentParser(description='Train the Model on images and target masks',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
-    #                     help='Batch size', dest='batchsize')
-    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.001,
-                        help='Learning rate', dest='learning_rate')
-    parser.add_argument('-f', '--load', dest='load', type=str, default=None,
-                        help='Load model from a .pth file')
-    parser.add_argument('-g', '--gpu', metavar='G', type=str, default='-1',
-                        help='GPU', dest='gpu')
-    parser.add_argument('-dir', '--data-dir', type=str, default=None,
-                        help='dataset dir', dest='dataset_dir')
-    parser.add_argument('-pretrained', type=str, default=None, help='pretrained yolov4.conv.137')
-    parser.add_argument('-classes', type=int, default=80, help='dataset classes')
-    parser.add_argument('-train_label_path', dest='train_label', type=str, default='train.txt', help="train label path")
-    parser.add_argument(
-        '-optimizer', type=str, default='adam',
-        help='training optimizer',
-        dest='TRAIN_OPTIMIZER')
-    parser.add_argument(
-        '-iou-type', type=str, default='iou',
-        help='iou type (iou, giou, diou, ciou)',
-        dest='iou_type')
-    parser.add_argument(
-        '-keep-checkpoint-max', type=int, default=10,
-        help='maximum number of checkpoints to keep. If set 0, all checkpoints will be kept',
-        dest='keep_checkpoint_max')
-    args = vars(parser.parse_args())
-    '''
-
-    args={'learning_rate': 0.001, 'load': None, 'gpu': '0', 'dataset_dir': '/mnt/bos/modules/perception/emergency_detection/data/emergency_vehicle/images', 
-    'pretrained': '/mnt/bos/modules/perception/emergency_detection/pretrained_model/yolov4.conv.137.pth', 'classes': 2, 
-    'train_label': '/mnt/bos/modules/perception/emergency_detection/data/emergency_vehicle/train.txt', 
-    'val_label': '/mnt/bos/modules/perception/emergency_detection/data/emergency_vehicle/val.txt', 
-    'checkpoints': '/mnt/bos/modules/perception/emergency_detection/checkpoints', 
-    'TRAIN_TENSORBOARD_DIR': '/mnt/bos/modules/perception/emergency_detection/log', 
-    'TRAIN_OPTIMIZER': 'adam', 'iou_type': 'iou', 'keep_checkpoint_max': 10}
-    
-
-
-    # for k in args.keys():
-    #     cfg[k] = args.get(k)
-    cfg.update(args)
-
-    return edict(cfg)
-
-def get_args_local(**kwargs):
-    cfg = kwargs
-    args={'learning_rate': 0.001, 'load': None, 'gpu': '-1', 'dataset_dir': '/fuel/fueling/perception/emergency_detection/data/emergency_vehicle/images', 
-    'pretrained': '/fuel/fueling/perception/emergency_detection/pretrained_model/yolov4.conv.137.pth', 'classes': 2, 
-    'train_label': '/fuel/fueling/perception/emergency_detection/data/emergency_vehicle/train.txt', 
-    'val_label': '/fuel/fueling/perception/emergency_detection/data/emergency_vehicle/val.txt', 
-    'TRAIN_OPTIMIZER': 'adam', 'iou_type': 'iou', 'keep_checkpoint_max': 10}
-
-    cfg.update(args)
-
-    return edict(cfg)
-
 
 def init_logger(log_file=None, log_dir=None, log_level=logging.INFO, mode='w', stdout=True):
     """
@@ -661,6 +598,51 @@ def train_yolov4(cfg):
             sys.exit(0)
         except SystemExit:
             os._exit(0)
+
+
+def get_args_local(**kwargs):
+    cfg = kwargs
+
+    '''
+    args={'learning_rate': 0.001, 'load': None, 'gpu': '-1', 'dataset_dir': '/fuel/fueling/perception/emergency_detection/data/emergency_vehicle/images', 
+    'pretrained': '/fuel/fueling/perception/emergency_detection/pretrained_model/yolov4.conv.137.pth', 'classes': 2, 
+    'train_label': '/fuel/fueling/perception/emergency_detection/data/emergency_vehicle/train.txt', 
+    'val_label': '/fuel/fueling/perception/emergency_detection/data/emergency_vehicle/val.txt', 
+    'TRAIN_OPTIMIZER': 'adam', 'iou_type': 'iou', 'keep_checkpoint_max': 10}
+    '''
+
+    parser = argparse.ArgumentParser(description='Train the Model on images and target masks',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=2,
+    #                     help='Batch size', dest='batchsize')
+    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.001,
+                        help='Learning rate', dest='learning_rate')
+    parser.add_argument('-f', '--load', dest='load', type=str, default=None,
+                        help='Load model from a .pth file')
+    parser.add_argument('-g', '--gpu', metavar='G', type=str, default='-1',
+                        help='GPU', dest='gpu')
+    parser.add_argument('-dir', '--data-dir', type=str, default=None,
+                        help='dataset dir', dest='dataset_dir')
+    parser.add_argument('-pretrained', type=str, default=None, help='pretrained yolov4.conv.137')
+    parser.add_argument('-classes', type=int, default=80, help='dataset classes')
+    parser.add_argument('-train_label_path', dest='train_label', type=str, default='train.txt', help="train label path")
+    parser.add_argument(
+        '-optimizer', type=str, default='adam',
+        help='training optimizer',
+        dest='TRAIN_OPTIMIZER')
+    parser.add_argument(
+        '-iou-type', type=str, default='iou',
+        help='iou type (iou, giou, diou, ciou)',
+        dest='iou_type')
+    parser.add_argument(
+        '-keep-checkpoint-max', type=int, default=10,
+        help='maximum number of checkpoints to keep. If set 0, all checkpoints will be kept',
+        dest='keep_checkpoint_max')
+    args = vars(parser.parse_args())
+    
+    cfg.update(args)
+
+    return edict(cfg)
 
 
 if __name__ == "__main__":
