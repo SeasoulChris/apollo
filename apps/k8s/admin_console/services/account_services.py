8396d3a52b2fb1360146182599a170c8677f52bb
walle-zhao <208665675@qq.com>
2020-08-04 16:57:20 +0800

    admin_console:modify the http code (#2283)

--- apps/k8s/admin_console/services/account_services.py
+++ apps/k8s/admin_console/services/account_services.py
@@ -1,24 +1,23 @@
-#!/usr/bin/env python
-
 import flask_restful
 from flask_restful import reqparse
 
 from controllers import account
+from fueling.common import file_utils
 from utils import conf_utils
 
 
 class AccountServices(flask_restful.Resource):
     def get(self, account_id):
-        parse = reqparse.RequestParser()
-        parse.add_argument("id", type=str, required=True, help="Id cannot be blank!")
+        if len(account_id) != 24:
+            return {"desc": "The id is invalid",
+                    "result": {}}, 400
         accounts = account.account_db.get_account_info({"_id": account_id})
         if not accounts:
-            return {"code": 400,
-                    "desc": "Don't find the account obj, please check the id",
-                    "result": {}}
+            return {"desc": "Don't find the account obj, please check the id",
+                    "result": {}}, 400
         accounts_used = account.get_job_used(accounts)
         accounts_objs = account.stamp_account_time(accounts_used)
-        return {"code": 200, "desc": "success", "result": accounts_objs[0]}
+        return {"desc": "success", "result": accounts_objs[0]}
 
     def post(self):
         parse = reqparse.RequestParser()
@@ -43,7 +42,8 @@ class AccountServices(flask_restful.Resource):
         account_dict["no_vehicle_sn"] = False
         if not vehicle_sn:
             account_dict["vehicle_sn"] = (account.get_virtual_vehicle_sn
-                                          ("apps/k8s/admin_console/conf/admin.json"))
+                                          (file_utils.fuel_path
+                                           ("apps/k8s/admin_console/conf/admin.json")))
             account_dict["no_vehicle_sn"] = True
         account_id = account.account_db.create_account_msg(account_dict)
         conf_dict = conf_utils.get_conf("JOB_TYPE")
@@ -56,16 +56,17 @@ class AccountServices(flask_restful.Resource):
         accounts = account.account_db.get_account_info({"_id": account_id})
         accounts_used = account.get_job_used(accounts)
         accounts_objs = account.stamp_account_time(accounts_used)
-        return {"code": 200, "desc": "success", "result": accounts_objs[0]}
+        return {"desc": "success", "result": accounts_objs[0]}
 
     def put(self, account_id):
         parse = reqparse.RequestParser()
-        parse.add_argument("id", type=str, required=True, help="Id cannot be blank!")
+        if len(account_id) != 24:
+            return {"desc": "The id is invalid",
+                    "result": {}}, 400
         accounts = account.account_db.get_account_info({"_id": account_id})
         if not accounts:
-            return {"code": 400,
-                    "desc": "Don't find the account obj, please check the id",
-                    "result": {}}
+            return {"desc": "Don't find the account obj, please check the id",
+                    "result": {}}, 400
         account_obj = accounts[0]
         if account_obj["status"] == "Rejected":
             parse.add_argument("com_name", type=str, required=True,
@@ -85,7 +86,6 @@ class AccountServices(flask_restful.Resource):
                                help="Purpose cannot be blank!")
             args = parse.parse_args()
             account_dict = args.copy()
-            account_dict.pop("id")
             vehicle_sn = account_dict.get("vehicle_sn")
             account_dict["no_vehicle_sn"] = False
             if not vehicle_sn:
@@ -97,7 +97,8 @@ class AccountServices(flask_restful.Resource):
                     is_virtual = False
                 account_dict["vehicle_sn"] = (obj_vehicle_sn if is_virtual else
                                               account.get_virtual_vehicle_sn
-                                              ("apps/k8s/admin_console/conf/admin.json"))
+                                              (file_utils.fuel_path
+                                               ("apps/k8s/admin_console/conf/admin.json")))
             account.account_db.update_account_msg(account_id, account_dict)
         elif account_obj["status"] == "Enabled":
             parse.add_argument("com_name", type=str, required=True,
@@ -112,13 +113,11 @@ class AccountServices(flask_restful.Resource):
                                help="Bos Sk cannot be blank!")
             args = parse.parse_args()
             account_dict = args.copy()
-            account_dict.pop("id")
             account.account_db.update_account_msg(account_id, account_dict)
         else:
-            return {"code": 400,
-                    "desc": "The status is error, the data hasn't changed",
-                    "result": {}}
+            return {"desc": "The status is error, the data hasn't changed",
+                    "result": {}}, 400
         new_accounts = account.account_db.get_account_info({"_id": account_id})
         accounts_used = account.get_job_used(new_accounts)
         accounts_objs = account.stamp_account_time(accounts_used)
-        return {"code": 200, "desc": "success", "result": accounts_objs[0]}
+        return {"desc": "success", "result": accounts_objs[0]}
