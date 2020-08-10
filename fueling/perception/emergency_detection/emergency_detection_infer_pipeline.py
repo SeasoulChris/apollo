6271ca409afab7c2cccfd57f88f49896d251cc6c
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2020-08-10 13:04:55 -0700

    Perception: Fix check issues.

--- fueling/perception/emergency_detection/emergency_detection_infer_pipeline.py
+++ fueling/perception/emergency_detection/emergency_detection_infer_pipeline.py
@@ -1,24 +1,8 @@
 #!/usr/bin/env python
-"""
-A simplest demo to calculate square sum of 1...n.
 
-Run at local:
-    bazel run //fueling/demo:simplest_demo
-    bazel run //fueling/demo:simplest_demo -- --square_sum_of_n=1000
-
-Run in cloud:
-    bazel run //fueling/demo:simplest_demo -- --cloud
-    bazel run //fueling/demo:simplest_demo -- --cloud --square_sum_of_n=1000
-"""
-
-import sys
-sys.path.append("/fuel")
-
-from absl import flags
+from yolov4.inference import inference_yolov4
 
 from fueling.common.base_pipeline import BasePipeline
-from fueling.perception.emergency_detection.YOLOv4.inference import inference_yolov4
-
 
 
 class EmergencyVehicleDetector(BasePipeline):
@@ -30,4 +14,4 @@ class EmergencyVehicleDetector(BasePipeline):
 
 if __name__ == '__main__':
     EmergencyVehicleDetector().main()
-    #inference_yolov4(is_local=True)
+    # inference_yolov4(is_local=True)
