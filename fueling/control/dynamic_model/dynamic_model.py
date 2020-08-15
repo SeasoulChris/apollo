84d801920444eee59fe8d2998b532f6c0975a6c8
kevin-y-wang <yuwang01@baidu.com>
2020-08-15 16:38:26 -0700

    DM1.0: refactor the visualizaton function for online service

--- fueling/control/dynamic_model/dynamic_model.py
+++ fueling/control/dynamic_model/dynamic_model.py
@@ -4,14 +4,15 @@
 from fueling.common.base_pipeline import SequentialPipeline
 from fueling.control.dynamic_model.feature_extraction.sample_set import SampleSet
 from fueling.control.dynamic_model.feature_extraction.uniform_set import UniformSet
+from fueling.control.dynamic_model.dynamic_model_data_visualization \
+    import DynamicModelDatasetDistribution
 from fueling.control.dynamic_model.dynamic_model_training import DynamicModelTraining
 
 
 if __name__ == '__main__':
     SequentialPipeline([
-        # the total pipeline of dynamic model training does not work properly, please divide
-        # feature extration and model training
         SampleSet(),
         UniformSet(),
+        DynamicModelDatasetDistribution(),
         DynamicModelTraining(),
     ]).main()
--- fueling/control/dynamic_model/dynamic_model_data_visualization.py
+++ fueling/control/dynamic_model/dynamic_model_data_visualization.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
-import fueling.control.common.multi_vehicle_plot_utils as multi_vehicle_plot_utils
-import fueling.control.dynamic_model.data_generator.feature_extraction as data_generator
-import fueling.common.logging as logging
-import fueling.common.spark_helper as spark_helper
-from fueling.common.base_pipeline import BasePipeline
-import glob
 import os
 import time
 
+import glob
 import matplotlib
 matplotlib.use('Agg')
+
+from fueling.common.base_pipeline import BasePipeline
+import fueling.common.logging as logging
+import fueling.common.spark_helper as spark_helper
+import fueling.control.common.multi_vehicle_plot_utils as multi_vehicle_plot_utils
+import fueling.control.common.multi_vehicle_utils as multi_vehicle_utils
+from fueling.control.dynamic_model.conf.model_config import task_config
+import fueling.control.dynamic_model.data_generator.feature_extraction as feature_extraction
 
 
 class DynamicModelDatasetDistribution(BasePipeline):
@@ -31,29 +34,48 @@ class DynamicModelDatasetDistribution(BasePipeline):
 
         hdf5_file_list = self.to_rdd(glob.glob(file_dir))
         print(hdf5_file_list.collect())
-        self.run_internal(hdf5_file_list, result_file)
+        self.run_internal('Mkz7', hdf5_file_list, result_file)
 
     def run(self):
-        # hdf5 data directory
-        # prefix = 'modules/control/data/results/UniformDistributed/Mkz7'
-        prefix = 'modules/control/data/results/SampleSet/Mkz7'
-        # file path to save visualization results
-        output_dir = self.our_storage().abs_path(prefix)
-        timestr = time.strftime('%Y%m%d-%H%M%S')
-        file_name = ('dataset_distribution_%s.pdf' % timestr)
-        result_file = os.path.join(output_dir, file_name)
-        logging.info('Result File: %s', result_file)
+        # initialize input/output dirs
+        job_owner = self.FLAGS.get('job_owner')
+        job_id = self.FLAGS.get('job_id')
+        is_backward = self.FLAGS.get('is_backward')
 
-        hdf5_file_list = self.to_rdd(self.our_storage().list_files(prefix, '.hdf5'))
-        self.run_internal(hdf5_file_list, result_file)
+        our_storage = self.our_storage()
 
-    def run_internal(self, hdf5_file_list, result_file):
+        data_dir = task_config['sample_output_folder']
+        if is_backward:
+            data_prefix = os.path.join(data_dir, job_owner, 'backward', job_id)
+        else:
+            data_prefix = os.path.join(data_dir, job_owner, 'forward', job_id)
+        visual_data_prefix = our_storage.abs_path(data_prefix)
+
+        # get vehicles
+        vehicles = multi_vehicle_utils.get_vehicle(visual_data_prefix)
+        logging.info(f'Vehicles: {vehicles}')
+
+        # run proc as a vehicle ID
+        for vehicle in vehicles:
+            # list hdf5 files from sample set
+            visual_data_path = os.path.join(visual_data_prefix, vehicle)
+            hdf5_file_list = self.to_rdd(self.our_storage().list_files(visual_data_path, '.hdf5'))
+            logging.info(f'Visual data file: {hdf5_file_list.collect()}')
+            # define visual result file name
+            output_dir = os.path.join(visual_data_path, 'visual_result')
+            os.mkdir(output_dir)
+            result_file = os.path.join(output_dir, 'dynamic_model_sample_dataset_distribution.pdf')
+            logging.info(f'Visual result file: {result_file}')
+            # generate visual result file
+            self.run_internal(vehicle, hdf5_file_list, result_file)
+
+    def run_internal(self, vehicle, hdf5_file_list, result_file):
 
         data = spark_helper.cache_and_log(
             'plots',
-            hdf5_file_list.keyBy(lambda _: 'Mkz7')
+            hdf5_file_list.keyBy(lambda _: vehicle)
             .groupByKey()
-            .mapValues(data_generator.generate_segment_from_list))
+            .mapValues(feature_extraction.generate_segment_from_list))
 
         spark_helper.cache_and_log(
             'plots',
