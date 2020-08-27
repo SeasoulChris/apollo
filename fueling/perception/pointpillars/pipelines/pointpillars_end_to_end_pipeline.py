9d32f25c70646c87795de09c4da931a1aeaf2246
panfengsu <supanfeng@baidu.com>
2020-08-26 22:30:04 -0700

    Perception:fix lint issues

--- fueling/perception/pointpillars/pipelines/pointpillars_end_to_end_pipeline.py
+++ fueling/perception/pointpillars/pipelines/pointpillars_end_to_end_pipeline.py
@@ -3,7 +3,8 @@
 
 from fueling.common.base_pipeline import SequentialPipeline
 from fueling.perception.pointpillars.pipelines.create_data_pipeline import CreateDataNuscenes
-from fueling.perception.pointpillars.pipelines.pointpillars_training_pipeline import PointPillarsTraining
+from fueling.perception.pointpillars.pipelines.pointpillars_training_pipeline import (
+    PointPillarsTraining,)
 
 if __name__ == '__main__':
     SequentialPipeline([
