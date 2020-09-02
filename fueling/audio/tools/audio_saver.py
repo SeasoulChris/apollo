6063e68b171a8fd2b15628c1fd426536fc55b96b
Kecheng Xu <kechxu89@gmail.com>
2020-09-02 15:50:17 -0700

    audio: keep folder structure which saving to .wav files (#2469)

--- fueling/audio/tools/audio_saver.py
+++ fueling/audio/tools/audio_saver.py
@@ -2,10 +2,8 @@
 """
 Save audio data in records to wave files
 
-Run with:
-    bazel run //fueling/demo:stat_auto_mileage -- --cloud
-    --record_folder=<records_dir_path_in_bos>
-    --output_folder=<output_path_for_wave_in_bos>
+Example command:
+bazel run fueling/audio/tools:audio_saver -- --cloud --record_folder=public-test/2020/2020-08-24
 """
 
 from absl import flags
@@ -15,24 +13,24 @@ import fueling.common.logging as logging
 import os
 import wave
 
-# RECORD_PREFIX = 'public-test/2020/2020-08-24/2020-08-24-14-58-13'
-# OUTPUT_DIR = 'modules/audio/waves'
+RECORD_PREFIX = 'public-test'
+OUTPUT_PREFIX = 'modules/audio/wav_files'
 
 flags.DEFINE_string('record_folder', None, 'parse records in record folder')
-flags.DEFINE_string('output_folder', None, 'save wave files to output folder')
 
 
 class AudioSaver(BasePipeline):
 
     def run(self):
-        output_dir = self.our_storage().abs_path(flags.FLAGS.output_folder)
-        logging.info("Save to {}".format(output_dir))
+        # output_dir = self.our_storage().abs_path(flags.FLAGS.output_folder)
+        # record_dir = self.our_storage().abs_path(flags.FLAGS.record_folder)
+        # logging.info("Save to {}".format(output_dir))
         self.to_rdd(
             self.our_storage().list_files(flags.FLAGS.record_folder)).filter(
                 record_utils.is_record_file).foreach(
-                    lambda r: self.save_to_wave(r, output_dir))
+                    lambda r: self.save_to_wave(r))
 
-    def save_to_wave(self, record, output_dir):
+    def save_to_wave(self, record):
         """Save records to wave files"""
         logging.info("Processing record: {}".format(record))
         self.frames = [b"" for _ in range(6)]
@@ -42,15 +40,24 @@ class AudioSaver(BasePipeline):
             if msg.topic == record_utils.MICROPHONE_CHANNEL:
                 audio = record_utils.message_to_proto(msg)
                 self.parse(audio)
-        self.dump_to_wave(os.path.basename(record), output_dir)
+        self.dump_to_wave(record)
 
-    def dump_to_wave(self, record_base, output_dir):
+    def dump_to_wave(self, record):
         """Save frame to file.wave"""
+        record_base = os.path.basename(record)
+        record_end_dir = os.path.dirname(record)
+        output_end_dir = record_end_dir.replace(RECORD_PREFIX, OUTPUT_PREFIX)
+        mkdir_cmd = 'sudo mkdir -p {}'.format(output_end_dir)
+        chmod_cmd = 'sudo chmod 777 {}'.format(output_end_dir)
+        os.system(mkdir_cmd)
+        logging.info(mkdir_cmd)
+        os.system(chmod_cmd)
+        logging.info(chmod_cmd)
         for idx, data in enumerate(self.frames):
             if idx != 2:
                 continue
             file_path = os.path.join(
-                output_dir, "{}_channel_{}.wav".format(record_base, idx))
+                output_end_dir, "{}_channel_{}.wav".format(record_base, idx))
             logging.info("Saving wave to {}".format(file_path))
             with wave.open(file_path, 'wb') as wf:
                 wf.setnchannels(1)
