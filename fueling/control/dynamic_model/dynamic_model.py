5be6022bc23fdbe748ca125f9c18ba18b530c211
kevin-y-wang <yuwang01@baidu.com>
2020-09-03 02:26:52 -0700

    DM1.0: fix bugs on the Flags setting

--- fueling/control/dynamic_model/dynamic_model.py
+++ fueling/control/dynamic_model/dynamic_model.py
@@ -2,11 +2,13 @@
 """Wrapper of dynamic model pipeline."""
 
 from fueling.common.base_pipeline import SequentialPipeline
-from fueling.control.dynamic_model.feature_extraction.sample_set import SampleSet
-from fueling.control.dynamic_model.feature_extraction.uniform_set import UniformSet
 from fueling.control.dynamic_model.dynamic_model_data_visualization \
     import DynamicModelDatasetDistribution
 from fueling.control.dynamic_model.dynamic_model_training import DynamicModelTraining
+from fueling.control.dynamic_model.flag import flags
+from fueling.control.dynamic_model.feature_extraction.sample_set import SampleSet
+from fueling.control.dynamic_model.feature_extraction.uniform_set import UniformSet
+import fueling.common.logging as logging
 
 
 if __name__ == '__main__':
@@ -16,3 +18,7 @@ if __name__ == '__main__':
         DynamicModelDatasetDistribution(),
         DynamicModelTraining(),
     ]).main()
+    if flags.FLAGS.is_backward:
+        logging.info('Backward dynamic modeling finished.')
+    else:
+        logging.info('Forward dynamic modeling finished.')
--- fueling/control/dynamic_model/flag.py
+++ fueling/control/dynamic_model/flag.py
@@ -2,8 +2,8 @@
 from absl import flags
 
 # flags open to public via online service interface
-flags.DEFINE_bool('is_backward', False, 'backward or forward for dynamic model training')
+flags.DEFINE_boolean('is_backward', False, 'backward or forward for dynamic model training')
 
 # flags hold for internal use
-flags.DEFINE_bool('is_holistic', False, 'holistic or nonholistic for dynamic model training')
-flags.DEFINE_bool('evaluation_vehicle_id', 'Mkz7', 'default vehicle_id for evaluation')
+flags.DEFINE_boolean('is_holistic', False, 'holistic or nonholistic for dynamic model training')
+flags.DEFINE_string('evaluation_vehicle_id', 'Mkz7', 'default vehicle_id for evaluation')
